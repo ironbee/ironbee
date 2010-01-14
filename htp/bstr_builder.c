a0a6563eb9ffcfad51db578f79e2fa2a5b4acabe
Ivan Ristic <iristic@qualys.com>
2010-01-14 08:51:42 +0000

    Document the string builder code.

--- htp/bstr_builder.c
+++ htp/bstr_builder.c
@@ -16,10 +16,24 @@
 #include "bstr_builder.h"
 #include "dslib.h"
 
+/**
+ * Returns the size (the number of pieces) currently in a string builder.
+ *
+ * @param bb
+ * @return size
+ */
 size_t bstr_builder_size(bstr_builder_t *bb) {
     return list_size(bb->pieces);
 }
 
+/**
+ * Clears this string builder, destroying all existing pieces. You may
+ * want to clear a builder once you've either read all the pieces and
+ * done something with them, or after you've converted the builder into
+ * a single string.
+ *
+ * @param bb
+ */
 void bstr_builder_clear(bstr_builder_t *bb) {
     // Destroy any pieces we might have
     bstr *b = NULL;
@@ -34,6 +48,11 @@ void bstr_builder_clear(bstr_builder_t *bb) {
     // TODO What should we do on allocation failure?
 }
 
+/**
+ * Creates a new string builder.
+ *
+ * @return New string builder
+ */
 bstr_builder_t * bstr_builder_create() {
     bstr_builder_t *bb = calloc(1, sizeof(bstr_builder_t));
     if (bb == NULL) return NULL;
@@ -47,6 +66,12 @@ bstr_builder_t * bstr_builder_create() {
     return bb;
 }
 
+/**
+ * Destroys an existing string builder, also destroying all
+ * the pieces stored within.
+ * 
+ * @param bb
+ */
 void bstr_builder_destroy(bstr_builder_t *bb) {
     if (bb == NULL) return;
 
@@ -62,22 +87,53 @@ void bstr_builder_destroy(bstr_builder_t *bb) {
     free(bb);
 }
 
+/**
+ * Adds one new string to the builder.
+ *
+ * @param bb
+ * @param b
+ * @return Success indication
+ */
 int bstr_builder_append(bstr_builder_t *bb, bstr *b) {
     return list_push(bb->pieces, b);
 }
 
+/**
+ * Adds one new piece, defined with the supplied pointer and
+ * length, to the builder.
+ *
+ * @param bb
+ * @param data
+ * @param len
+ * @return Success indication
+ */
 int bstr_builder_append_mem(bstr_builder_t *bb, char *data, size_t len) {
     bstr *b = bstr_memdup(data, len);
     if (b == NULL) return -1; // TODO Is the return code correct?
     return list_push(bb->pieces, b);
 }
 
-int bstr_builder_append_cstr(bstr_builder_t *bb, char *str) {
-    bstr *b = bstr_cstrdup(str);
+/**
+ * Adds one new piece, in the form of a NUL-terminated string, to
+ * the builder.
+ *
+ * @param bb
+ * @param cstr
+ * @return Success indication
+ */
+int bstr_builder_append_cstr(bstr_builder_t *bb, char *cstr) {
+    bstr *b = bstr_cstrdup(cstr);
     if (b == NULL) return -1; // TODO Is the return code correct?
     return list_push(bb->pieces, b);
 }
 
+/**
+ * Creates a single string out of all the pieces held in a
+ * string builder. This method will not destroy any of the pieces.
+ *
+ * @param bb
+ * @return New string
+ */
 bstr * bstr_builder_to_str(bstr_builder_t *bb) {
     bstr *b = NULL;
     size_t len = 0;
