aa972bf1329380d1c78675b8ab831a4a5221300b
Brian Rectanus <brectanus@qualys.com>
2011-02-24 00:26:21 -0800

    Fix typo in autogen script.

--- autogen.sh
+++ autogen.sh
@@ -7,7 +7,7 @@ rm -rf autom4te.cache
 set -x
 aclocal
 # TODO: detect glibtoolize
-if [ "$OSTYPE" == "darwin10.0" ]; then
+if [ "$OSTYPE" = "darwin10.0" ]; then
     glibtoolize --automake --force --copy
 else
     libtoolize --automake --force --copy
