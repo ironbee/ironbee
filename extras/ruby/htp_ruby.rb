8388fc3067238d7d7cf26b07142313d20564c2c8
Christopher Alfeld <calfeld@qualys.com>
2011-06-07 10:24:23 -0500

    Clean up implementation of request_params.  Removes some unnecessary overhead.

--- extras/ruby/htp_ruby.rb
+++ extras/ruby/htp_ruby.rb
@@ -180,13 +180,8 @@ module HTP
     def request_params
       if ! @request_params
         @request_params = Hash.new {|h,k| h[k] = []}
-        if request_params_query
-          request_params_query.each do |k,v|
-            @request_params[k] << v
-          end
-        end
-        if request_params_body
-          request_params_body.each do |k,v|
+        [ request_params_query, request_params_body ].compact.each do |result|
+          result.each do |k,v|
             @request_params[k] << v
           end
         end
