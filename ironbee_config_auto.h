66022ff2207fa31951c16ee3333893a49a6ff354
Brian Rectanus <brectanus@qualys.com>
2011-11-09 10:04:19 -0600

    Wrap the generated autoconf include file.

---
+++ ironbee_config_auto.h
@@ -0,0 +1,35 @@
+/*****************************************************************************
+ * Licensed to Qualys, Inc. (QUALYS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * QUALYS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+#ifndef _IB_CONFIG_AUTO_H_
+#define _IB_CONFIG_AUTO_H_
+
+/**
+ * @file
+ * @brief IronBee - Autoconf Header
+ *
+ * This file wraps the generated autoconf header to provide define
+ * blocks to prevent issue when included more than once.
+ *
+ * @warning Only include this in source files.
+ *
+ * @author Brian Rectanus <brectanus@qualys.com>
+ */
+
+#include "ironbee_config_auto_gen.h"
+
+#endif /* _IB_CONFIG_AUTO_H_ */
