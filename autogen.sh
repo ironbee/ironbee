7b52cbec2510afe758ac845e509d49d20958d65e
William Metcalf <wmetcalf@qualys.com>
2011-12-21 10:54:53 -0600

    small autogen.sh update

--- autogen.sh
+++ autogen.sh
@@ -2,16 +2,6 @@
 
 # Cleanup
 rm -rf autom4te.cache
-touch build/config.rpath
 
-# Generate
-set -x
-aclocal
-# TODO: detect glibtoolize
-case x"$OSTYPE" in
-    xdarwin*) glibtoolize --automake --force --copy ;;
-    *) libtoolize --automake --force --copy ;;
-esac
-autoconf
-autoheader
-automake --add-missing --force --copy --foreign
+touch build/config.rpath
+autoreconf -i -f  -v
