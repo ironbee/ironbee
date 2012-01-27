35a4bff4d72a36897865a1d8759debf3e70db7da
Nick LeRoy <nleroy@qualys.com>
2012-01-27 13:28:32 -0600

    Integrated the remote address module into the user agent module, removed the remote address module

--- modules/user_agent.c
+++ modules/user_agent.c
@@ -494,7 +494,7 @@ static ib_status_t modua_agent_fields(ib_engine_t *ib,
 
 /**
  * @internal
- * Handle request_header events.
+ * Handle request_header events for user agent extraction.
  *
  * Extract the "request_headers" field (a list) from the transactions's
  * data provider instance, then loop through the list, looking for the
@@ -507,9 +507,9 @@ static ib_status_t modua_agent_fields(ib_engine_t *ib,
  *
  * @returns Status code
  */
-static ib_status_t modua_handle_req_headers(ib_engine_t *ib,
-                                            ib_tx_t *tx,
-                                            void *data)
+static ib_status_t modua_user_agent(ib_engine_t *ib,
+                                    ib_tx_t *tx,
+                                    void *data)
 {
     IB_FTRACE_INIT(modua_handle_req_headers);
     ib_field_t   *req_agent = NULL;
@@ -534,6 +534,75 @@ static ib_status_t modua_handle_req_headers(ib_engine_t *ib,
 
 /**
  * @internal
+ * Handle request_header events for remote IP extraction.
+ *
+ * Extract the "request_headers" field (a list) from the transactions's
+ * data provider instance, then loop through the list, looking for the
+ * "X-Forwarded-For"  field.  If found, the first value in the (comma
+ * separated) list replaces the local ip address string in the connection
+ * object.
+ *
+ * @param[in] ib IronBee object
+ * @param[in,out] tx Transaction object
+ * @param[in] data Callback data (not used)
+ *
+ * @returns Status code
+ */
+static ib_status_t modua_remoteip(ib_engine_t *ib,
+                                  ib_tx_t *tx,
+                                  void *data)
+{
+    IB_FTRACE_INIT(modra_remoteip_);
+    ib_field_t    *req_fwd = NULL;
+    ib_status_t    rc = IB_OK;
+    ib_bytestr_t  *bs;
+    unsigned       len;
+    char          *buf;
+    uint8_t       *comma;
+
+    /* Extract the request headers field from the provider instance */
+    rc = ib_data_get(tx->dpi, "request_headers.X-Forwarded-For", &req_fwd);
+    if ( (req_fwd == NULL) || (rc != IB_OK) ) {
+        ib_log_debug(ib, 4,
+                     "request_headers_event: No forward header" );
+        IB_FTRACE_RET_STATUS(IB_OK);
+    }
+
+
+    /* Found it: copy the data into a newly allocated string buffer */
+    bs = ib_field_value_bytestr(req_fwd);
+    len = ib_bytestr_length(bs);
+
+    /* Search for a comma in the buffer */
+    comma = memchr(ib_bytestr_ptr(bs), ',', len);
+    if (comma != NULL) {
+        len = comma - ib_bytestr_ptr(bs);
+    }
+
+    /* Allocate the memory */
+    buf = (char *)ib_mpool_calloc(tx->mp, 1, len+1);
+    if (buf == NULL) {
+        ib_log_error( ib, 4,
+                      "Failed to allocate %d bytes for local address",
+                      len+1 );
+        IB_FTRACE_RET_STATUS(IB_EALLOC);
+    }
+
+    /* Copy the string out */
+    memcpy(buf, ib_bytestr_ptr(bs), len);
+    buf[len] = '\0';
+
+    ib_log_debug(ib, 4, "Remote address => '%s'", buf);
+
+    /* This will lose the pointer to the original address
+     * buffer, but it should be cleaned up with the rest
+     * of the memory pool. */
+    tx->er_ipstr = buf;
+    IB_FTRACE_RET_STATUS(IB_OK);
+}
+
+/**
+ * @internal
  * Called to initialize the user agent module (when the module is loaded).
  *
  * Registers a handler for the request_headers_event event.
@@ -550,9 +619,17 @@ static ib_status_t modua_init(ib_engine_t *ib, ib_module_t *m)
     modua_match_rule_t *failed_rule;
     unsigned int failed_frule_num;
 
-    /* Register our callback */
+    /* Register the user agent callback */
     rc = ib_hook_register(ib, request_headers_event,
-                          (ib_void_fn_t)modua_handle_req_headers,
+                          (ib_void_fn_t)modua_user_agent,
+                          (void*)request_headers_event);
+    if (rc != IB_OK) {
+        ib_log_error(ib, 4, "Hook register returned %d", rc);
+    }
+
+    /* Register the remote address callback */
+    rc = ib_hook_register(ib, request_headers_event,
+                          (ib_void_fn_t)modua_remoteip,
                           (void*)request_headers_event);
     if (rc != IB_OK) {
         ib_log_error(ib, 4, "Hook register returned %d", rc);
