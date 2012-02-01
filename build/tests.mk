b9dbe0534e8db8eec3e65416b49c273d48755f28
Christopher Alfeld <calfeld@qualys.com>
2012-02-01 14:47:16 -0600

    build: Add -Wno-unused-parameter to all builds.
    The callback heavy nature of IronBee combined with the C requirements that all parameters be named makes these unavoidable.

--- build/tests.mk
+++ build/tests.mk
@@ -1,10 +1,14 @@
+# -Wno-unused-parameter is because IronBee is so callback heavy multiple
+# implements ignore one or more of their parameters.
+# In C++, we could ommit the name, but not in C.
 CPPFLAGS += @GCC_CHARACTERISTICS_CPPFLAGS@ \
             @IB_DEBUG@ \
             -I$(top_srcdir)/tests/gtest/include \
             -I$(top_srcdir)/include \
             -I$(top_srcdir)/util \
             -I$(top_srcdir)/engine \
-            -I$(top_srcdir)
+            -I$(top_srcdir) \
+            -Wno-unused-parameter
 
 CXXFLAGS += -g -O2
 
