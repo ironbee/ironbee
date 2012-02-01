c8107ae6725ca9ba6d30602c49b1b38290e86de1
Christopher Alfeld <calfeld@qualys.com>
2012-02-01 14:54:58 -0600

    test/test_util_uuid.cc: Fix warnings.

--- tests/test_util_uuid.cc
+++ tests/test_util_uuid.cc
@@ -40,31 +40,31 @@ struct testval {
 static struct testval uuidstr[] = {
     { "01234567-89ab-cdef-0123-456789abcdef",
       IB_OK,
-      { 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
-        0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef } },
+      { { 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
+          0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef } } },
     { "01234567-89ab-cdef-0123-456789abcdef ",
-      IB_EINVAL, { 0 } },
+      IB_EINVAL, { { 0 } } },
     { " 01234567-89ab-cdef-0123-456789abcdef",
-      IB_EINVAL, { 0 } },
+      IB_EINVAL, { { 0 } } },
     { " 01234567-89ab-cdef-0123-456789abcdef ",
-      IB_EINVAL, { 0 } },
+      IB_EINVAL, { { 0 } } },
     { "0123456789abcdef0123456789abcdef",
-      IB_EINVAL, { 0 } },
+      IB_EINVAL, { { 0 } } },
     { "1234",
-      IB_EINVAL, { 0 } },
+      IB_EINVAL, { { 0 } } },
     { "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-      IB_EINVAL, { 0 } },
+      IB_EINVAL, { { 0 } } },
     { "0123456789abcdef0123456789abcdex",
-      IB_EINVAL, { 0 } },
+      IB_EINVAL, { { 0 } } },
     { "0123456789abcdef0123456789abcdxf",
-      IB_EINVAL, { 0 } },
+      IB_EINVAL, { { 0 } } },
     { "0123456789abcdef0123456789abcdefxxx",
-      IB_EINVAL, { 0 } },
+      IB_EINVAL, { { 0 } } },
     { "xxx0123456789abcdef0123456789abcdef",
-      IB_EINVAL, { 0 } },
+      IB_EINVAL, { { 0 } } },
     { "xxx0123456789abcdef0123456789abcdefxxx",
-      IB_EINVAL, { 0 } },
-    { NULL, IB_OK, { 0 } }
+      IB_EINVAL, { { 0 } } },
+    { NULL, IB_OK, { { 0 } } }
 };
 
 
@@ -74,11 +74,10 @@ static struct testval uuidstr[] = {
 TEST(TestIBUtilUUID, test_field_create)
 {
     struct testval *rec;
-    ib_uuid_t uuid;
     ib_status_t rc;
 
     for (rec = &uuidstr[0];rec->str != NULL; ++rec) {
-        ib_uuid_t uuid = { 0 };
+        ib_uuid_t uuid = { { 0 } };
         rc = ib_uuid_ascii_to_bin(&uuid, rec->str);
         ASSERT_TRUE(rc == rec->ret) << "ib_uuid_ascii_to_bin() failed - bad return code: " << rec->str;
         if (rc == IB_OK) {
