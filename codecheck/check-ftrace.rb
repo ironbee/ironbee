e3d28e5db08a9048140980648f71d0bfacc7706e
Christopher Alfeld <calfeld@qualys.com>
2012-02-06 15:11:27 -0600

    codecheck/check-ftrace.rb: Add heuristic script to look for functions that use FTRACE_INIT but are missing FTRACE_RET_X macros.

---
+++ codecheck/check-ftrace.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+
+MY_DIR = File.expand_path(File.dirname(__FILE__))
+IRONBEE_DIR = File.dirname(MY_DIR)
+
+Dir.chdir( IRONBEE_DIR )
+Dir.glob( '**/*.[ch]' ).each do |path|
+  prev_line = nil
+  current_function = nil
+  last_function_line = nil
+  in_ftrace_function = false
+  IO.foreach(path).each do |line|
+    error = lambda do |msg|
+      puts "#{path}:#{$.}: #{msg}"
+    end
+    
+    line.chomp!
+    next if line =~ %r{^\s*$} || line =~ %r{^\s*/?\*\s} || line =~ %r{^\s*//}
+    line.gsub!(%r{/\* .+ \*/},'')
+    
+    if line =~ /^\w/
+      last_function_line = line
+    end
+    
+    if line[0..0] == '{'
+      if prev_line =~ /\)/
+        current_function = last_function_line
+      else 
+        current_function = nil
+      end
+      in_ftrace_function = false
+    elsif prev_line && prev_line[0..0] == '{' && current_function
+      if line =~ /FTRACE_INIT/
+        in_ftrace_function = true
+      end
+    elsif line =~ /\breturn(\s|;)/ && in_ftrace_function
+      error["MISSING FTRACE_RET: #{current_function}"]
+    end
+    
+    prev_line = line
+  end
+end
