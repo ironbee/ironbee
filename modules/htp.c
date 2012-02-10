fbe339e967d7e09aa50a84e3537151cd1850472c
Christopher Alfeld <calfeld@qualys.com>
2012-02-10 09:12:46 -0600

    htp.c: Ignoring a warning in htp.h that shows up with clang3.

--- modules/htp.c
+++ modules/htp.c
@@ -43,7 +43,14 @@
 #include <ironbee/module.h>
 #include <ironbee/provider.h>
 
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wundef"
+#endif
 #include <htp.h>
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
 #include <dslib.h>
 
 
