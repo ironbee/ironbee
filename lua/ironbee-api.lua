6fea550bcbb80b0a9cbb0b72ab5325a41c5ef69f
Sam Baskinger <basking2@yahoo.com>
2012-02-15 14:57:37 -0600

    Building basic lua testing framework.

--- lua/ironbee-api.lua
+++ lua/ironbee-api.lua
@@ -2,4 +2,13 @@
 -- Lua 5.2 and later style module.
 ib = {}
 
+local ffi = require("ffi")
+local ironbee = require("ironbee-ffi")
+
+ib.new = function()
+    ib_obj = {}
+
+    return ob_obj
+end
+
 return ib
