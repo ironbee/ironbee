f25ab29bff2bc71638773cc84819a1bf28bfde7a
Sam Baskinger <basking2@yahoo.com>
2012-02-17 23:20:37 -0600

    Adding unit tests for new Lua API to set/get strings from the DPI.

--- tests/test_ironbee_lua_api.cc
+++ tests/test_ironbee_lua_api.cc
@@ -1,11 +1,14 @@
 #include <gtest/gtest.h>
 
+#include <iostream>
 #include <string>
 #include <stdexcept>
 #include "base_fixture.h"
 
 extern "C" {
 #include <ironbee/release.h>
+#include <ironbee/engine.h>
+#include <ironbee/field.h>
 #include <lauxlib.h>
 #include <lua.h>
 #include <lualib.h>
@@ -25,7 +28,9 @@ public:
 
     lua_State* L;
 
-    ib_tx_t ib_tx;
+    ib_conn_t *ib_conn;
+    ib_tx_t *ib_tx;
+    ib_module_t *mod_htp;
 
     /**
      * Calls BaseFixture::SetUp(), then creates a new Lua State,
@@ -35,6 +40,29 @@ public:
     virtual void SetUp()
     {
         BaseFixture::SetUp();
+
+        ib_state_notify_cfg_started(ib_engine);
+
+        assert(ib_engine->temp_mp != NULL);
+        assert(ib_engine->config_mp != NULL);
+
+        /* We need the ibmod_htp to initialize the ib_tx. */
+        configureIronBee("test_ironbee_lua_api.conf");
+        assert(IB_OK == ib_state_notify_cfg_finished(ib_engine));
+
+        ib_conn_create(ib_engine, &ib_conn, NULL);
+        ib_conn->local_ipstr = "1.0.0.1";
+        ib_conn->remote_ipstr = "1.0.0.2";
+        ib_conn->remote_port = 65534;
+        ib_conn->local_port = 80;
+        ib_state_notify_conn_opened(ib_engine, ib_conn);
+
+        sendDataIn("GET / HTTP/1.1\r\nHost: UnitTest\r\n\r\n");
+        sendDataOut("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n");
+
+        /* Lib htp.c does this, so we do this here. */
+        assert(ib_conn->tx!=NULL);
+        ib_tx = ib_conn->tx;
 
         /* Initialize a new lua state. */
         L = luaL_newstate();
@@ -51,8 +79,27 @@ public:
 
         lua_pushlightuserdata(L, ib_engine);
         lua_setglobal(L, "ib_engine");
-        lua_pushlightuserdata(L, &ib_tx);
+        lua_pushlightuserdata(L, ib_tx);
         lua_setglobal(L, "ib_tx");
+    
+        /* Construct an IB value. */
+        eval("ib = ibapi.new(ib_engine, ib_tx)");
+    }
+
+    void sendDataIn(const string& req) {
+        ib_conndata_t *ib_conndata;
+        ib_conn_data_create(ib_conn, &ib_conndata, req.size());
+        ib_conndata->dlen = req.size();
+        memcpy(ib_conndata->data, req.data(), req.size());
+        ib_state_notify_conn_data_in(ib_engine, ib_conndata);
+    }
+
+    void sendDataOut(const string& req) {
+        ib_conndata_t *ib_conndata;
+        ib_conn_data_create(ib_conn, &ib_conndata, req.size());
+        ib_conndata->dlen = req.size();
+        memcpy(ib_conndata->data, req.data(), req.size());
+        ib_state_notify_conn_data_out(ib_engine, ib_conndata);
     }
 
     void require(const string& name, const string& module)
@@ -108,12 +155,59 @@ public:
     virtual void TearDown()
     {
         lua_close(L);
+
+        ib_state_notify_conn_closed(ib_engine, ib_conn);
+
         BaseFixture::TearDown();
     }
 };
 
 TEST_F(IronBeeLuaApi, log_error)
 {
-    eval("ib = ibapi.new(ib_engine, ib_tx)");
     eval("ib:log_error(\"======== Test Log Message %d ========\", 100)");
 }
+
+TEST_F(IronBeeLuaApi, log_debug)
+{
+    eval("ib:log_debug(\"======== Test Log Message %d ========\", 100)");
+}
+
+TEST_F(IronBeeLuaApi, set_string)
+{
+    const char* key = "key1";
+    const char* val = "myStringValue";
+
+    // Call ib:setString("key1", "myStringValue")
+    eval(std::string("ib:setString(\"")+key+"\", \""+val+"\")");
+
+    ib_field_t* ib_field;
+    ib_data_get_ex(ib_tx->dpi, key, strlen(key), &ib_field);
+
+    ASSERT_TRUE(NULL!=ib_field);
+    ASSERT_STREQ(val, static_cast<char*>(ib_field_value(ib_field)));
+}
+
+TEST_F(IronBeeLuaApi, get_string)
+{
+    const char* key = "key2";
+    const char* const_value = "myStringValue";
+    char* value = (char*)malloc(sizeof(const_value)+1);
+
+    strcpy(value, const_value);
+    ib_field_t* ib_field;
+    ib_data_add_nulstr_ex(ib_tx->dpi, key, strlen(key), value, &ib_field);
+    ib_data_get_ex(ib_tx->dpi, key, strlen(key), &ib_field);
+
+    ASSERT_TRUE(NULL!=ib_field);
+    ASSERT_STREQ(const_value, static_cast<char*>(ib_field_value(ib_field)));
+
+    // Run return ib:getString("key2")
+    // We require the return to put the value on the lua stack to check.
+    eval(std::string("return ib:getString(\"")+key+"\")");
+    ASSERT_STREQ(const_value, lua_tostring(L, -1));
+    lua_pop(L, 1);
+
+    free(value);
+}
+
+
