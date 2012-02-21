298f69c6d4ae6d5b256bd5295663e187bcdc5f9d
Christopher Alfeld <calfeld@qualys.com>
2012-02-21 11:23:02 -0600

    test_util_array: Clean up.

--- tests/test_util_array.cc
+++ tests/test_util_array.cc
@@ -24,43 +24,56 @@
 
 #include "ironbee_config_auto.h"
 
+#include <ironbee/array.h>
+#include <ironbee/mpool.h>
+#include <ironbee/util.h>
+
 #include "gtest/gtest.h"
 #include "gtest/gtest-spi.h"
 
-#include "util/util.c"
-#include "util/array.c"
-#include "util/mpool.c"
-#include "util/debug.c"
+#include <stdexcept>
 
+class TestIBUtilArray : public ::testing::Test
+{
+public:
+    TestIBUtilArray()
+    {
+        ib_status_t rc;
+        
+        ib_initialize();
+        rc = ib_mpool_create(&m_pool, NULL, NULL);
+        if (rc != IB_OK) {
+            throw std::runtime_error("Could not create mpool.");
+        }
+    }
+    
+    ~TestIBUtilArray()
+    {
+        ib_shutdown();        
+    }
+    
+protected:
+    ib_mpool_t* m_pool;
+};
 
 /* -- Tests -- */
 
 /// @test Test util array library - ib_htree_array() and ib_array_destroy()
-TEST(TestIBUtilArray, test_array_create_and_destroy)
+TEST_F(TestIBUtilArray, test_array_create_and_destroy)
 {
-    ib_mpool_t *mp;
     ib_array_t *arr;
     ib_status_t rc;
-
-    atexit(ib_shutdown);
-    rc = ib_initialize();
-    ASSERT_TRUE(rc == IB_OK) << "ib_initialize() failed - rc != IB_OK";
-    rc = ib_mpool_create(&mp, NULL, NULL);
-    ASSERT_TRUE(rc == IB_OK) << "ib_mpool_create() failed - rc != IB_OK";
     
-    rc = ib_array_create(&arr, mp, 10, 10);
-    ASSERT_TRUE(rc == IB_OK) << "ib_array_create() failed - rc != IB_OK";
-    ASSERT_TRUE(arr != NULL) << "ib_array_create() failed - NULL value";
-    ASSERT_TRUE(ib_array_size(arr) == 10) << "ib_array_create() failed - wrong size";
-    ASSERT_TRUE(ib_array_elements(arr) == 0) << "ib_array_create() failed - wrong number of elements";
-
-    ib_mpool_destroy(mp);
+    rc = ib_array_create(&arr, m_pool, 10, 10);
+    ASSERT_EQ(IB_OK, rc);
+    ASSERT_TRUE(arr);
+    ASSERT_EQ(10UL, ib_array_size(arr));
+    ASSERT_EQ(0UL, ib_array_elements(arr));
 }
 
 /// @test Test util array library - ib_htree_setn() and ib_array_get()
-TEST(TestIBUtilArray, test_array_set_and_get)
+TEST_F(TestIBUtilArray, test_array_set_and_get)
 {
-    ib_mpool_t *mp;
     ib_array_t *arr;
     ib_status_t rc;
     int v0 = 0;
@@ -71,110 +84,101 @@ TEST(TestIBUtilArray, test_array_set_and_get)
     int v1000 = 1000;
     int v1000000 = 1000000;
     int *val;
-    
-    atexit(ib_shutdown);
-    rc = ib_initialize();
-    ASSERT_TRUE(rc == IB_OK) << "ib_initialize() failed - rc != IB_OK";
-    rc = ib_mpool_create(&mp, NULL, NULL);
-    ASSERT_TRUE(rc == IB_OK) << "ib_mpool_create() failed - rc != IB_OK";
-    
-    rc = ib_array_create(&arr, mp, 10, 10);
-    ASSERT_TRUE(rc == IB_OK) << "ib_array_create() failed - rc != IB_OK";
-    ASSERT_TRUE(arr != NULL) << "ib_array_create() failed - NULL value";
-    ASSERT_TRUE(ib_array_size(arr) == 10) << "ib_array_create() failed - wrong size";
-    ASSERT_TRUE(ib_array_elements(arr) == 0) << "ib_array_create() failed - wrong number of elements";
+        
+    rc = ib_array_create(&arr, m_pool, 10, 10);
+    ASSERT_EQ(IB_OK, rc);
+    ASSERT_TRUE(arr);
+    ASSERT_EQ(10UL, ib_array_size(arr));
+    ASSERT_EQ(0UL, ib_array_elements(arr));
 
     /* Get invalid. */
     rc = ib_array_get(arr, 10, &val);
-    ASSERT_TRUE(rc == IB_EINVAL) << "ib_array_get() failed - rc != IB_EINVAL";
-    ASSERT_TRUE(val == NULL) << "ib_array_get() failed - not NULL value";
-    ASSERT_TRUE(ib_array_size(arr) == 10) << "ib_array_create() failed - wrong size";
-    ASSERT_TRUE(ib_array_elements(arr) == 0) << "ib_array_create() failed - wrong number of elements";
+    ASSERT_EQ(IB_EINVAL, rc);
+    ASSERT_FALSE(val);
+    ASSERT_EQ(10UL, ib_array_size(arr));
+    ASSERT_EQ(0UL, ib_array_elements(arr));
 
     /* Simple set. */
     rc = ib_array_setn(arr, 0, &v0);
-    ASSERT_TRUE(rc == IB_OK) << "ib_array_get() failed - rc != IB_EINVAL";
+    ASSERT_EQ(IB_OK, rc);
     rc = ib_array_get(arr, 0, &val);
-    ASSERT_TRUE(rc == IB_OK) << "ib_array_get() failed - rc != IB_EINVAL";
-    ASSERT_TRUE(val != NULL) << "ib_array_get() failed - NULL value";
-    ASSERT_TRUE(*val == v0) << "ib_array_get() failed - wrong value";
-    ASSERT_TRUE(ib_array_size(arr) == 10) << "ib_array_create() failed - wrong size";
-    ASSERT_TRUE(ib_array_elements(arr) == 1) << "ib_array_create() failed - wrong number of elements";
+    ASSERT_EQ(IB_OK, rc);
+    ASSERT_TRUE(val);
+    ASSERT_EQ(v0, *val);
+    ASSERT_EQ(10UL, ib_array_size(arr));
+    ASSERT_EQ(1UL, ib_array_elements(arr));
 
     /* Should not extend. */
     rc = ib_array_setn(arr, 9, &v9);
-    ASSERT_TRUE(rc == IB_OK) << "ib_array_get() failed - rc != IB_EINVAL";
+    ASSERT_EQ(IB_OK, rc);
     rc = ib_array_get(arr, 9, &val);
-    ASSERT_TRUE(rc == IB_OK) << "ib_array_get() failed - rc != IB_EINVAL";
-    ASSERT_TRUE(val != NULL) << "ib_array_get() failed - NULL value";
-    ASSERT_TRUE(*val == v9) << "ib_array_get() failed - wrong value";
-    ASSERT_TRUE(ib_array_size(arr) == 10) << "ib_array_create() failed - wrong size";
-    ASSERT_TRUE(ib_array_elements(arr) == 10) << "ib_array_create() failed - wrong number of elements";
+    ASSERT_EQ(IB_OK, rc);
+    ASSERT_TRUE(val);
+    ASSERT_EQ(v9, *val);
+    ASSERT_EQ(10UL, ib_array_size(arr));
+    ASSERT_EQ(10UL, ib_array_elements(arr));
 
     /* Should be null if unset. */
     rc = ib_array_get(arr, 5, &val);
-    ASSERT_TRUE(rc == IB_OK) << "ib_array_get() failed - rc != IB_EINVAL";
-    ASSERT_TRUE(val == NULL) << "ib_array_get() failed - not NULL value";
-    ASSERT_TRUE(ib_array_size(arr) == 10) << "ib_array_create() failed - wrong size";
-    ASSERT_TRUE(ib_array_elements(arr) == 10) << "ib_array_create() failed - wrong number of elements";
+    ASSERT_EQ(IB_OK, rc);
+    ASSERT_FALSE(val);
+    ASSERT_EQ(10UL, ib_array_size(arr));
+    ASSERT_EQ(10UL, ib_array_elements(arr));
 
     /* Should extend once. */
     rc = ib_array_setn(arr, 10, &v10);
-    ASSERT_TRUE(rc == IB_OK) << "ib_array_get() failed - rc != IB_EINVAL";
+    ASSERT_EQ(IB_OK, rc);
     rc = ib_array_get(arr, 10, &val);
-    ASSERT_TRUE(rc == IB_OK) << "ib_array_get() failed - rc != IB_EINVAL";
-    ASSERT_TRUE(val != NULL) << "ib_array_get() failed - NULL value";
-    ASSERT_TRUE(*val == v10) << "ib_array_get() failed - wrong value";
-    ASSERT_TRUE(ib_array_size(arr) == 20) << "ib_array_create() failed - wrong size";
-    ASSERT_TRUE(ib_array_elements(arr) == 11) << "ib_array_create() failed - wrong number of elements";
+    ASSERT_EQ(IB_OK, rc);
+    ASSERT_TRUE(val);
+    ASSERT_EQ(v10, *val);
+    ASSERT_EQ(20UL, ib_array_size(arr));
+    ASSERT_EQ(11UL, ib_array_elements(arr));
 
     /* Should extend to max. */
     rc = ib_array_setn(arr, 99, &v99);
-    ASSERT_TRUE(rc == IB_OK) << "ib_array_get() failed - rc != IB_EINVAL";
+    ASSERT_EQ(IB_OK, rc);
     rc = ib_array_get(arr, 99, &val);
-    ASSERT_TRUE(rc == IB_OK) << "ib_array_get() failed - rc != IB_EINVAL";
-    ASSERT_TRUE(val != NULL) << "ib_array_get() failed - NULL value";
-    ASSERT_TRUE(*val == v99) << "ib_array_get() failed - wrong value";
-    ASSERT_TRUE(ib_array_size(arr) == 100) << "ib_array_create() failed - wrong size";
-    ASSERT_TRUE(ib_array_elements(arr) == 100) << "ib_array_create() failed - wrong number of elements";
+    ASSERT_EQ(IB_OK, rc);
+    ASSERT_TRUE(val);
+    ASSERT_EQ(v99, *val);
+    ASSERT_EQ(100UL, ib_array_size(arr));
+    ASSERT_EQ(100UL, ib_array_elements(arr));
 
     /* Should reallocate extents. */
     rc = ib_array_setn(arr, 100, &v100);
-    ASSERT_TRUE(rc == IB_OK) << "ib_array_get() failed - rc != IB_EINVAL";
+    ASSERT_EQ(IB_OK, rc);
     rc = ib_array_get(arr, 100, &val);
-    ASSERT_TRUE(rc == IB_OK) << "ib_array_get() failed - rc != IB_EINVAL";
-    ASSERT_TRUE(val != NULL) << "ib_array_get() failed - NULL value";
-    ASSERT_TRUE(*val == v100) << "ib_array_get() failed - wrong value";
-    ASSERT_TRUE(ib_array_size(arr) == 110) << "ib_array_create() failed - wrong size";
-    ASSERT_TRUE(ib_array_elements(arr) == 101) << "ib_array_create() failed - wrong number of elements";
+    ASSERT_EQ(IB_OK, rc);
+    ASSERT_TRUE(val);
+    ASSERT_EQ(v100, *val);
+    ASSERT_EQ(110UL, ib_array_size(arr));
+    ASSERT_EQ(101UL, ib_array_elements(arr));
 
     /* Should reallocate extents 2 more times. */
     rc = ib_array_setn(arr, 1000, &v1000);
-    ASSERT_TRUE(rc == IB_OK) << "ib_array_get() failed - rc != IB_EINVAL";
+    ASSERT_EQ(IB_OK, rc);
     rc = ib_array_get(arr, 1000, &val);
-    ASSERT_TRUE(rc == IB_OK) << "ib_array_get() failed - rc != IB_EINVAL";
-    ASSERT_TRUE(val != NULL) << "ib_array_get() failed - NULL value";
-    ASSERT_TRUE(*val == v1000) << "ib_array_get() failed - wrong value";
-    ASSERT_TRUE(ib_array_size(arr) == 1010) << "ib_array_create() failed - wrong size";
-    ASSERT_TRUE(ib_array_elements(arr) == 1001) << "ib_array_create() failed - wrong number of elements";
+    ASSERT_EQ(IB_OK, rc);
+    ASSERT_TRUE(val);
+    ASSERT_EQ(v1000, *val);
+    ASSERT_EQ(1010UL, ib_array_size(arr));
+    ASSERT_EQ(1001UL, ib_array_elements(arr));
 
     /* Should reallocate extents many more times. */
     rc = ib_array_setn(arr, 1000000, &v1000000);
-    ASSERT_TRUE(rc == IB_OK) << "ib_array_get() failed - rc != IB_EINVAL";
+    ASSERT_EQ(IB_OK, rc);
     rc = ib_array_get(arr, 1000000, &val);
-    ASSERT_TRUE(rc == IB_OK) << "ib_array_get() failed - rc != IB_EINVAL";
-    ASSERT_TRUE(val != NULL) << "ib_array_get() failed - NULL value";
-    ASSERT_TRUE(*val == v1000000) << "ib_array_get() failed - wrong value";
-    ASSERT_TRUE(ib_array_size(arr) == 1000010) << "ib_array_create() failed - wrong size";
-    ASSERT_TRUE(ib_array_elements(arr) == 1000001) << "ib_array_create() failed - wrong number of elements";
-
-    ib_mpool_destroy(mp);
+    ASSERT_EQ(IB_OK, rc);
+    ASSERT_TRUE(val);
+    ASSERT_EQ(v1000000, *val);
+    ASSERT_EQ(1000010UL, ib_array_size(arr));
+    ASSERT_EQ(1000001UL, ib_array_elements(arr));
 }
 
 /// @test Test util array library - IB_ARRAY_LOOP()
-TEST(TestIBUtilArray, test_array_loop)
+TEST_F(TestIBUtilArray, test_array_loop)
 {
-    ib_mpool_t *mp;
     ib_array_t *arr;
     size_t nelts;
     size_t i;
@@ -184,32 +188,24 @@ TEST(TestIBUtilArray, test_array_loop)
          0,  1,  2,  3,  4,  5,  6,  7,  8,  9,
         10, 11, 12, 13, 14, 15, 16, 17, 18, 19
     };
-    
-    atexit(ib_shutdown);
-    rc = ib_initialize();
-    ASSERT_TRUE(rc == IB_OK) << "ib_initialize() failed - rc != IB_OK";
-    rc = ib_mpool_create(&mp, NULL, NULL);
-    ASSERT_TRUE(rc == IB_OK) << "ib_mpool_create() failed - rc != IB_OK";
-    
-    rc = ib_array_create(&arr, mp, 16, 8);
-    ASSERT_TRUE(rc == IB_OK) << "ib_array_create() failed - rc != IB_OK";
-    ASSERT_TRUE(arr != NULL) << "ib_array_create() failed - NULL value";
-    ASSERT_TRUE(ib_array_size(arr) == 16) << "ib_array_create() failed - wrong size";
-    ASSERT_TRUE(ib_array_elements(arr) == 0) << "ib_array_create() failed - wrong number of elements";
+        
+    rc = ib_array_create(&arr, m_pool, 16, 8);
+    ASSERT_EQ(IB_OK, rc);
+    ASSERT_TRUE(arr);
+    ASSERT_EQ(16UL, ib_array_size(arr));
+    ASSERT_EQ(0UL, ib_array_elements(arr));
 
     for (i = 0; i < (sizeof(init)/sizeof(int)); i++) {
         rc = ib_array_setn(arr, i, init + i);
-        ASSERT_TRUE(rc == IB_OK) << "ib_array_setn() failed - rc != IB_OK";
+        ASSERT_EQ(IB_OK, rc);
     }
-    ASSERT_TRUE(ib_array_size(arr) == 32) << "ib_array_setn() failed - wrong size";
-    ASSERT_TRUE(ib_array_elements(arr) == 20) << "ib_array_setn() failed - wrong number of elements";
+    ASSERT_EQ(32UL, ib_array_size(arr));
+    ASSERT_EQ(20UL, ib_array_elements(arr));
     rc = ib_array_get(arr, 1, &val);
-    ASSERT_TRUE(rc == IB_OK) << "ib_array_get() failed - rc != IB_OK";
+    ASSERT_EQ(IB_OK, rc);
 
     IB_ARRAY_LOOP(arr, nelts, i, val) {
-        //ASSERT_TRUE(*val == init[i]) << "IB_ARRAY_LOOP() failed - wrong value at index=" << (int)i "/" << (int)(nelts - 1);
-        ASSERT_TRUE(*val == init[i]) << "IB_ARRAY_LOOP() failed - wrong value";
+        //ASSERT_EQ(init[i], *val);
+        ASSERT_EQ(init[i], *val);
     }
-
-    ib_mpool_destroy(mp);
 }
