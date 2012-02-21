2b68b1a88b4a37fa06f07c3ab50f525531401fb4
Christopher Alfeld <calfeld@qualys.com>
2012-02-21 11:04:39 -0600

    test_util_logformat: Clean up.

--- tests/test_util_logformat.cc
+++ tests/test_util_logformat.cc
@@ -24,127 +24,98 @@
 
 #include "ironbee_config_auto.h"
 
+#include <ironbee/types.h>
+#include <ironbee/mpool.h>
+#include <ironbee/util.h>
+#include <ironbee/logformat.h>
+
 #include "gtest/gtest.h"
 #include "gtest/gtest-spi.h"
 
-#include "util/util.c"
-#include "util/logformat.c"
-#include "util/mpool.c"
-#include "util/debug.c"
-
+#include <stdexcept>
 
 /* -- Tests -- */
 
-/// @test Test util logformat library - ib_logformat_create() and *_set()
-TEST(TestIBUtilLogformat, test_logformat_create_and_set)
+class TestIBUtilLogformat : public ::testing::Test
 {
-    ib_mpool_t *mp;
+public:
+    TestIBUtilLogformat()
+    {
+        ib_status_t rc;
+        
+        ib_initialize();
+        rc = ib_mpool_create(&m_pool, NULL, NULL);
+        if (rc != IB_OK) {
+            throw std::runtime_error("Could not create mpool.");
+        }
+    }
+    
+    ~TestIBUtilLogformat()
+    {
+        ib_shutdown();        
+    }
+    
+protected:
+    ib_mpool_t* m_pool;
+};
+
+/// @test Test util logformat library - ib_logformat_create() and *_set()
+TEST_F(TestIBUtilLogformat, test_logformat_create_and_set)
+{
     ib_status_t rc;
-    
-    atexit(ib_shutdown);
-    rc = ib_initialize();
-    ASSERT_TRUE(rc == IB_OK) << "ib_initialize() failed - rc != IB_OK";
-    rc = ib_mpool_create(&mp, NULL, NULL);
-    ASSERT_TRUE(rc == IB_OK) << "ib_mpool_create() failed - rc != IB_OK";
-    
+ 
     ib_logformat_t *lf = NULL;
     
-    rc = ib_logformat_create(mp, &lf);
-    ASSERT_TRUE(rc == IB_OK) << "ib_logformat_create() failed - rc != IB_OK";
+    rc = ib_logformat_create(m_pool, &lf);
+    ASSERT_EQ(IB_OK, rc);
 
     rc = ib_logformat_set(lf, IB_LOGFORMAT_DEFAULT);
-    ASSERT_TRUE(rc == IB_OK) << "ib_logformat_set() failed - rc != IB_OK";
-    ASSERT_TRUE(strncmp(lf->fields, "ThaSstf", 7) == 0) << "ib_logformat_set()"
-                            " failed - fields string (parsed) should be "
-                            "ThaSstf, got " << lf->fields;
+    ASSERT_EQ(IB_OK, rc);
+    ASSERT_EQ(0, strncmp(lf->fields, "ThaSstf", 7));
+    ASSERT_EQ(7, lf->field_cnt);
+    ASSERT_EQ(6, lf->literal_cnt);
+    ASSERT_FALSE(lf->literals[6]);
+    ASSERT_EQ(0, lf->literal_starts);
 
-    ASSERT_TRUE(lf->field_cnt == 7) << "ib_logformat_set()"
-                            " failed - should have " << lf->field_cnt <<
-                            "fields, got " << lf->field_cnt;
-
-    ASSERT_TRUE(lf->literal_cnt == 6) << "ib_logformat_set()"
-                            " failed - should have " << lf->literal_cnt <<
-                            "literals, got " << lf->literal_cnt;
-    ASSERT_TRUE(lf->literals[6] == NULL) << "ib_logformat_set()"
-                            " failed - should be NULL";
-
-    ASSERT_TRUE(lf->literal_starts == 0) << "ib_logformat_set()"
-                            " failed - should start with a field (because of "
-                            "the default format string";
     int i = 0;
     /* All the literal strings between fields are spaces, because of the
        default format string */
 
     /* We DO want to check for '\0' as well as the data itself */
-    ASSERT_TRUE(memcmp(lf->literals[i++], " ", 2) == 0) << "ib_logformat_set()"
-                            " failed - wrong literal string here";
-
-    ASSERT_TRUE(memcmp(lf->literals[i++], " ", 2) == 0) << "ib_logformat_set()"
-                            " failed - wrong literal string here";
-    ASSERT_TRUE(memcmp(lf->literals[i++], " ", 2) == 0) << "ib_logformat_set()"
-                            " failed - wrong literal string here";
-    ASSERT_TRUE(memcmp(lf->literals[i++], " ", 2) == 0) << "ib_logformat_set()"
-                            " failed - wrong literal string here";
-    ASSERT_TRUE(memcmp(lf->literals[i++], " ", 2) == 0) << "ib_logformat_set()"
-                            " failed - wrong literal string here";
-    ASSERT_TRUE(memcmp(lf->literals[i++], " ", 2) == 0) << "ib_logformat_set()"
-                            " failed - wrong literal string here";
-
-    ib_mpool_destroy(mp);
+    ASSERT_EQ(0, memcmp(lf->literals[i++], " ", 2));
+    ASSERT_EQ(0, memcmp(lf->literals[i++], " ", 2));
+    ASSERT_EQ(0, memcmp(lf->literals[i++], " ", 2));
+    ASSERT_EQ(0, memcmp(lf->literals[i++], " ", 2));
+    ASSERT_EQ(0, memcmp(lf->literals[i++], " ", 2));
+    ASSERT_EQ(0, memcmp(lf->literals[i++], " ", 2));
 }
 
 /// @test Test util logformat library - ib_logformat_create() and *_set()
-TEST(TestIBUtilLogformat, test_logformat_set)
+TEST_F(TestIBUtilLogformat, test_logformat_set)
 {
-    ib_mpool_t *mp;
     ib_status_t rc;
-    
-    atexit(ib_shutdown);
-    rc = ib_initialize();
-    ASSERT_TRUE(rc == IB_OK) << "ib_initialize() failed - rc != IB_OK";
-    rc = ib_mpool_create(&mp, NULL, NULL);
-    ASSERT_TRUE(rc == IB_OK) << "ib_mpool_create() failed - rc != IB_OK";
     
     ib_logformat_t *lf = NULL;
     
-    rc = ib_logformat_create(mp, &lf);
-    ASSERT_TRUE(rc == IB_OK) << "ib_logformat_create() failed - rc != IB_OK";
+    rc = ib_logformat_create(m_pool, &lf);
+    ASSERT_EQ(IB_OK, rc);
 
     rc = ib_logformat_set(lf, (char *)"Myformat %S %h %s %f end");
-    ASSERT_TRUE(rc == IB_OK) << "ib_logformat_set() failed - rc != IB_OK";
-    ASSERT_TRUE(strncmp(lf->fields, "Shsf", 4) == 0) << "ib_logformat_set()"
-                            " failed - fields string (parsed) should be "
-                            "ThaSstf, got " << lf->fields;
+    ASSERT_EQ(IB_OK, rc);
+    ASSERT_EQ(0, strncmp(lf->fields, "Shsf", 4));
+    ASSERT_EQ(4, lf->field_cnt);
+    ASSERT_EQ(5, lf->literal_cnt);
+    ASSERT_FALSE(lf->literals[5]);
+    ASSERT_EQ(1, lf->literal_starts);
 
-    ASSERT_TRUE(lf->field_cnt == 4) << "ib_logformat_set()"
-                            " failed - should have " << lf->field_cnt <<
-                            "fields, got " << lf->field_cnt;
-
-    ASSERT_TRUE(lf->literal_cnt == 5) << "ib_logformat_set()"
-                            " failed - should have " << lf->literal_cnt <<
-                            "literals, got " << lf->literal_cnt;
-
-    ASSERT_TRUE(lf->literals[5] == NULL) << "ib_logformat_set()"
-                            " failed - should be NULL";
-
-    ASSERT_TRUE(lf->literal_starts == 1) << "ib_logformat_set()"
-                            " failed - should start with a literal";
     int i = 0;
     /* All the literal strings between fields are spaces, because of the
        default format string */
 
     /* We DO want to check for '\0' as well as the data itself */
-    ASSERT_TRUE(memcmp(lf->literals[i++], "Myformat ", 9) == 0) << "ib_logformat_set()"
-                            " failed - wrong literal string here";
-
-    ASSERT_TRUE(memcmp(lf->literals[i++], " ", 2) == 0) << "ib_logformat_set()"
-                            " failed - wrong literal string here";
-    ASSERT_TRUE(memcmp(lf->literals[i++], " ", 2) == 0) << "ib_logformat_set()"
-                            " failed - wrong literal string here";
-    ASSERT_TRUE(memcmp(lf->literals[i++], " ", 2) == 0) << "ib_logformat_set()"
-                            " failed - wrong literal string here";
-    ASSERT_TRUE(memcmp(lf->literals[i++], " end", 5) == 0) << "ib_logformat_set()"
-                            " failed - wrong literal string here";
-
-    ib_mpool_destroy(mp);
+    ASSERT_EQ(0, memcmp(lf->literals[i++], "Myformat ", 9));
+    ASSERT_EQ(0, memcmp(lf->literals[i++], " ", 2));
+    ASSERT_EQ(0, memcmp(lf->literals[i++], " ", 2));
+    ASSERT_EQ(0, memcmp(lf->literals[i++], " ", 2));
+    ASSERT_EQ(0, memcmp(lf->literals[i++], " end", 5));
 }
