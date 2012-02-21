2725dd5179f29b3483c2cef34472bba2be0efda1
Christopher Alfeld <calfeld@qualys.com>
2012-02-21 11:15:22 -0600

    test_util_field: Clean up.

--- tests/test_util_field.cc
+++ tests/test_util_field.cc
@@ -24,65 +24,83 @@
 
 #include "ironbee_config_auto.h"
 
+#include <ironbee/field.h>
+#include <ironbee/util.h>
+#include <ironbee/mpool.h>
+#include <ironbee/bytestr.h>
+
 #include "gtest/gtest.h"
 #include "gtest/gtest-spi.h"
 
-#include "util/field.c"
+#include <stdexcept>
 
+class TestIBUtilField : public ::testing::Test
+{
+public:
+    TestIBUtilField()
+    {
+        ib_status_t rc;
+        
+        ib_initialize();
+        rc = ib_mpool_create(&m_pool, NULL, NULL);
+        if (rc != IB_OK) {
+            throw std::runtime_error("Could not create mpool.");
+        }
+    }
+    
+    ~TestIBUtilField()
+    {
+        ib_shutdown();        
+    }
+    
+protected:
+    ib_mpool_t* m_pool;
+};
 
 /* -- Tests -- */
 
 /// @test Test util field library - ib_field_create() ib_field_create_ex()
-TEST(TestIBUtilField, test_field_create)
+TEST_F(TestIBUtilField, test_field_create)
 {
-    ib_mpool_t *mp;
     ib_field_t *f;
     ib_status_t rc;
     const char *nulstrval = "TestValue";
     ib_num_t numval = 5;
     ib_bytestr_t *bytestrval;
 
-    atexit(ib_shutdown);
-    rc = ib_initialize();
-    ASSERT_TRUE(rc == IB_OK) << "ib_initialize() failed - rc != IB_OK";
-    rc = ib_mpool_create(&mp, NULL, NULL);
-    ASSERT_TRUE(rc == IB_OK) << "ib_mpool_create() failed - rc != IB_OK";
+    rc = ib_field_create(&f, m_pool, "test_nulstr", IB_FTYPE_NULSTR, &nulstrval);
+    ASSERT_EQ(IB_OK, rc);
+    ASSERT_TRUE(f);
+    ASSERT_EQ(11UL, f->nlen);
+    ASSERT_EQ(0, memcmp("test_nulstr", f->name, 11));
 
-    rc = ib_field_create(&f, mp, "test_nulstr", IB_FTYPE_NULSTR, &nulstrval);
-    ASSERT_TRUE(rc == IB_OK) << "ib_field_create() NULSTR failed - rc != IB_OK";
-    ASSERT_TRUE(f != NULL) << "ib_field_create() NULSTR failed - NULL value";
-    ASSERT_TRUE(f->nlen == 11) << "ib_field_create() NULSTR failed - incorrect nlen";
-    ASSERT_TRUE(memcmp("test_nulstr", f->name, 11) == 0) << "ib_field_create() NULSTR failed - wrong name";
+    rc = ib_field_create(&f, m_pool, "test_num", IB_FTYPE_NUM, &numval);
+    ASSERT_EQ(IB_OK, rc);
+    ASSERT_TRUE(f);
+    ASSERT_EQ(8UL, f->nlen);
+    ASSERT_EQ(0, memcmp("test_num", f->name, 8));
 
-    rc = ib_field_create(&f, mp, "test_num", IB_FTYPE_NUM, &numval);
-    ASSERT_TRUE(rc == IB_OK) << "ib_field_create() NUM failed - rc != IB_OK";
-    ASSERT_TRUE(f != NULL) << "ib_field_create() NUM failed - NULL value";
-    ASSERT_TRUE(f->nlen == 8) << "ib_field_create() NUM failed - incorrect nlen";
-    ASSERT_TRUE(memcmp("test_num", f->name, 8) == 0) << "ib_field_create() NUM failed - wrong name";
+    rc = ib_bytestr_dup_mem(&bytestrval, m_pool, (uint8_t *)nulstrval, strlen(nulstrval));
+    ASSERT_EQ(IB_OK, rc);
+    ASSERT_TRUE(f);
 
-    rc = ib_bytestr_dup_mem(&bytestrval, mp, (uint8_t *)nulstrval, strlen(nulstrval));
-    ASSERT_TRUE(rc == IB_OK) << "ib_bytestr_dup_mem() failed - rc != IB_OK";
-    ASSERT_TRUE(f != NULL) << "ib_bytestr_dup_mem() failed - NULL value";
+    rc = ib_field_create(&f, m_pool, "test_bytestr", IB_FTYPE_BYTESTR, &bytestrval);
+    ASSERT_EQ(IB_OK, rc);
+    ASSERT_TRUE(f);
+    ASSERT_EQ(12UL, f->nlen);
+    ASSERT_EQ(0, memcmp("test_bytestr", f->name, 12));
 
-    rc = ib_field_create(&f, mp, "test_bytestr", IB_FTYPE_BYTESTR, &bytestrval);
-    ASSERT_TRUE(rc == IB_OK) << "ib_field_create() BYTESTR failed - rc != IB_OK";
-    ASSERT_TRUE(f != NULL) << "ib_field_create() BYTESTR failed - NULL value";
-    ASSERT_TRUE(f->nlen == 12) << "ib_field_create() BYTESTR failed - incorrect nlen";
-    ASSERT_TRUE(memcmp("test_bytestr", f->name, 12) == 0) << "ib_field_create() BYTESTR failed - wrong name";
+    rc = ib_field_create_ex(&f, m_pool, "test_nulstr_ex", 14, IB_FTYPE_NULSTR, &nulstrval);
+    ASSERT_EQ(IB_OK, rc);
+    ASSERT_TRUE(f);
 
-    rc = ib_field_create_ex(&f, mp, "test_nulstr_ex", 14, IB_FTYPE_NULSTR, &nulstrval);
-    ASSERT_TRUE(rc == IB_OK) << "ib_field_create_ex() NULSTR failed - rc != IB_OK";
-    ASSERT_TRUE(f != NULL) << "ib_field_create_ex() NULSTR failed - NULL value";
+    rc = ib_field_create_ex(&f, m_pool, "test_num_ex", 11, IB_FTYPE_NUM, &numval);
+    ASSERT_EQ(IB_OK, rc);
+    ASSERT_TRUE(f);
 
-    rc = ib_field_create_ex(&f, mp, "test_num_ex", 11, IB_FTYPE_NUM, &numval);
-    ASSERT_TRUE(rc == IB_OK) << "ib_field_create_ex() NUM failed - rc != IB_OK";
-    ASSERT_TRUE(f != NULL) << "ib_field_create_ex() NUM failed - NULL value";
-
-    rc = ib_field_create_ex(&f, mp, "test_bytestr_ex", 15, IB_FTYPE_BYTESTR, &bytestrval);
-    ASSERT_TRUE(rc == IB_OK) << "ib_field_create() BYTESTR failed - rc != IB_OK";
-    ASSERT_TRUE(f != NULL) << "ib_field_create() BYTESTR failed - NULL value";
-
-    ib_mpool_destroy(mp);
+    rc = ib_field_create_ex(&f, m_pool, "test_bytestr_ex", 15, IB_FTYPE_BYTESTR, &bytestrval);
+    ASSERT_EQ(IB_OK, rc);
+    ASSERT_TRUE(f);
 }
 
 // Globals used to test if dyn_get caching is working
@@ -122,26 +140,19 @@ static void *dyn_get_cached(ib_field_t *f,
 }
 
 ///@test Test util field library - ib_field_dyn_register_get()
-TEST(TestIBUtilField, test_dyn_field)
+TEST_F(TestIBUtilField, test_dyn_field)
 {
-    ib_mpool_t *mp;
     ib_field_t *dynf;
     ib_field_t *cdynf;
     ib_status_t rc;
     const char *fval;
 
-    atexit(ib_shutdown);
-    rc = ib_initialize();
-    ASSERT_TRUE(rc == IB_OK) << "ib_initialize() failed - rc != IB_OK";
-    rc = ib_mpool_create(&mp, NULL, NULL);
-    ASSERT_TRUE(rc == IB_OK) << "ib_mpool_create() failed - rc != IB_OK";
-
     /* Create a field with no initial value. */
-    rc = ib_field_create(&dynf, mp, "test_dynf", IB_FTYPE_NULSTR, NULL);
-    ASSERT_TRUE(rc == IB_OK) << "ib_field_create() NULSTR failed - rc != IB_OK";
-    ASSERT_TRUE(dynf != NULL) << "ib_field_create() NULSTR failed - NULL value";
-    ASSERT_TRUE(dynf->nlen == 9) << "ib_field_create() NULSTR failed - incorrect nlen";
-    ASSERT_TRUE(memcmp("test_dynf", dynf->name, 9) == 0) << "ib_field_create() NULSTR failed - wrong name";
+    rc = ib_field_create(&dynf, m_pool, "test_dynf", IB_FTYPE_NULSTR, NULL);
+    ASSERT_EQ(IB_OK, rc);
+    ASSERT_TRUE(dynf);
+    ASSERT_EQ(9UL, dynf->nlen);
+    ASSERT_EQ(0, memcmp("test_dynf", dynf->name, 9));
 
     /* Make it a dynamic field which calls dyn_get() with "dynf" as the data. */
     ib_field_dyn_register_get(dynf, dyn_get);
@@ -149,21 +160,21 @@ TEST(TestIBUtilField, test_dyn_field)
 
     /* Get the value from the dynamic field. */
     fval = ib_field_value_nulstr_ex(dynf, (void *)"fetch1", 6);
-    ASSERT_TRUE((fval != NULL) && (strcmp("testval_dynf_fetch1_call01", fval) == 0)) << "bad dynamic field value: " << fval;
+    ASSERT_TRUE((fval != NULL) && (strcmp("testval_dynf_fetch1_call01", fval) == 0));
 
     /* Get the value from the dynamic field again. */
     fval = ib_field_value_nulstr_ex(dynf, (void *)"fetch2", 6);
-    ASSERT_TRUE((fval != NULL) && (strcmp("testval_dynf_fetch2_call02", fval) == 0)) << "bad dynamic field value - not incremented: " << fval;
+    ASSERT_TRUE( (fval != NULL) && (strcmp("testval_dynf_fetch2_call02", fval)) == 0);
 
     /* Reset call counter. */
     g_dyn_call_count = 0;
 
     /* Create another field with no initial value. */
-    rc = ib_field_create(&cdynf, mp, "test_cdynf", IB_FTYPE_NULSTR, NULL);
-    ASSERT_TRUE(rc == IB_OK) << "ib_field_create() NULSTR failed - rc != IB_OK";
-    ASSERT_TRUE(cdynf != NULL) << "ib_field_create() NULSTR failed - NULL value";
-    ASSERT_TRUE(cdynf->nlen == 10) << "ib_field_create() NULSTR failed - incorrect nlen";
-    ASSERT_TRUE(memcmp("test_cdynf", cdynf->name, 10) == 0) << "ib_field_create() NULSTR failed - wrong name";
+    rc = ib_field_create(&cdynf, m_pool, "test_cdynf", IB_FTYPE_NULSTR, NULL);
+    ASSERT_EQ(IB_OK, rc);
+    ASSERT_TRUE(cdynf);
+    ASSERT_EQ(10UL, cdynf->nlen);
+    ASSERT_EQ(0, memcmp("test_cdynf", cdynf->name, 10));
 
     /* Make it a dynamic field which calls dyn_get_cached() with "cdynf" as the data. */
     ib_field_dyn_register_get(cdynf, dyn_get_cached);
@@ -171,11 +182,9 @@ TEST(TestIBUtilField, test_dyn_field)
 
     /* Get the value from the dynamic field. */
     fval = ib_field_value_nulstr_ex(cdynf, (void *)"fetch1", 6);
-    ASSERT_TRUE((fval != NULL) && (strcmp("testval_cdynf_fetch1_call01", fval) == 0)) << "bad dynamic field value: " << fval;
+    ASSERT_TRUE((fval != NULL) && (strcmp("testval_cdynf_fetch1_call01", fval) == 0));
 
     /* Get the value from the dynamic field again. */
     fval = ib_field_value_nulstr_ex(cdynf, (void *)"fetch2", 6);
-    ASSERT_TRUE((fval != NULL) && (strcmp("testval_cdynf_fetch1_call01", fval) == 0)) << "bad dynamic field value - not cached: " << fval;
-
-    ib_mpool_destroy(mp);
+    ASSERT_TRUE((fval != NULL) && (strcmp("testval_cdynf_fetch1_call01", fval) == 0));
 }
