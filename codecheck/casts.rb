70630fdc263fcf4a65c55fd4b8a694c6eeaee7d3
Christopher Alfeld <calfeld@qualys.com>
2012-03-29 13:52:05 -0500

    codecheck/casts.rb: Add script to look for casts that don't match CODESTYLE.

---
+++ codecheck/casts.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+$:.unshift(File.dirname(__FILE__))
+
+require 'all-code'
+
+all_ironbee_code do |path|
+    IO.foreach(path) do |line|
+        if line =~ %r{\([a-z_ ]*[a-z_]\*\)}
+            puts "#{path}:#{$.} #{line.chomp}"
+        end
+    end
+end
