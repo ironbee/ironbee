5f5fe5852c56ae5f0b5ee93c12bde5e117e16b38
Christopher Alfeld <calfeld@qualys.com>
2012-04-25 14:41:42 -0500

    IronBee++: Add convenience all.hpp header to include all headers.

---
+++ include/ironbeepp/all.hpp
@@ -0,0 +1,57 @@
+/*****************************************************************************
+ * Licensed to Qualys, Inc. (QUALYS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * QUALYS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ****************************************************************************/
+
+/**
+ * @file
+ * @brief IronBee++ &mdash; All IronBee++ Headers
+ *
+ * @author Christopher Alfeld <calfeld@qualys.com>
+ */
+
+#ifndef __IBPP__ALL__
+#define __IBPP__ALL__
+
+#include<ironbeepp/apidoc.hpp>
+#include<ironbeepp/byte_string.hpp>
+#include<ironbeepp/clock.hpp>
+#include<ironbeepp/common_semantics.hpp>
+#include<ironbeepp/configuration_directives.hpp>
+#include<ironbeepp/configuration_map.hpp>
+#include<ironbeepp/configuration_parser.hpp>
+#include<ironbeepp/connection.hpp>
+#include<ironbeepp/connection_data.hpp>
+#include<ironbeepp/context.hpp>
+#include<ironbeepp/engine.hpp>
+#include<ironbeepp/exception.hpp>
+#include<ironbeepp/field.hpp>
+#include<ironbeepp/hooks.hpp>
+#include<ironbeepp/internal/catch.hpp>
+#include<ironbeepp/internal/data.hpp>
+#include<ironbeepp/internal/throw.hpp>
+#include<ironbeepp/list.hpp>
+#include<ironbeepp/memory_pool.hpp>
+#include<ironbeepp/module.hpp>
+#include<ironbeepp/module_bootstrap.hpp>
+#include<ironbeepp/module_delegate.hpp>
+#include<ironbeepp/parsed_name_value.hpp>
+#include<ironbeepp/parsed_request_line.hpp>
+#include<ironbeepp/parsed_response_line.hpp>
+#include<ironbeepp/site.hpp>
+#include<ironbeepp/transaction.hpp>
+#include<ironbeepp/transaction_data.hpp>
+
+#endif
