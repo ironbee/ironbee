aabc4d5fbbad497762ee0d5b37e3432fe5e21c70
Christopher Alfeld <calfeld@qualys.com>
2012-05-02 09:38:47 -0500

    CodeCheck: Added script to check for missing licenses.

---
+++ codecheck/license.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+SKIP = [
+    %r{^cpp/},
+    %r{config-parser.c}
+]
+
+$:.unshift(File.dirname(__FILE__))
+
+require 'all-code'
+
+all_ironbee_code do |path|
+    next if SKIP.find {|x| path =~ x}
+    have_license = false
+    IO.foreach(path) do |line|
+        if line =~ %r{Licensed to Qualys, Inc.}
+            have_license = true
+            break
+        elsif line =~ /^\s*$/
+            break
+        end
+    end
+    
+    puts path if ! have_license
+end
