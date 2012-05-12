3756675d91a417d79ed2566037fa6d168f62c3f2
Brian Rectanus <brectanus@qualys.com>
2012-05-11 22:21:50 -0500

    Squashed 'docs/docbook/' content from commit 98184ed
    git-subtree-dir: docs/docbook
    git-subtree-split: 98184ed6a1333abb5c73567c93c4ce1042fa2ac7

---
+++ manual/generate-docs.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+HOME=.
+FOP=$HOME/fop-1.0/fop
+XMLINPUT=ironbee-reference-manual.xml
+OUTPUT=./output
+VERSION="0.2.0"
+
+# Generate a clean directory structure
+rm -rf $OUTPUT
+mkdir $OUTPUT
+mkdir $OUTPUT/html
+mkdir $OUTPUT/html-chunked
+
+# Generate PDF
+$FOP -c $HOME/fop.xconf -xml $XMLINPUT -xsl $HOME/pdf.xsl -pdf $OUTPUT/ironbee-reference-manual.pdf
+
+# Generate HTML/single
+$FOP -xml $XMLINPUT -xsl $HOME/html.xsl -foout /dev/null -param base.dir $OUTPUT/
+mv $OUTPUT/index.html $OUTPUT/ironbee-reference-manual.html
+# XXX Copy extra files to $OUTPUT
+
+# Generate HTML/chunked
+$FOP -xml $XMLINPUT -xsl $HOME/html-chunked.xsl -foout /dev/null -param base.dir $OUTPUT/html-chunked/
+# XXX Copy extra files to $OUTPUT/html-chunked/
+
+# Cover page
+cp $HOME/resources/index.html $OUTPUT/index.html
+perl -pi -e s/\\\$version/$VERSION/ $OUTPUT/index.html
+
