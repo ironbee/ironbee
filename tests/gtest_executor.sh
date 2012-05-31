eb231ee47c2422c759d4aa84d9945109ea9e09c2
Sam Baskinger <basking2@yahoo.com>
2012-05-31 13:22:46 -0500

    Adding missing unit test file and adding better error reporting from our gtest_executor.sh script.

--- tests/gtest_executor.sh
+++ tests/gtest_executor.sh
@@ -9,3 +9,9 @@ test_name=`basename "$test_prg"`
     --gtest_output=xml:"${test_name}_details.xml" \
     2> "${test_name}_stderr.log"
 
+rc=$?
+
+if [ $rc != 0 ]; then
+    echo "Test $test_prg exited non-zero. See ${test_name}_stderr.log for details."
+    exit $rc
+fi
