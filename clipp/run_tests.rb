e2f2a685fc2ac4dbdc67b14761988cc764721c2c
Christopher Alfeld <calfeld@qualys.com>
2012-06-04 10:39:27 -0500

    clipp: Tests now properly fail if clipp exit's non-0; also improve output.

--- clipp/run_tests.rb
+++ clipp/run_tests.rb
@@ -3,8 +3,27 @@
 require 'erb'
 require 'tempfile'
 require 'tmpdir'
-require 'open3'
 
+def color_puts(code, s)
+    if STDOUT.isatty
+        puts "\033[#{code}m#{s}\033[0m"
+    else
+        puts s
+    end
+end
+   
+def green(s)
+    color_puts(32, s)
+end
+
+def yellow(s)
+    color_puts(33, s)
+end
+
+def red(s)
+    color_puts(31, s)
+end
+     
 TESTDIR = File.join(File.dirname(__FILE__), 'tests')
 FAIL_MESSAGES = [
     / (CONFIG_)?(ERROR|CRITICAL|ALERT|EMERGENCY)\s+-/
@@ -22,35 +41,35 @@ $clipp  = ARGV[0]
 config = ARGV[1]
 
 def run_clipp(config_path)
+    r, w = IO.pipe
+    
+    Process.fork do
+        r.close
+        STDOUT.reopen(w)
+        STDERR.reopen(w)
+        exec($clipp, '-c', config_path)
+    end
+    
+    w.close
+    
     result = nil
-    Open3.popen3($clipp, '-c', config_path) do |cin, cout, cerr, wt|
-        cin.close
-        
-        while true
-            reads = IO.select([cout, cerr])[0]
-            break if reads.find {|i| i.eof?}
-            
-            reads.each do |i|
-                line = i.gets
-                print line
-                if result.nil?
-                    is_fail  = ! FAIL_MESSAGES.find      {|x| x=~ line}.nil?
-                    is_white = ! WHITELIST_MESSAGES.find {|x| x=~ line}.nil?
-                    if is_fail && ! is_white
-                        result = "Failure message: #{line}"
-                    end 
-                end
-            end
+    r.each do |line|
+        print line
+        if result.nil?
+            is_fail  = ! FAIL_MESSAGES.find      {|x| x=~ line}.nil?
+            is_white = ! WHITELIST_MESSAGES.find {|x| x=~ line}.nil?
+            if is_fail && ! is_white
+                result = "Failure message: #{line}"
+            end 
         end
-        
-        cout.close
-        cerr.close
     end
-    exit_status = $?
-    if exit_status != 0 && result.nil?
-        result = "Non-zero exit status"
+    Process.wait
+    exitstatus = $?.exitstatus
+    if exitstatus != 0 && result.nil?
+        "Non-zero exit status: #{$?.to_s}"
+    else
+        result
     end
-    result
 end
 
 failure = false
@@ -66,15 +85,16 @@ Dir.glob('*.erb').each do |test|
         clipp_config.write(erb.result(binding))
     end
         
+    failure = false
     result = run_clipp(tmppath)
     if result
-        puts "#{base} FAIL #{result}"
+        red "#{base} FAIL #{result}"
         failure = true
     else 
-        puts "#{base} PASS"
+        green "#{base} PASS"
     end
     if failure
-        puts "Configuration at #{tmppath}"
+        yellow "Configuration at #{tmppath}"
     else
         File.unlink(tmppath) if ! failure
     end
