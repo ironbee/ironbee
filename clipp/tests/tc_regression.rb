4579e70e94c32357c34c78c8eab2964c4f28c343
Christopher Alfeld <calfeld@qualys.com>
2012-06-15 14:00:40 -0500

    clipp: Move old test to new infrastructure.

--- clipp/tests/tc_regression.rb
+++ clipp/tests/tc_regression.rb
@@ -5,4 +5,20 @@ class TestBasic < CLIPPTestCase
   def test_trivial
     clipp(input: "echo:foo")
   end
+
+  def test_body_before_header
+    clipp(input: "htp:body_before_header.t")
+  end
+  
+  def test_empty_header
+    clipp(input: "raw:empty_header.req,empty_header.resp")
+  end
+  
+  def test_http09
+    clipp(input: "raw:http09.req,http09.resp")
+  end
+  
+  def test_manyblank
+    clipp(input: "raw:manyblank.req,manyblank.resp")
+  end
 end
