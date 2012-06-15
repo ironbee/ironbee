9b6aecbf23009fb7b3a4e7697f9ba78d8811c53d
Christopher Alfeld <calfeld@qualys.com>
2012-06-15 15:00:10 -0500

    tc_regression.rb: Strip trailing whitespace.

--- clipp/tests/tc_regression.rb
+++ clipp/tests/tc_regression.rb
@@ -9,15 +9,15 @@ class TestBasic < CLIPPTestCase
   def test_body_before_header
     clipp(input: "htp:body_before_header.t")
   end
-  
+
   def test_empty_header
     clipp(input: "raw:empty_header.req,empty_header.resp")
   end
-  
+
   def test_http09
     clipp(input: "raw:http09.req,http09.resp")
   end
-  
+
   def test_manyblank
     clipp(input: "raw:manyblank.req,manyblank.resp")
   end
