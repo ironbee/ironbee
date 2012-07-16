4a94a8f26923de9f64f54b654bc5a7eff85ef842
Brian Rectanus <brectanus@qualys.com>
2012-07-16 11:18:57 -0500

    Revert "Populate fields in audit log"
    This reverts commit c26872f4055ac3de17a95adace021cea446a8da1.

--- engine/core_actions.c
+++ engine/core_actions.c
@@ -177,7 +177,6 @@ static ib_status_t act_event_execute(void *data,
     ib_status_t  rc;
     ib_logevent_t *event;
     const char *expanded;
-    ib_field_t *field;
 
     ib_log_debug_tx(tx, "Creating event via action");
 
@@ -243,27 +242,6 @@ static ib_status_t act_event_execute(void *data,
     /* Link to rule tags. */
     /// @todo Probably need to copy here
     event->tags = rule->meta.tags;
-
-    /* Populate fields */
-    if (ib_flags_any(rule->flags, IB_RULE_FLAG_NO_TGT) == false) {
-        rc = ib_data_get(tx->dpi, "FIELD_NAME_FULL", &field);
-        if ( (rc == IB_OK) && (field->type == IB_FTYPE_NULSTR) ) {
-            const char *name = NULL;
-            rc = ib_field_value(field, ib_ftype_nulstr_out(&name));
-            if (rc == IB_OK) {
-                ib_logevent_field_add(event, name);
-            }
-        }
-        else if ( (rc == IB_OK) && (field->type == IB_FTYPE_BYTESTR) ) {
-            const ib_bytestr_t *bs;
-            rc = ib_field_value(field, ib_ftype_bytestr_out(&bs));
-            if (rc == IB_OK) {
-                ib_logevent_field_add_ex(event,
-                                         (const char *)ib_bytestr_const_ptr(bs),
-                                         ib_bytestr_length(bs));
-            }
-        }
-    }
 
     /* Set the actions if appropriate */
     if (ib_tx_flags_isset(tx,
