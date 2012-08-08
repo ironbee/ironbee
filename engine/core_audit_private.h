d912be6bc4eb2f9c66ec7d8f8720e30c97fc5cc9
Christopher Alfeld <calfeld@qualys.com>
2012-08-08 13:43:27 -0500

    core_audit_private.h: Add missing LICENSE and File documentation.

--- engine/core_audit_private.h
+++ engine/core_audit_private.h
@@ -1,8 +1,33 @@
+/*****************************************************************************
+ * Licensed to Qualys, Inc. (QUALYS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * QUALYS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ****************************************************************************/
+
+/**
+ * @file
+ * @brief IronBee -- Core Audit Related Routines
+ *
+ * @author Sam Baskinger <sbaskinger@qualys.com>
+ */
+
 #ifndef _IB_CORE_AUDIT_PRIVATE_H_
 #define _IB_CORE_AUDIT_PRIVATE_H_
 
 #include <stdio.h>
 #include <ironbee/core.h>
+
 /* -- Audit Provider -- */
 
 /* Forward define this structure. */
@@ -105,4 +130,5 @@ ib_status_t core_audit_get_index_line(ib_provider_inst_t *lpi,
                                       int *line_size);
 
 ib_status_t core_audit_close(ib_provider_inst_t *lpi, ib_auditlog_t *log);
+
 #endif // _IB_CORE_AUDIT_PRIVATE_H_
