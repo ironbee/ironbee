91e72bc06b812f758446eec36c3924e4d9eeef66
Nick LeRoy <nleroy@qualys.com>
2012-08-22 09:02:58 -0500

    Reworked clock test to be more reliable

--- tests/test_util_clock.cc
+++ tests/test_util_clock.cc
@@ -45,15 +45,28 @@ double TvToSecs(const ib_timeval_t &tv)
     return tv.tv_sec + (tv.tv_usec * 1e-6);
 }
 
-bool CheckUsecDiff(int64_t diff, unsigned int usecs)
+double GetTime(void)
 {
-    int64_t limit = usecs ? usecs / 5 : 100000;
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return TvToSecs(tv);
+}
+
+bool CheckUsecDiff(int64_t t1, int64_t t2, unsigned int usecs)
+{
+    int64_t diff = (int64_t)llabs(t2 - t1);
+    int64_t mean = (int64_t)llabs( (t2 + t1) / 2);
+    int64_t limit = mean ? mean / 5 : 100000;
+
     return (diff >= 0) && (abs(diff - usecs) < limit);
 }
-bool CheckSecDiff(double diff, double secs, double limit=-1.0)
+bool CheckSecDiff(double t1, double t2, double secs, double limit=-1.0)
 {
+    double diff = fabs(t2 - t1);
+    double mean = fabs( (t1 + t2) * 0.5);
+
     if (limit < 0.0) {
-        limit = (secs > 0.0001) ? secs * 0.2 : 1e-5;
+        limit = (mean > 0.0001) ? mean * 0.2 : 1e-5;
     }
     return (diff >= 0.0) && (fabs(diff - secs) < limit);
 }
@@ -62,24 +75,28 @@ bool Compare(const struct timeval &tv,
              const ib_timeval_t &itv,
              double limit=-1.0)
 {
-    double secs = fabs(TvToSecs(tv) - TvToSecs(itv));
-    return CheckSecDiff(secs, 0, limit);
+    return CheckSecDiff(TvToSecs(tv), TvToSecs(itv), 0, limit);
 }
 
 bool CheckDelta(const ib_timeval_t &tv1,
                 const ib_timeval_t &tv2,
                 ib_time_t expected_usecs)
 {
-    double secs = TvToSecs(tv2) - TvToSecs(tv1);
-    return CheckSecDiff(secs, expected_usecs * 1e-6);
+    return CheckSecDiff(TvToSecs(tv1), TvToSecs(tv2), expected_usecs * 1e-6);
+}
+
+bool CheckDelta(const ib_timeval_t &tv1,
+                const ib_timeval_t &tv2,
+                double expected_secs)
+{
+    return CheckSecDiff(TvToSecs(tv1), TvToSecs(tv2), expected_secs);
 }
 
 bool CheckDelta(ib_time_t t1,
                 ib_time_t t2,
                 ib_time_t expected_usecs)
 {
-    int64_t diff = (int64_t)t2 - (int64_t)t1;
-    return CheckUsecDiff(diff, expected_usecs);
+    return CheckUsecDiff(t1, t2, expected_usecs);
 }
 
 TEST(TestClock, test_get_time)
@@ -136,26 +153,34 @@ TEST(TestClock, test_gettimeofday_diffs)
     ib_timeval_t tv2;
     unsigned int usecs;
     bool         rv;
+    double       t1;
+    double       t2;
 
     usecs = 100000;
     ib_clock_gettimeofday(&tv1);
+    t1 = GetTime( );
     usleep(usecs);
     ib_clock_gettimeofday(&tv2);
-    rv = CheckDelta(tv1, tv2, usecs);
+    t2 = GetTime( );
+    rv = CheckDelta(tv1, tv2, t2 - t1);
     ASSERT_TRUE(rv);
 
     usecs = 500000;
     ib_clock_gettimeofday(&tv1);
+    t1 = GetTime( );
     usleep(usecs);
     ib_clock_gettimeofday(&tv2);
-    rv = CheckDelta(tv1, tv2, usecs);
+    t2 = GetTime( );
+    rv = CheckDelta(tv1, tv2, t2 - t1);
     ASSERT_TRUE(rv);
 
     usecs = 1000000;
     ib_clock_gettimeofday(&tv1);
+    t1 = GetTime( );
     usleep(usecs);
     ib_clock_gettimeofday(&tv2);
-    rv = CheckDelta(tv1, tv2, usecs);
+    t2 = GetTime( );
+    rv = CheckDelta(tv1, tv2, t2 - t1);
     ASSERT_TRUE(rv);
 }
 
@@ -190,7 +215,7 @@ void TestTimestamp(bool relative, int seconds)
 TEST(TestClock, test_timestamp)
 {
     {
-        SCOPED_TRACE("Timestamp");
+        SCOPED_TRACE("test_timestamp");
         TestTimestamp(false, 0);
     }
 }
@@ -198,23 +223,23 @@ TEST(TestClock, test_timestamp)
 TEST(TestClock, test_relative_timestamp)
 {
     {
-        SCOPED_TRACE("RelativeTimestamp 0s");
+        SCOPED_TRACE("test_relative_timestamp: 0s");
         TestTimestamp(true, 0);
     }
     {
-        SCOPED_TRACE("RelativeTimestamp 1s");
+        SCOPED_TRACE("test_relative_timestamp: 1s");
         TestTimestamp(true, 1);
     }
     {
-        SCOPED_TRACE("RelativeTimestamp -1s");
+        SCOPED_TRACE("test_relative_timestamp: -1s");
         TestTimestamp(true, -1);
     }
     {
-        SCOPED_TRACE("RelativeTimestamp 60s");
+        SCOPED_TRACE("test_relative_timestamp: 60s");
         TestTimestamp(true, 60);
     }
     {
-        SCOPED_TRACE("RelativeTimestamp -60s");
+        SCOPED_TRACE("test_relative_timestamp: -60s");
         TestTimestamp(true, -60);
     }
 }
