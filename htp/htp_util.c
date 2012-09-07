ironbee/ironbee

commit b79a1e953173a1e8410b544fd0a887468c2096ea

htp_util.c: Fixed bug in htp_replace_hostname where it didn't actually replace the hostname if a port was present.

htp/htp_util.c
@@ -1599,6 +1599,9 @@ void htp_replace_hostname(htp_connp_t *connp, htp_uri_t *parsed_uri, bstr *hostn
         // TODO Handle whitespace around hostname
         htp_normalize_hostname_inplace(new_hostname);
 
+        if (parsed_uri->hostname != NULL) bstr_free(&parsed_uri->hostname);
+        parsed_uri->hostname = new_hostname;
+
         // Port
         int port = htp_parse_positive_integer_whitespace((unsigned char *) bstr_ptr(hostname) + colon + 1,
             bstr_len(hostname) - colon - 1, 10);

commit f748d24b6f898ceba59a0da10861517affb70b15

htp_util.c: Fix double free caused by b79a1e953173a1e8410b544fd0a887468c2096ea.
Resolves issue #23.

htp/htp_util.c
@@ -1601,7 +1601,8 @@ void htp_replace_hostname(htp_connp_t *connp, htp_uri_t *parsed_uri, bstr *hostn
 
         if (parsed_uri->hostname != NULL) bstr_free(&parsed_uri->hostname);
         parsed_uri->hostname = new_hostname;
-
+        parsed_uri->port_number = 0;
+        
         // Port
         int port = htp_parse_positive_integer_whitespace((unsigned char *) bstr_ptr(hostname) + colon + 1,
             bstr_len(hostname) - colon - 1, 10);
@@ -1614,8 +1615,6 @@ void htp_replace_hostname(htp_connp_t *connp, htp_uri_t *parsed_uri, bstr *hostn
                 // Port was specified in connection and is different from the TCP port
                 htp_log(connp, HTP_LOG_MARK, HTP_LOG_ERROR, 0, "Request server port=%d number differs from the actual TCP port=%d", port, connp->conn->local_port);
             } else {
-                if (parsed_uri->hostname != NULL) bstr_free(&parsed_uri->hostname);
-                parsed_uri->hostname = new_hostname;
                 parsed_uri->port_number = port;                
             }
         }