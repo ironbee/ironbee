2bcebf302f2d69c98b49a246773dfb283cfd46d2
Christopher Alfeld <calfeld@qualys.com>
2012-10-01 09:42:18 -0500

    automata: AC tests now use C++ AC generator rather than Ruby AC generator.

--- automata/tests/automata_test.rb
+++ automata/tests/automata_test.rb
@@ -1,10 +1,10 @@
 $:.unshift(File.dirname(File.dirname(File.expand_path(__FILE__))))
-require 'ac_generator'
 require 'set'
 
 module AutomataTest
   BENCH = File.join(ENV['abs_builddir'], "..", "bench")
   EC = File.join(ENV['abs_builddir'], "..", "ec")
+  ACGEN = File.join(ENV['abs_builddir'], "..", "ac_generator")
 
   # Returns map of word to ending position of word in input.
   def substrings(words, input)
@@ -38,14 +38,34 @@ module AutomataTest
     end
   end
 
+  def run_from_file(cmd, inpath, outpath)
+    child_pid = nil
+    File.open(inpath, "r") do |inf|
+      File.open(outpath, "w") do |outf|
+        child_pid = fork
+        if child_pid.nil?
+          STDIN.reopen(inf)
+          STDOUT.reopen(outf)
+          exec(*cmd)
+        end
+      end
+    end
+    Process.wait(child_pid)
+    assert($?.success?)
+  end
+
   def ac_test(words, text, prefix = "full_test")
     dir = "/tmp/automata_test_#{prefix}#{$$}"
     Dir.mkdir(dir)
     puts "Test files are in #{dir}"
-    automata_path = File.join(dir, "automata")
-    File.open(automata_path, "w") do |fp|
-      fp.print IronAutomata::aho_corasick(words)
+
+    words_path = File.join(dir, "words")
+    File.open(words_path, "w") do |fp|
+      fp.puts words.join("\n")
     end
+
+    automata_path = File.join(dir, "automata")
+    run_from_file([ACGEN], words_path, automata_path)
 
     input_path = File.join(dir, "input")
     File.open(input_path, "w") do |fp|
