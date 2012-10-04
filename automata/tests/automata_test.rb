a72424fc8e2cb596cc52fc3a44a19ac4bf55faba
Christopher Alfeld <calfeld@qualys.com>
2012-10-04 12:23:10 -0500

    automata/test: Fix bug in substrings that would not match a 1 word string at the end of the input.

--- automata/tests/automata_test.rb
+++ automata/tests/automata_test.rb
@@ -12,7 +12,7 @@ module AutomataTest
     l = input.length
     words.each do |word|
       i = 0
-      while i + word.length < l
+      while i + word.length <= l
         j = input.index(word, i)
         break if ! j
         result[word] << j + word.length
