75911b618229aad6971a0fdfae3082bc5ba9d35a
Christopher Alfeld <calfeld@qualys.com>
2012-12-13 13:31:44 -0600

    build: Cleanup use of CPPFLAGS vs. AM_CPPFLAGS.

--- build/common.mk
+++ build/common.mk
@@ -1,15 +1,15 @@
 # -Wno-unused-parameter is because IronBee is so callback heavy multiple
 # implements ignore one or more of their parameters.
 # In C++, we could ommit the name, but not in C.
-CPPFLAGS += @IB_DEBUG@ \
-            -I$(top_srcdir) \
-            -I$(top_srcdir)/include \
-            -I$(top_builddir)/include \
-            -I$(top_srcdir)/util \
-            -I$(top_srcdir)/engine \
-            -DMODULE_BASE_PATH=$(libdir) \
-            -DRULE_BASE_PATH=$(libdir) \
-            -Wno-unused-parameter
+AM_CPPFLAGS += @IB_DEBUG@ \
+             -I$(top_srcdir) \
+             -I$(top_srcdir)/include \
+             -I$(top_builddir)/include \
+             -I$(top_srcdir)/util \
+             -I$(top_srcdir)/engine \
+             -DMODULE_BASE_PATH=$(libdir) \
+             -DRULE_BASE_PATH=$(libdir) \
+             -Wno-unused-parameter
 
 # Alias for "check"
 test: check
