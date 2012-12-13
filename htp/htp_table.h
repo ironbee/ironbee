b3090543a05869983231330f59d23b582316931c
Ivan Ristic <ivanr@webkreator.com>
2012-12-13 10:27:30 +0000

    Tidy up.

--- htp/htp_table.h
+++ htp/htp_table.h
@@ -43,8 +43,6 @@ extern "C" {
 
 typedef struct htp_table_t htp_table_t;
 
-struct htp_table_t;
-
 /**
  * Add a new element to the table. The key will be copied, and the copy
  * managed by the table. The point of the element will be stored, but the
