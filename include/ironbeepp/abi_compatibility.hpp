5797aa75900ed1b240628ff95ba27cc889617b0f
Christopher Alfeld <calfeld@qualys.com>
2013-01-31 10:36:34 -0600

    release.h: Bump ABI number to reflect recent API changes.

--- include/ironbeepp/abi_compatibility.hpp
+++ include/ironbeepp/abi_compatibility.hpp
@@ -35,7 +35,7 @@
 namespace IronBee {
 
 // Update following line when IronBee ABI changes.
-#if IB_ABINUM!=2013011101
+#if IB_ABINUM!=2013013101
 #error "ABI mismatch between IronBee++ and IronBee"
 #endif
 
