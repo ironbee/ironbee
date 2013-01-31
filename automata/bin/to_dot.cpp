f900c0871244aeedaa142ac3dab34b97c84754ce
Christopher Alfeld <calfeld@qualys.com>
2013-01-31 12:25:57 -0600

    automata/to_dot: Fix (ignore) warning in older versions of boost.

--- automata/bin/to_dot.cpp
+++ automata/bin/to_dot.cpp
@@ -27,8 +27,15 @@
 
 #include <ironautomata/intermediate.hpp>
 
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-parameter"
+#endif
 #include <boost/foreach.hpp>
 #include <boost/format.hpp>
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
 #include <fstream>
 
 using namespace std;
