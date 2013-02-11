fd75b0b3ad319d1f14df67080f4ff82924f9b7fc
Nick Kew <nkew@qualys.com>
2013-02-11 09:45:39 +0000

    Handle header dependency in config; improve docs

--- servers/nginx/ngx_ironbee.h
+++ servers/nginx/ngx_ironbee.h
@@ -29,58 +29,76 @@
 #include <ironbee/provider.h>
 #include <ngx_http.h>
 
+/* HTTP statuses we'll support when Ironbee asks us to return them */
 #define STATUS_IS_ERROR(code) ( ((code) >= 200) && ((code) <  600) )
+
+/* Placeholder for generic conversion of an Ironbee error to an nginx error */
 #define IB2NG(x) x
 
+/* A struct used to track a connection across requests (required because
+ * there's no connection API in nginx).
+ */
 typedef struct ngxib_conn_t ngxib_conn_t;
 
+/* Buffering status for request/response bodies */
 typedef enum { IOBUF_NOBUF, IOBUF_DISCARD, IOBUF_BUFFER } iobuf_t;
 
+/* The main per-request record for the plugin */
 typedef struct ngxib_req_ctx {
-    ngx_http_request_t *r;
-    ngxib_conn_t *conn;
-    ib_tx_t *tx;
-    int status;
-    iobuf_t output_buffering;
-    ngx_chain_t *response_buf;
-    ngx_chain_t *response_ptr;
-    int body_done:1;
-    int body_wait:1;
-    int has_request_body:1;
-    int tested_request_body:1;
-    int output_filter_init:1;
-    int output_filter_done:1;
-    int hdrs_in:1;
-    int hdrs_out:1;
-    int start_response:1;
+    ngx_http_request_t *r;        /* The nginx request struct */
+    ngxib_conn_t *conn;           /* Connection tracking */
+    ib_tx_t *tx;                  /* The Ironbee request struct */
+    int status;                   /* Request status set by ironbee */
+    iobuf_t output_buffering;     /* Output buffer management */
+    ngx_chain_t *response_buf;    /* Output buffer management */
+    ngx_chain_t *response_ptr;    /* Output buffer management */
+    int body_done:1;              /* State flags */
+    int body_wait:1;              /* State flags */
+    int has_request_body:1;       /* State flags */
+    int tested_request_body:1;    /* State flags */
+    int output_filter_init:1;     /* State flags */
+    int output_filter_done:1;     /* State flags */
+    int hdrs_in:1;                /* State flags */
+    int hdrs_out:1;               /* State flags */
+    int start_response:1;         /* State flags */
 } ngxib_req_ctx;
 
+/* Determine whether a connection is known to Ironbee.  If yes, retrieve it;
+ * if no then initialise it and retreive it.
+ */
 ib_conn_t *ngxib_conn_get(ngxib_req_ctx *rctx, ib_engine_t *ib);
 
+/* Ironbee's callback to initialise its connection rec */
 ib_status_t ngxib_conn_init(ib_engine_t *ib,
                             ib_state_event_type_t event,
                             ib_conn_t *iconn,
                             void *cbdata);
 
-ib_server_t *ngxib_server(void);
-
-#if 0
-IB_PROVIDER_IFACE_TYPE(logger) *ngxib_logger_iface(void);
-#else
+/* Ironbee log function to write to nginx's error log */
 void ngxib_logger(const ib_engine_t *ib, ib_log_level_t level,
                   const char *file, int line, const char *fmt,
                   va_list ap, void *dummy);
+
+/* Dummy funvtion to set Ironbee log level */
 ib_log_level_t ngxib_loglevel(const ib_engine_t *ib, void *cbdata);
-#endif
+
+/* Set/retrieve global log descriptor.  A fudge for the absence of appdata
+ * pointer in ironbee logger API.
+ */
 ngx_log_t *ngxib_log(ngx_log_t *log);
 
-ngx_int_t ngxib_handler(ngx_http_request_t *r);
 
-extern ngx_module_t  ngx_ironbee_module;
+/* Misc symbols that need visibility across source files */
+extern ngx_module_t  ngx_ironbee_module;         /* The module struct */
+ngx_int_t ngxib_handler(ngx_http_request_t *r);  /* Handler for Request Data */
+ib_engine_t *ngxib_engine(void);                 /* The ironbee engine */
+ib_server_t *ngxib_server(void);                 /* The ironbee server */
 
-ib_engine_t *ngxib_engine(void);
 
-
+/* Return from a function that has set globals, ensuring those
+ * globals are tidied up after use.  An ugly but necessary hack.
+ * Would become completely untenable if a threaded nginx happens.
+ */
 #define cleanup_return(log) return ngxib_log(log),ngx_regex_malloc_done(),
 
 
