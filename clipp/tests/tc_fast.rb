2007e1011e52e353709345b6e4ddd52bd4bb729e
Christopher Alfeld <calfeld@qualys.com>
2013-02-18 11:06:56 -0600

    clipp tests: Add a bunch of fast tests, one of which is disabled pending RNS-192.

--- clipp/tests/tc_fast.rb
+++ clipp/tests/tc_fast.rb
@@ -9,11 +9,13 @@ class TestFast < Test::Unit::TestCase
     "FastAutomata \"#{TESTDIR}/fast_rules.txt.e\""
   ].join("\n")
 
-  REQUEST = "GET /foobar/a HTTP/1.1\nHost: foo.bar\n\n"
+  def make_request(s)
+    simple_hash("GET /#{s}/a HTTP/1.1\nHost: foo.bar\n\n")
+  end
 
   def test_without_fast
     clipp(
-      :input_hashes => [simple_hash(REQUEST)],
+      :input_hashes => [make_request('foobar')],
       :default_site_config => <<-EOS
         Rule REQUEST_URI @rx foobar id:1 phase:REQUEST_HEADER clipp_announce:basic_rule
       EOS
@@ -24,26 +26,86 @@ class TestFast < Test::Unit::TestCase
 
   def test_load
     clipp(
-      :input_hashes => [simple_hash(REQUEST)],
+      :input_hashes => [make_request('foobar')],
       :config => 'LoadModule "ibmod_fast.so"',
       :default_site_config => <<-EOS
         Rule REQUEST_URI @rx foobar id:1 phase:REQUEST_HEADER clipp_announce:basic_rule
       EOS
     )
-    puts clipp_log
     assert_no_issues
     assert_log_match /CLIPP ANNOUNCE: basic_rule/
   end
 
   def test_load_automata
     clipp(
-      :input_hashes => [simple_hash(REQUEST)],
+      :input_hashes => [make_request('foobar')],
       :config => CONFIG,
       :default_site_config => "Include \"#{TESTDIR}/fast_rules.txt\""
     )
-    puts clipp_log
     assert_no_issues
     assert_log_match /CLIPP ANNOUNCE: foobar/
   end
 
+  def test_multiple
+    clipp(
+      :input_hashes => [make_request('abcdef')],
+      :config => CONFIG,
+      :default_site_config => "Include \"#{TESTDIR}/fast_rules.txt\""
+    )
+    assert_no_issues
+    assert_log_match /CLIPP ANNOUNCE: abc/
+    assert_log_match /CLIPP ANNOUNCE: def/
+  end
+  
+  def test_falseinject
+    clipp(
+      :input_hashes => [make_request('abcdef')],
+      :config => CONFIG,
+      :default_site_config => "Include \"#{TESTDIR}/fast_rules.txt\""
+    )
+    assert_no_issues
+    assert_log_no_match /CLIPP ANNOUNCE: somethingelse/
+  end
+
+  def test_nonfast
+    clipp(
+      :input_hashes => [make_request('nonfast')],
+      :config => CONFIG,
+      :default_site_config => "Include \"#{TESTDIR}/fast_rules.txt\""
+    )
+    assert_no_issues
+    assert_log_match /CLIPP ANNOUNCE: nonfast/
+  end
+  
+  def test_contradiction
+    clipp(
+      :input_hashes => [make_request('contradiction')],
+      :config => CONFIG,
+      :default_site_config => "Include \"#{TESTDIR}/fast_rules.txt\""
+    )
+    assert_no_issues
+    assert_log_no_match /CLIPP ANNOUNCE: contradiction/
+  end
+
+  def test_headers
+    clipp(
+      :input_hashes => [simple_hash("GET /a HTTP/1.1\nHost: headervalue\n\n")],
+      :config => CONFIG,
+      :default_site_config => "Include \"#{TESTDIR}/fast_rules.txt\""
+    )
+    assert_no_issues
+    assert_log_no_match /CLIPP ANNOUNCE: headervalue/
+  end
+
+  # Disabled until RNS-192 is fixed.
+  #def test_body
+  #  s = "POST /a HTTP/1.1\nContent-Type: application/x-www-form-urlencoded\nContent-Length: 19\n\nfoo=bar&hello=world\n"
+  #  clipp(
+  #    :input_hashes => [simple_hash(s)],
+  #    :config => CONFIG,
+  #    :default_site_config => "Include \"#{TESTDIR}/fast_rules.txt\""
+  #  )
+  #  assert_no_issues
+  #  assert_log_no_match /CLIPP ANNOUNCE: body/
+  #end
 end
