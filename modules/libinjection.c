e90cf7e3aa813fafeb250f4a86393ca6ea03f310
Brian Rectanus <brectanus@qualys.com>
2013-03-03 23:50:36 -0600

    modules: Update libinjection transformations to allow for string injection and folding.

--- modules/libinjection.c
+++ modules/libinjection.c
@@ -39,7 +39,9 @@
 #include <ironbee/util.h>
 #include <ironbee/transformation.h>
 
+#include "modp_ascii.h"
 #include "sqlparse.h"
+#include "sqlparse_private.h"
 #include "sqli_normalize.h"
 #include "sqli_fingerprints.h"
 
@@ -52,6 +54,13 @@
 /* Declare the public module symbol. */
 IB_MODULE_DECLARE();
 
+/* Private configuration structure. */
+typedef struct sqli_config_t {
+    bool fold;
+} sqli_config_t;
+
+/* Normalization function prototype. */
+typedef bool (*sqli_tokenize_fn_t)(sfilter * sf, stoken_t * sout);
 
 /*********************************
  * Transformations
@@ -65,15 +74,26 @@ ib_status_t sqli_normalize_tfn(ib_engine_t *ib,
                                ib_field_t **field_out,
                                ib_flags_t *pflags)
 {
-    ib_bytestr_t *bs;
-    size_t new_size;
-    ib_status_t rc;
-
     assert(ib != NULL);
     assert(mp != NULL);
     assert(field_in != NULL);
     assert(field_out != NULL);
     assert(pflags != NULL);
+
+    sqli_config_t *cfg = (sqli_config_t *)tfn_data;
+    sqli_tokenize_fn_t tokenize_fn;
+    sfilter sf;
+    stoken_t current;
+    ib_bytestr_t *bs_in;
+    ib_bytestr_t *bs_out;
+    char *buf_in;
+    char *buf_in_start;
+    size_t buf_in_len;
+    char *buf_out;
+    char *buf_out_end;
+    size_t buf_out_len;
+    char prev_token_type;
+    ib_status_t rc;
 
     /* Currently only bytestring types are supported.
      * Other types will just get passed through. */
@@ -81,27 +101,98 @@ ib_status_t sqli_normalize_tfn(ib_engine_t *ib,
         return ib_field_copy(field_out, mp, field_in->name, field_in->nlen, field_in);
     }
 
-    /* Make a copy of the field and extract the underlying value. */
-    rc = ib_field_copy(field_out, mp, field_in->name, field_in->nlen, field_in);
-    if (rc != IB_OK) {
-        return rc;
-    }
-    rc = ib_field_value(*field_out, ib_ftype_bytestr_mutable_out(&bs));
+    /* Extract the underlying incoming value. */
+    rc = ib_field_value(field_in, ib_ftype_bytestr_mutable_out(&bs_in));
     if (rc != IB_OK) {
         return rc;
     }
 
-    /* Normalize the value with libinjection. */
-    new_size = sqli_qs_normalize((char *)ib_bytestr_ptr(bs), ib_bytestr_length(bs));
-    ib_bytestr_setv(bs, ib_bytestr_ptr(bs), new_size);
+    /* Normalize the incoming value for the tokenizer (toupper). */
+    buf_in = (char *)ib_mpool_memdup(mp, ib_bytestr_const_ptr(bs_in), ib_bytestr_length(bs_in));
+    if (buf_in == NULL) {
+        return IB_EALLOC;
+    }
+    modp_toupper(buf_in, ib_bytestr_length(bs_in));
+
+    /* Create a buffer big enough (double) to allow for normalization. */
+    buf_out = buf_out_end = (char *)ib_mpool_calloc(mp, 2, ib_bytestr_length(bs_in));
+    if (buf_out == NULL) {
+        return IB_EALLOC;
+    }
+
+    /* As SQL can be injected into a string, the normalization
+     * needs to start after the first quote character if one
+     * exists.
+     *
+     * First try single quote, then double, then none.
+     *
+     * TODO: Handle returning multiple transformations:
+     *       1) Straight normalization
+     *       2) Normalization as if with single quotes (starting point
+     *          should be based on straight normalization)
+     *       3) Normalization as if with double quotes (starting point
+     *          should be based on straight normalization)
+     */
+    buf_in_start = memchr(buf_in, CHAR_SINGLE, ib_bytestr_length(bs_in));
+    if (buf_in_start == NULL) {
+        buf_in_start = memchr(buf_in, CHAR_DOUBLE, ib_bytestr_length(bs_in));
+    }
+    if (buf_in_start == NULL) {
+        buf_in_start = buf_in;
+        buf_in_len = ib_bytestr_length(bs_in);
+    }
+    else {
+        ++buf_in_start; /* After the quote. */
+        buf_in_len = ib_bytestr_length(bs_in) - (buf_in_start - buf_in);
+    }
+
+    /* Copy the normalized tokens as a space separated list. Since
+     * the tokenizer does not backtrack, and the normalized values
+     * are always equal to or less than the original length, the
+     * tokens are written back to the beginning of the original
+     * buffer.
+     */
+    tokenize_fn = cfg->fold ? filter_fold : sqli_tokenize;
+    sfilter_reset(&sf, buf_in_start, buf_in_len);
+    buf_out_len = 0;
+    prev_token_type = 0;
+    while (tokenize_fn(&sf, &current)) {
+        size_t token_len = strlen(current.val);
+        ib_log_debug2(ib, "SQLi TOKEN: %c \"%s\"\n", current.type, current.val);
+
+        /* Add in the space if required. */
+        if ((buf_out_end != buf_out) &&
+            (current.type != 'o') &&
+            (prev_token_type != 'o') &&
+            (current.type != ',') &&
+            (*(buf_out_end - 1) != ','))
+        {
+            *buf_out_end = ' ';
+            buf_out_end += 1;
+            ++buf_out_len;
+        }
+
+        /* Copy the token value. */
+        memcpy(buf_out_end, current.val, token_len);
+        buf_out_end += token_len;
+        buf_out_len += token_len;
+
+        prev_token_type = current.type;
+    }
 
     /* Mark as modified. */
-    /* TODO: Need to know if it was actually modified or not. */
     *pflags = IB_TFN_FMODIFIED;
 
-    return IB_OK;
+    /* Create the output field wrapping bs_out. */
+    rc = ib_bytestr_alias_mem(&bs_out, mp, (uint8_t *)buf_out, buf_out_len);
+    if (rc != IB_OK) {
+        return rc;
+    }
+    return ib_field_create(field_out, mp,
+                           field_in->name, field_in->nlen,
+                           IB_FTYPE_BYTESTR,
+                           ib_ftype_bytestr_mutable_in(bs_out));
 }
-
 
 /*********************************
  * Operators
@@ -194,16 +285,39 @@ ib_status_t sqli_op_execute(const ib_rule_exec_t *rule_exec,
 /* Called to initialize a module (on load). */
 static ib_status_t sqli_init(ib_engine_t *ib, ib_module_t *m, void *cbdata)
 {
+    assert(ib != NULL);
+    assert(m != NULL);
+
+    ib_mpool_t *pool = ib_engine_pool_config_get(ib);
+    sqli_config_t *sqli_config;
     ib_status_t rc;
 
     ib_log_debug(ib, "Initializing %s module.", MODULE_NAME_STR);
 
+    /* Register normalizeSqli transformation. */
+    sqli_config = (sqli_config_t *)ib_mpool_calloc(pool, 1, sizeof(*sqli_config));
+    if (sqli_config == NULL) {
+        return IB_EALLOC;
+    }
     rc = ib_tfn_register(ib, "normalizeSqli", sqli_normalize_tfn,
-                         IB_TFN_FLAG_NONE, NULL);
+                         IB_TFN_FLAG_NONE, sqli_config);
     if (rc != IB_OK) {
         return rc;
     }
 
+    /* Register normalizeSqliFold transformation. */
+    sqli_config = (sqli_config_t *)ib_mpool_calloc(pool, 1, sizeof(*sqli_config));
+    if (sqli_config == NULL) {
+        return IB_EALLOC;
+    }
+    sqli_config->fold = 1;
+    rc = ib_tfn_register(ib, "normalizeSqliFold", sqli_normalize_tfn,
+                         IB_TFN_FLAG_NONE, sqli_config);
+    if (rc != IB_OK) {
+        return rc;
+    }
+
+    /* Register is_sqli operator. */
     rc = ib_operator_register(ib,
                               "is_sqli",
                               IB_OP_FLAG_PHASE,
