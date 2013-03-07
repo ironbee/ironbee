c4601d854dd46ff06155a0c16c42356f5976e358
Ivan Ristic <ivanr@webkreator.com>
2013-03-07 08:51:22 +0000

    Remove unused htp_tx_t::request_line_raw.

--- htp/htp.h
+++ htp/htp.h
@@ -228,10 +228,7 @@ struct htp_tx_t {
     unsigned int request_ignored_lines;
 
     /** The first line of this request. */
-    bstr *request_line;
-
-    /** The first line of this request including ws+line terminator(s). */
-    bstr *request_line_raw;
+    bstr *request_line;   
 
     /** How many NUL bytes are there in the request line? */
     int request_line_nul;
