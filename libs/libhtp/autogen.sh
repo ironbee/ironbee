1b0e9219084e62e55654ded1a22ce4f7b4cbbaf1
Nick LeRoy <nleroy@qualys.com>
2013-03-08 14:33:18 -0600

    Removed old libhtp

--- libs/libhtp/autogen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Cleanup
-rm -rf autom4te.cache
-
-# Generate
-autoreconf -i -f  -v
--- libs/libhtp/extras/ruby/extconf.rb
+++
@@ -1,6 +0,0 @@
-require 'mkmf'
-
-dir_config( 'htp' )
-have_library( 'htp', 'htp_connp_create' ) || abort( "Can't find HTP library." )
-have_header( 'htp/htp.h' ) || abort( "Can't find htp.h" )
-create_makefile( 'htp' )
