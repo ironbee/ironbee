4b44c2c1f22a7db5a2c68c7c62ad67cc6105384e
Christopher Alfeld <calfeld@qualys.com>
2013-03-19 13:22:50 -0500

    predicate/merge_graph: Add common subexpression merging class, MergeGraph, with partial implementation and very initial tests.

---
+++ predicate/merge_graph.cpp
@@ -0,0 +1,309 @@
+/*****************************************************************************
+ * Licensed to Qualys, Inc. (QUALYS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * QUALYS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ****************************************************************************/
+
+/**
+ * @file
+ * @brief Predicate --- MergeGraph Implementation
+ *
+ * @author Christopher Alfeld <calfeld@qualys.com>
+ */
+
+#include "merge_graph.hpp"
+
+#include "bfs.hpp"
+#include "dot.hpp"
+
+#include <sstream>
+
+#include <boost/bind.hpp>
+#include <boost/function_output_iterator.hpp>
+
+using namespace std;
+
+namespace IronBee {
+namespace Predicate {
+namespace DAG {
+
+size_t MergeGraph::add_root(node_p& root)
+{
+    if (! root) {
+        BOOST_THROW_EXCEPTION(
+            IronBee::einval() << errinfo_what(
+                "Cannot add singular root."
+            )
+        );
+    }
+    if (! root->parents().empty()) {
+        BOOST_THROW_EXCEPTION(
+            IronBee::einval() << errinfo_what(
+                "Root has parents."
+            )
+        );
+
+    }
+
+    merge_tree(root); // May change root.
+
+    m_roots.push_back(root);
+    size_t index = m_roots.size() - 1;
+
+    m_root_indices[root] = index;
+    return index;
+}
+
+const node_p& MergeGraph::root(size_t index) const
+{
+    if (index >= m_roots.size()) {
+        BOOST_THROW_EXCEPTION(
+            IronBee::enoent() << errinfo_what(
+                "Root index too large."
+            )
+        );
+    }
+
+    return m_roots[index];
+}
+
+size_t MergeGraph::root_index(const node_cp& root) const
+{
+    if (! root) {
+        BOOST_THROW_EXCEPTION(
+            IronBee::einval() << errinfo_what(
+                "Cannot search for singular root."
+            )
+        );
+    }
+    node_cp known_root = known(root);
+    if (! known_root) {
+        known_root = root;
+    }
+    root_indices_t::const_iterator i = m_root_indices.find(known_root);
+    if (i == m_root_indices.end()) {
+        BOOST_THROW_EXCEPTION(
+            IronBee::enoent() << errinfo_what(
+                "No such root."
+            )
+        );
+    }
+    return i->second;
+}
+
+pair<bool, node_p> MergeGraph::learn(const node_p& which)
+{
+    if (! which) {
+        BOOST_THROW_EXCEPTION(
+            IronBee::einval() << errinfo_what(
+                "Cannot add singular node."
+            )
+        );
+    }
+    bool is_new;
+    node_by_sexpr_t::iterator i;
+
+    boost::tie(i, is_new) =
+        m_node_by_sexpr.insert(make_pair(which->to_s(), which));
+    return make_pair(is_new, i->second);
+}
+
+bool MergeGraph::unlearn(const node_cp& which)
+{
+    node_by_sexpr_t::iterator i = m_node_by_sexpr.find(which->to_s());
+    if (i != m_node_by_sexpr.end()) {
+        m_node_by_sexpr.erase(i);
+        return true;
+    }
+    return false;
+}
+
+node_p MergeGraph::known(const node_cp& node) const
+{
+    if (! node) {
+        BOOST_THROW_EXCEPTION(
+            IronBee::einval() << errinfo_what(
+                "Cannot add singular node."
+            )
+        );
+    }
+    node_by_sexpr_t::const_iterator i =
+        m_node_by_sexpr.find(node->to_s());
+    if (i == m_node_by_sexpr.end()) {
+        return node_p();
+    }
+    else {
+        return i->second;
+    }
+}
+
+void MergeGraph::replace(const node_cp& which, node_p& with)
+{
+    node_p known_which = known(which);
+    if (! known_which) {
+        BOOST_THROW_EXCEPTION(
+            IronBee::enoent() << errinfo_what(
+                "No such subexpression."
+            )
+        );
+    }
+
+    // If with is known, this will change with to point to it.  Otherwise,
+    // this will merge in the tree based at with.
+    merge_tree(with);
+
+    // Unlearn all subexpressions of known_which and parents.
+    bfs_up(
+        known_which,
+        boost::make_function_output_iterator(
+            boost::bind(&MergeGraph::unlearn, this, _1)
+        )
+    );
+
+    // Replace known_which with with in all parents of known_which.
+    // Doing so will update the sexprs of all ancestors of with.
+    // As we are holding a node_p to known_which, it and its descendants
+    // will stay around long enough for us to unlearn them as necessary
+    // later on.
+    BOOST_FOREACH(const weak_node_p& weak_parent, known_which->parents()) {
+        weak_parent.lock()->replace_child(known_which, with);
+    }
+
+    // Learn all new ancestor sexprs.
+    bfs_up(
+        with,
+        boost::make_function_output_iterator(
+            boost::bind(&MergeGraph::learn, this, _1)
+        )
+    );
+
+    // Follow descendants of known_which so long as single parent.
+    // When we hit a multiple parent child, we need to remove it from the
+    // parent we came from (the desendant of known_which), but should not
+    // unlearn its sexpr (it's still in the graph from another parent)
+    // and can stop our descent.
+    // When known_which has know common subexpressions with other parts of
+    // the graph, this is equivalent to unlearning all children.
+    // Note that we do not check for multiple visits.  It is important, if a
+    // child is reached via multiple paths, to handle all such paths.  E.g.,
+    // a child with two paths will, the first time, be removed from one of its
+    // parents and the second time unlearn (as it now has a single parent).
+    list<node_p> todo;
+    todo.push_back(known_which);
+    while (! todo.empty()) {
+        node_p parent = todo.front();
+        todo.pop_front();
+        BOOST_FOREACH(const node_p& child, parent->children()) {
+            if (child->parents().size() == 1) {
+                unlearn(child);
+                todo.push_back(child);
+            }
+            else {
+                parent->remove_child(child);
+            }
+        }
+    }
+
+    // If replacing a root, need to update root datastructures, preserving
+    // existing index.
+    root_indices_t::iterator root_index_i =
+        m_root_indices.find(known_which);
+    if (root_index_i != m_root_indices.end()) {
+        size_t index = root_index_i->second;
+        m_root_indices.erase(root_index_i);
+        m_roots[index] = with;
+        m_root_indices[with] = index;
+    }
+
+    // At this point, we're done.  Once any external references to known_which
+    // are gone, its shared count will go to 0 and it will be freed, reducing
+    // any childrens shared count, and so forth, until it and all children
+    // not still part of the graph are freed.
+}
+
+void MergeGraph::merge_tree(node_p& which)
+{
+    if (! which) {
+        BOOST_THROW_EXCEPTION(
+            IronBee::einval() << errinfo_what(
+                "Cannot merge singular node."
+            )
+        );
+    }
+    bool new_which;
+    node_p known_which;
+    boost::tie(new_which, known_which) = learn(which);
+
+    if (! new_which) {
+        assert(which != known_which);
+        which = known_which;
+    }
+    else {
+        list<node_p> todo;
+        todo.push_back(which);
+
+        while (! todo.empty()) {
+            node_p parent = todo.front();
+            todo.pop_front();
+            BOOST_FOREACH(const node_p& child, parent->children()) {
+                bool new_child;
+                node_p known_child;
+                boost::tie(new_child, known_child) = learn(child);
+                if (! new_child) {
+                    assert(child != known_child);
+                    parent->replace_child(child, known_child);
+                }
+                else {
+                    todo.push_back(child);
+                }
+            }
+        }
+    }
+}
+
+namespace {
+
+string debug_node_decorator(const node_cp& node)
+{
+    stringstream r;
+    r << "label=\"" << node << "\\n"
+      << node->to_s()
+      << "\"";
+    return r.str();
+}
+
+}
+
+void MergeGraph::write_debug_report(std::ostream& out)
+{
+    out << "node_by_sexpr: " << endl;
+    BOOST_FOREACH(node_by_sexpr_t::const_reference& v, m_node_by_sexpr) {
+        out << v.first << " -> " << v.second->to_s() << " @ " << v.second << endl;
+        if (v.first != v.second->to_s()) {
+            out << "  ERROR: Mismatch." << endl;
+        }
+    }
+
+    out << "root_indices: " << endl;
+    BOOST_FOREACH(root_indices_t::const_reference& v, m_root_indices) {
+        out << v.first->to_s() << " @ " << v.first << " -> " << v.second << endl;
+    }
+
+    out << endl << "Graph: " << endl;
+    to_dot(out, m_roots.begin(), m_roots.end(), debug_node_decorator);
+}
+
+} // DAG
+} // Predicate
+} // IronBee
---
+++ predicate/tests/test_merge_graph.cpp
@@ -0,0 +1,111 @@
+/*****************************************************************************
+ * Licensed to Qualys, Inc. (QUALYS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * QUALYS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ****************************************************************************/
+
+/**
+ * @file
+ * @brief Predicate --- BFS Tests
+ *
+ * @author Christopher Alfeld <calfeld@qualys.com>
+ **/
+
+#include "../merge_graph.hpp"
+#include "parse_fixture.hpp"
+
+#include "gtest/gtest.h"
+
+using namespace IronBee::Predicate;
+using namespace std;
+
+class TestMergeGraph :
+    public ::testing::Test,
+    public ParseFixture
+{
+protected:
+    virtual void SetUp()
+    {
+        m_factory
+            .add("A", &create)
+            .add("B", &create)
+            .add("C", &create)
+            ;
+    }
+
+    size_t num_descendants(const DAG::node_cp& node) const
+    {
+        vector<DAG::node_cp> r;
+        DAG::bfs_down(node, back_inserter(r));
+        return r.size();
+    }
+};
+
+TEST_F(TestMergeGraph, Easy)
+{
+    DAG::node_p n = parse("(A (B (C)))");
+    DAG::MergeGraph g;
+    size_t n_i;
+
+    EXPECT_TRUE(g.empty());
+    EXPECT_NO_THROW(n_i = g.add_root(n));
+    EXPECT_EQ(n, g.root(n_i));
+    EXPECT_EQ(n_i, g.root_index(n));
+    EXPECT_FALSE(g.empty());
+}
+
+TEST_F(TestMergeGraph, Basic)
+{
+    DAG::node_p n = parse("(A (B (C)) (B (C)))");
+    DAG::MergeGraph g;
+    size_t n_i;
+
+    EXPECT_NO_THROW(n_i = g.add_root(n));
+    EXPECT_EQ(n, g.root(n_i));
+    EXPECT_EQ(n_i, g.root_index(n));
+
+    EXPECT_EQ(3, num_descendants(n));
+}
+
+TEST_F(TestMergeGraph, MultipleRoots)
+{
+    DAG::MergeGraph g;
+    DAG::node_p n = parse("(A (B (C)) (B (C)))");
+    size_t n_i;
+    DAG::node_p m = parse("(C (B (C)))");
+    size_t m_i;
+
+    EXPECT_NO_THROW(n_i = g.add_root(n));
+    EXPECT_NO_THROW(m_i = g.add_root(m));
+    EXPECT_EQ(n, g.root(n_i));
+    EXPECT_EQ(n_i, g.root_index(n));
+    EXPECT_EQ(m, g.root(m_i));
+    EXPECT_EQ(m_i, g.root_index(m));
+
+    EXPECT_EQ(2UL, g.size());
+    EXPECT_EQ(n, *g.roots().first);
+    EXPECT_EQ(m, *boost::next(g.roots().first));
+}
+
+TEST_F(TestMergeGraph, KnownRoot)
+{
+    DAG::MergeGraph g;
+    DAG::node_p n = parse("(A (B (C)) (B (C)))");
+    DAG::node_p m = parse("(B (C))");
+    DAG::node_p backup_m = m;
+
+    EXPECT_NO_THROW(g.add_root(n));
+    EXPECT_NO_THROW(g.add_root(m));
+    EXPECT_NE(backup_m, m);
+}
