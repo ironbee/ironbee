642532e0989a097724d6f70b441e20834fc07a05
Christopher Alfeld <calfeld@qualys.com>
2013-03-22 14:00:32 -0500

    predicate/reporter: Add simple classes for collection error and warning messages.

---
+++ predicate/reporter.cpp
@@ -0,0 +1,76 @@
+/*****************************************************************************
+ * Licensed to Qualys, Inc. (QUALYS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * QUALYS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ****************************************************************************/
+
+/**
+ * @file
+ * @brief Predicate --- Reporter Implementation
+ *
+ * @author Christopher Alfeld <calfeld@qualys.com>
+ */
+
+#include "reporter.hpp"
+
+#include <boost/foreach.hpp>
+
+using namespace std;
+
+namespace IronBee {
+namespace Predicate {
+
+Reporter::Reporter() :
+    m_num_errors(0),
+    m_num_warnings(0)
+{
+    // nop
+}
+
+void Reporter::error(const string& message)
+{
+    m_messages.push_back("ERROR: " + message);
+}
+
+void Reporter::warn(const string& message)
+{
+    m_messages.push_back("WARNING: " + message);
+}
+
+void Reporter::write_report(ostream& out) const
+{
+    BOOST_FOREACH(const string& msg, m_messages) {
+        out << msg << endl;
+    }
+}
+
+NodeReporter::NodeReporter(Reporter& reporter, const node_cp& node) :
+    m_reporter(reporter),
+    m_node(node)
+{
+
+}
+
+void NodeReporter::error(const string& msg)
+{
+    m_reporter.error(m_node->to_s() + ": " + msg);
+}
+
+void NodeReporter::warn(const string& msg)
+{
+    m_reporter.warn(m_node->to_s() + ": " + msg);
+}
+
+} // Predicate
+} // IronBee
---
+++ predicate/reporter.hpp
@@ -0,0 +1,106 @@
+/*****************************************************************************
+ * Licensed to Qualys, Inc. (QUALYS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * QUALYS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ****************************************************************************/
+
+/**
+ * @file
+ * @brief Predicate --- Reporter
+ *
+ * Defines Reporter and NodeReporter for Calls to report warnings and errors.
+ *
+ * @author Christopher Alfeld <calfeld@qualys.com>
+ */
+
+#ifndef __PREDICATE__REPORTER__
+#define __PREDICATE__REPORTER__
+
+#include "dag.hpp"
+
+#include <list>
+#include <string>
+
+namespace IronBee {
+namespace Predicate {
+
+/**
+ * Collect error and warning messages.
+ **/
+class Reporter
+{
+public:
+    //! Default constructor.
+    Reporter();
+
+    //! Add error message.
+    void error(const std::string& message);
+    //! Add warning message.
+    void warn(const std::string& message);
+    //! Write all messages to @a out.
+    void write_report(std::ostream& out) const;
+
+    //! Number of error messages added.
+    size_t num_errors() const
+    {
+        return m_num_errors;
+    }
+
+    //! Number of warning messages added.
+    size_t num_warnings() const
+    {
+        return m_num_warnings;
+    }
+
+private:
+    typedef std::list<std::string> messages_t;
+    messages_t m_messages;
+    size_t m_num_errors;
+    size_t m_num_warnings;
+};
+
+/**
+ * Collect error and warning messages for a specific node.
+ **/
+class NodeReporter
+{
+public:
+    /**
+     * Constructor.
+     *
+     * @param[in] reporter Reporter to report messages to.
+     * @param[in] node     Node to report messages for.
+     **/
+    NodeReporter(Reporter& reporter, const node_cp& node);
+
+    //! Node accessor.
+    const node_cp& node() const
+    {
+        return m_node;
+    }
+
+    //! Add error message.
+    void error(const std::string& msg);
+    //! Add warning message.
+    void warn(const std::string& msg);
+
+private:
+    Reporter&     m_reporter;
+    const node_cp m_node;
+};
+
+} // Predicate
+} // IronBee
+
+#endif
