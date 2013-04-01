ironbee/ironbee

commit 6a82cbced8a3d76120ffb1f26ba9c38235a37ff9

Add request_hostname and request_port to htp_tx_t.

htp/htp_transaction.c
@@ -377,7 +377,16 @@ static htp_status_t htp_tx_process_request_headers(htp_tx_t *tx) {
         return HTP_OK;
     }
 
-    // Host resolution
+    // Use the hostname from the URI, when available.   
+
+    if (tx->parsed_uri->hostname != NULL) {
+        tx->request_hostname = bstr_dup(tx->parsed_uri->hostname);
+    }
+
+    tx->request_port_number = tx->parsed_uri->port_number;
+
+    // Examine the Host header.
+    
     htp_header_t *h = htp_table_get_c(tx->request_headers, "host");
     if (h == NULL) {
         // No host information in the headers.
@@ -397,16 +406,15 @@ static htp_status_t htp_tx_process_request_headers(htp_tx_t *tx) {
         if (htp_parse_header_hostport(h->value, &hostname, &port, &(tx->flags)) != HTP_OK) return HTP_ERROR;
 
         // Is there host information in the URI?
-        if (tx->parsed_uri->hostname == NULL) {
+        if (tx->request_hostname == NULL) {
             // There is no host information in the URI. Place the
             // hostname from the headers into the parsed_uri structure.
-            tx->parsed_uri->hostname = hostname;
-            tx->parsed_uri->port_number = port;
+            tx->request_hostname = hostname;
+            tx->request_port_number = port;
         } else {
-            if ((bstr_cmp_nocase(hostname, tx->parsed_uri->hostname) != 0) || (port != tx->parsed_uri->port_number)) {
-                // The host information is different in the
-                // headers and the URI. The HTTP RFC states that
-                // we should ignore the header copy.
+            if ((bstr_cmp_nocase(hostname, tx->request_hostname) != 0) || (port != tx->request_port_number)) {
+                // The host information is different in the headers and the URI. The
+                // HTTP RFC states that we should ignore the header copy.
                 tx->flags |= HTP_HOST_AMBIGUOUS;
                 htp_log(tx->connp, HTP_LOG_MARK, HTP_LOG_WARNING, 0, "Host information ambiguous");
             }

htp/htp.h
@@ -385,6 +385,20 @@ struct htp_tx_t {
     /** Authentication password. Available only when htp_tx_t::request_auth_type is HTP_AUTH_BASIC. */
     bstr *request_auth_password;
 
+    /**
+     * Request hostname. Per the RFC, the hostname will be taken from the Host header
+     * when available. If the host information is also available in the URI, it is used
+     * instead of whatever might be in the Host header. Can be NULL. This field does
+     * not contain port information.
+     */
+    bstr *request_hostname;
+
+    /**
+     * Request port number, if presented. The rules for htp_tx_t::request_host apply. Set to
+     * -1 by default.
+     */
+    int request_port_number;
+
 
     // Response fields
 

test/test_hybrid.cpp
@@ -360,6 +360,9 @@ TEST_F(HybridParsing, PostUrlecodedTest) {
     htp_tx_req_set_protocol_number(tx, HTP_PROTOCOL_1_1);
     htp_tx_req_set_protocol_0_9(tx, 0);
 
+    // Request line complete
+    htp_tx_state_request_line(tx);
+
     // Configure headers to trigger the URLENCODED parser
     htp_tx_req_set_header(tx, "Content-Type", 12, HTP_URLENCODED_MIME_TYPE,
             strlen(HTP_URLENCODED_MIME_TYPE), HTP_ALLOC_COPY);
@@ -423,6 +426,7 @@ static void HybridParsing_CompressedResponse_Setup(htp_tx_t *tx) {
     htp_tx_req_set_protocol_number(tx, HTP_PROTOCOL_1_1);
     htp_tx_req_set_protocol_0_9(tx, 0);
 
+    htp_tx_state_request_line(tx);
     htp_tx_state_request_headers(tx);
     htp_tx_state_request_complete(tx);
 
@@ -585,6 +589,7 @@ TEST_F(HybridParsing, PostUrlecodedChunked) {
     htp_tx_req_set_protocol(tx, "HTTP/1.1", 8, HTP_ALLOC_COPY);
     htp_tx_req_set_protocol_number(tx, HTP_PROTOCOL_1_1);
     htp_tx_req_set_protocol_0_9(tx, 0);
+    htp_tx_state_request_line(tx);
 
     // Configure headers to trigger the URLENCODED parser.
     htp_tx_req_set_header(tx, "Content-Type", 12, HTP_URLENCODED_MIME_TYPE,

test/test_main.cpp
@@ -257,23 +257,23 @@ TEST_F(ConnectionParsing, HeaderHostParsing) {
 
     htp_tx_t *tx1 = (htp_tx_t *) htp_list_get(connp->conn->transactions, 0);
     ASSERT_TRUE(tx1 != NULL);
-    ASSERT_TRUE(tx1->parsed_uri->hostname != NULL);
-    ASSERT_EQ(0, bstr_cmp_c(tx1->parsed_uri->hostname, "www.example.com"));
+    ASSERT_TRUE(tx1->request_hostname != NULL);
+    ASSERT_EQ(0, bstr_cmp_c(tx1->request_hostname, "www.example.com"));
 
     htp_tx_t *tx2 = (htp_tx_t *) htp_list_get(connp->conn->transactions, 1);
     ASSERT_TRUE(tx2 != NULL);
-    ASSERT_TRUE(tx2->parsed_uri->hostname != NULL);
-    ASSERT_EQ(0, bstr_cmp_c(tx2->parsed_uri->hostname, "www.example.com."));
+    ASSERT_TRUE(tx2->request_hostname != NULL);
+    ASSERT_EQ(0, bstr_cmp_c(tx2->request_hostname, "www.example.com."));
 
     htp_tx_t *tx3 = (htp_tx_t *) htp_list_get(connp->conn->transactions, 2);
     ASSERT_TRUE(tx3 != NULL);
-    ASSERT_TRUE(tx3->parsed_uri->hostname != NULL);
-    ASSERT_EQ(0, bstr_cmp_c(tx3->parsed_uri->hostname, "www.example.com"));
+    ASSERT_TRUE(tx3->request_hostname != NULL);
+    ASSERT_EQ(0, bstr_cmp_c(tx3->request_hostname, "www.example.com"));
 
     htp_tx_t *tx4 = (htp_tx_t *) htp_list_get(connp->conn->transactions, 3);
     ASSERT_TRUE(tx4 != NULL);
-    ASSERT_TRUE(tx4->parsed_uri->hostname != NULL);
-    ASSERT_EQ(0, bstr_cmp_c(tx4->parsed_uri->hostname, "www.example.com"));
+    ASSERT_TRUE(tx4->request_hostname != NULL);
+    ASSERT_EQ(0, bstr_cmp_c(tx4->request_hostname, "www.example.com"));
 }
 
 TEST_F(ConnectionParsing, ResponseWithoutContentLength) {
@@ -447,16 +447,24 @@ TEST_F(ConnectionParsing, AmbiguousHost) {
     ASSERT_TRUE(tx2 != NULL);
     ASSERT_EQ(HTP_RESPONSE_COMPLETE, tx2->progress);
     ASSERT_TRUE(tx2->flags & HTP_HOST_AMBIGUOUS);
+    ASSERT_TRUE(tx2->request_hostname != NULL);
+    ASSERT_EQ(0, bstr_cmp_c(tx2->request_hostname, "example.com"));
 
     htp_tx_t *tx3 = (htp_tx_t *) htp_list_get(connp->conn->transactions, 2);
     ASSERT_TRUE(tx3 != NULL);
     ASSERT_EQ(HTP_RESPONSE_COMPLETE, tx3->progress);
     ASSERT_FALSE(tx3->flags & HTP_HOST_AMBIGUOUS);
+    ASSERT_TRUE(tx3->request_hostname != NULL);
+    ASSERT_EQ(0, bstr_cmp_c(tx3->request_hostname, "www.example.com"));
+    ASSERT_EQ(8001, tx3->request_port_number);
 
     htp_tx_t *tx4 = (htp_tx_t *) htp_list_get(connp->conn->transactions, 3);
     ASSERT_TRUE(tx4 != NULL);
     ASSERT_EQ(HTP_RESPONSE_COMPLETE, tx4->progress);
     ASSERT_TRUE(tx4->flags & HTP_HOST_AMBIGUOUS);
+    ASSERT_TRUE(tx4->request_hostname != NULL);
+    ASSERT_EQ(0, bstr_cmp_c(tx4->request_hostname, "www.example.com"));
+    ASSERT_EQ(8002, tx4->request_port_number);
 }
 
 TEST_F(ConnectionParsing, Http_0_9) {

commit d600b6a3fda820169d9e51773e15d08a8e88631f

Squashed 'libs/libhtp/' changes from c81e169..cdd75a8
cdd75a8 htp_transaction.c: Fix memory leak introduced in 6a82cbced8a3d76120ffb1f26ba9c38235a37ff9.
5bc26da htp_utf8_decoder.c: Remove 'inline' keyword for two functions that have external linkage but use statics.
ac76409 htp_request.c, htp_response.c: Fix use of uninitialized memory.

git-subtree-dir: libs/libhtp
git-subtree-split: cdd75a8354f4e96eb277392f8ed6a29be44ca322

htp/htp_request.c
@@ -147,7 +147,7 @@ static htp_status_t htp_req_handle_state_change(htp_connp_t *connp) {
     if (connp->in_state_previous == connp->in_state) return HTP_OK;
 
     if (connp->in_state == htp_connp_REQ_HEADERS) {
-        htp_status_t rc;
+        htp_status_t rc = HTP_OK;
 
         switch (connp->in_tx->progress) {
             case HTP_REQUEST_HEADERS:

htp/htp_response.c
@@ -147,7 +147,7 @@ static htp_status_t htp_res_handle_state_change(htp_connp_t *connp) {
     if (connp->out_state_previous == connp->out_state) return HTP_OK;
 
     if (connp->out_state == htp_connp_RES_HEADERS) {
-        htp_status_t rc;
+        htp_status_t rc = HTP_OK;
 
         switch (connp->out_tx->progress) {
             case HTP_RESPONSE_HEADERS:

htp/htp_transaction.c
@@ -99,6 +99,10 @@ void htp_tx_destroy(htp_tx_t *tx) {
 
     bstr_free(tx->request_content_type);
 
+    if (tx->request_hostname != NULL) {
+        bstr_free(tx->request_hostname);
+    }
+
     // Response.
 
     bstr_free(tx->response_line);

htp/htp_utf8_decoder.c
@@ -103,7 +103,7 @@ static const uint8_t utf8d_allow_overlong[] = {
  * @return HTP_UTF8_ACCEPT for a valid character, HTP_UTF8_REJECT for an invalid character,
  *         or something else if the character has not yet been formed
  */
-inline uint32_t htp_utf8_decode(uint32_t* state, uint32_t* codep, uint32_t byte) {
+uint32_t htp_utf8_decode(uint32_t* state, uint32_t* codep, uint32_t byte) {
   uint32_t type = utf8d[byte];
 
   *codep = (*state != HTP_UTF8_ACCEPT) ?
@@ -124,7 +124,7 @@ inline uint32_t htp_utf8_decode(uint32_t* state, uint32_t* codep, uint32_t byte)
  * @return HTP_UTF8_ACCEPT for a valid character, HTP_UTF8_REJECT for an invalid character,
  *         or something else if the character has not yet been formed
  */
-inline uint32_t htp_utf8_decode_allow_overlong(uint32_t* state, uint32_t* codep, uint32_t byte) {
+uint32_t htp_utf8_decode_allow_overlong(uint32_t* state, uint32_t* codep, uint32_t byte) {
   uint32_t type = utf8d_allow_overlong[byte];
 
   *codep = (*state != HTP_UTF8_ACCEPT) ?