d2671a85553cc240c82c7c7445b522c006d6c7d7
Brian Rectanus <brectanus@qualys.com>
2013-04-16 11:30:40 -0500

    logevent: Change "Unknown" action name to "NoAction".

--- engine/logevent.c
+++ engine/logevent.c
@@ -46,7 +46,7 @@ static const char *ib_logevent_type_str[] = {
 
 /** Log Event Action Names */
 static const char *ib_logevent_action_str[] = {
-    "Unknown",
+    "NoAction",
     "Log",
     "Block",
     "Ignore",
