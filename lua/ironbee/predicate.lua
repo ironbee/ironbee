b2c21429554954cdb6f80e4b5547af6e24688aa9
Christopher Alfeld <calfeld@qualys.com>
2013-05-01 12:18:28 -0500

    predicate/frontend: Cleanup from code review.

--- lua/ironbee/predicate.lua
+++ lua/ironbee/predicate.lua
@@ -1,4 +1,25 @@
-local Predicate = {}
+----
+-- Predicate Lua Front End
+--
+-- This Lua module provides a front end to Predicate.  See
+-- predicate/predicate.md for an overview of Predicate and
+-- predicate/frontend.md for a description of this module from a user
+-- perspective.
+--
+-- This module defines a private class hierarchy with string_mt, call_mt,
+-- and raw_mt all inheriting from all_mt, and then provides a variety of
+-- methods to allow easy construction of these objects.  Ultimately, these
+-- objects can be called to convert them to their sexpr as strings.
+--
+-- Waggle provides a `predicate` method for all signature types that takes a
+-- single argument: either a string or an expression object, and sets up the
+-- predicate action.
+----
+
+local _M = {}
+_M._COPYRIGHT = "Copyright (C) 2013 Qualys, Inc."
+_M._DESCRIPTION = "IronBee Lua Predicate Frontend"
+_M._VERSION = "1.0"
 
 -- Private helpers
 
@@ -15,16 +36,16 @@ local function merge(name, a, b)
   end
   append(a)
   append(b)
-  return Predicate.C(name, unpack(r))
+  return _M.C(name, unpack(r))
 end
 
 -- Lua doesn't lookup operators via __index.
 local common_operators = {
   __add = function (a, b) return merge('and', a, b)  end,
   __div = function (a, b) return merge('or', a, b)   end,
-  __unm = function (a)    return Predicate.Not(a)    end,
+  __unm = function (a)    return _M.Not(a)    end,
   __sub = function (a, b) return a + (-b)            end,
-  __pow = function (a, b) return Predicate.Xor(a, b) end
+  __pow = function (a, b) return _M.Xor(a, b) end
 }
 
 -- Expression object and their methods.
@@ -63,13 +84,13 @@ function string_mt:new(value)
 end
 
 function string_mt:length()
-  return Predicate.S(self.value:len())
+  return _M.S(self.value:len())
 end
 function string_mt:streq(o)
-  return Predicate.Streq(self, o)
+  return _M.Streq(self, o)
 end
 function string_mt:istreq(o)
-  return Predicate.Istreq(self, o)
+  return _M.Istreq(self, o)
 end
 
 function string_mt:__call()
@@ -79,7 +100,7 @@ end
 local call_mt = all_mt:new('mt')
 
 -- Import string functions.
-for i,k in ipairs({'streq', 'istreq'})  do
+for _,k in ipairs({'streq', 'istreq'})  do
   call_mt[k] = string_mt[k]
 end
 
@@ -87,8 +108,8 @@ function call_mt:new(name, ...)
   local r = all_mt.new(self, 'call')
   local children = {}
 
-  for i,v in ipairs({...}) do
-    table.insert(children, Predicate.from_lua(v))
+  for _,v in ipairs({...}) do
+    table.insert(children, _M.from_lua(v))
   end
   r.name = name
   r.children = children
@@ -96,24 +117,24 @@ function call_mt:new(name, ...)
 end
 
 function call_mt:sub(field_name)
-  return Predicate.Sub(field_name, self)
+  return _M.Sub(field_name, self)
 end
 function call_mt:suball(field_name)
-  return Predicate.Suball(field_name, self)
+  return _M.Suball(field_name, self)
 end
 function call_mt:streq(value)
-  return Predicate.Streq(value, self)
+  return _M.Streq(value, self)
 end
 function call_mt:istreq(value)
-  return Predicate.Istreq(value, self)
+  return _M.Istreq(value, self)
 end
 function call_mt:eq(value)
-  return Predicate.Eq(value, self)
+  return _M.Eq(value, self)
 end
 
 function call_mt:__call()
   r = "(" .. self.name
-  for i,c in ipairs(self.children) do
+  for _,c in ipairs(self.children) do
       r = r .. " " .. c()
   end
   r = r .. ")"
@@ -132,24 +153,24 @@ end
 
 -- Fundamentals
 
-function Predicate.Raw(value)
+function _M.Raw(value)
   return raw_mt:new(value)
 end
-Predicate.R = Predicate.Raw
+_M.R = _M.Raw
 
-function Predicate.String(value)
+function _M.String(value)
   return string_mt:new(value)
 end
-Predicate.S = Predicate.String
-function Predicate.Call(name, ...)
+_M.S = _M.String
+function _M.Call(name, ...)
   return call_mt:new(name, ...)
 end
-Predicate.C = Predicate.Call
+_M.C = _M.Call
 
-Predicate.Null = all_mt:new("null")
-Predicate.Null.__call = function() return "null" end
-Predicate.True = Predicate.C("true")
-Predicate.False = Predicate.C("false")
+_M.Null = all_mt:new("null")
+_M.Null.__call = function() return "null" end
+_M.True = _M.C("true")
+_M.False = _M.C("false")
 
 -- Calls
 
@@ -158,16 +179,16 @@ local param2 = {'Sub', 'Suball', 'Name', 'Rx', 'Transformation'}
 local param3 = {'Operator'}
 local paramn = {'Or', 'And', 'List'}
 for i,n in ipairs(param1) do
-  Predicate[n] = function (a) return Predicate.C(n:lower(), a) end
+  _M[n] = function (a) return _M.C(n:lower(), a) end
 end
 for i,n in ipairs(param2) do
-  Predicate[n] = function (a, b) return Predicate.C(n:lower(), a, b) end
+  _M[n] = function (a, b) return _M.C(n:lower(), a, b) end
 end
 for i,n in ipairs(param3) do
-  Predicate[n] = function (a, b, c) return Predicate.C(n:lower(), a, b, c) end
+  _M[n] = function (a, b, c) return _M.C(n:lower(), a, b, c) end
 end
 for i,n in ipairs(paramn) do
-  Predicate[n] = function (...) return Predicate.C(n:lower(), ...) end
+  _M[n] = function (...) return _M.C(n:lower(), ...) end
 end
 
 local tfns = {
@@ -189,16 +210,16 @@ local tfns = {
 }
 for i,n in ipairs(tfns) do
   local capitalized = n:gsub("%l", string.upper)
-  Predicate[capitalized] = function (a) return Predicate.C(n, a) end
-  call_mt[n] = function (self) return Predicate[capitalized](self) end
+  _M[capitalized] = function (a) return _M.C(n, a) end
+  call_mt[n] = function (self) return _M[capitalized](self) end
 end
 
-function Predicate.If(a, b, c)
-  a = Predicate.from_lua(a)
-  b = Predicate.from_lua(b)
-  c = Predicate.from_lua(c)
+function _M.If(a, b, c)
+  a = _M.from_lua(a)
+  b = _M.from_lua(b)
+  c = _M.from_lua(c)
 
-  local is_true, is_converted = Predicate.to_lua(a)
+  local is_true, is_converted = _M.to_lua(a)
   if is_converted then
     if is_true then
       return b
@@ -230,60 +251,60 @@ local sym_swap = {
   Le = 'Ge'
 }
 for n,s in pairs(sym) do
-  Predicate[n] = function (a, b)
-    a = Predicate.from_lua(a)
-    b = Predicate.from_lua(b)
+  _M[n] = function (a, b)
+    a = _M.from_lua(a)
+    b = _M.from_lua(b)
     if a.type ~= 'string' and b.type ~= 'string' then
       error(n .. " requires at least one string literal argument.")
     end
     if a.type == 'string' and b.type == 'string' then
       if s(a, b) then
-        return Predicate.True
+        return _M.True
       else
-        return Predicate.False
+        return _M.False
       end
     end
     if a.type ~= 'string' then
       n = sym_swap[n] or n
-      return Predicate.C(n:lower(), b, a)
+      return _M.C(n:lower(), b, a)
     else
-      return Predicate.C(n:lower(), a, b)
+      return _M.C(n:lower(), a, b)
     end
   end
 end
 
 -- Indirect Calls
 
-function Predicate.Xor(a, b)
+function _M.Xor(a, b)
   return (a - b) / (b - a)
 end
-function Predicate.Nand(...)
-  return -Predicate.And(...)
+function _M.Nand(...)
+  return -_M.And(...)
 end
-function Predicate.Nor(...)
-  return -Predicate.Or(...)
+function _M.Nor(...)
+  return -_M.Or(...)
 end
-function Predicate.Nxor(...)
-  return -Predicate.Xor(...)
+function _M.Nxor(...)
+  return -_M.Xor(...)
 end
 
 -- Utility
 
-function Predicate.from_lua(v)
+function _M.from_lua(v)
   if type(v) == 'string' or type(v) == 'number' then
-    return Predicate.String(v)
+    return _M.String(v)
   end
   if type(v) == 'boolean' then
     if v then
-      return Predicate.True
+      return _M.True
     else
-      return Predicate.False
+      return _M.False
     end
   end
   return v
 end
 
-function Predicate.to_lua(v)
+function _M.to_lua(v)
   if v.type == nil then return nil, false end
   if v.type == 'null' then return nil, true end
   if v.type == 'call' then
@@ -301,7 +322,7 @@ function Predicate.to_lua(v)
   return nil, false
 end
 
-function Predicate.pp(s)
+function _M.pp(s)
   local indent = 0
   local r = ''
   local i = 1
@@ -337,4 +358,4 @@ function Predicate.pp(s)
   return r
 end
 
-return Predicate
+return _M
