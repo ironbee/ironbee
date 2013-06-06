5a339217655da30261f21d391d6b0da0372fad75
Sam Baskinger <sbaskinger@qualys.com>
2013-06-06 14:13:57 -0500

    Rough-cut implementation to build Lua objects.

--- lua/ironbee/util/ibjson.c
+++ lua/ironbee/util/ibjson.c
@@ -37,15 +37,66 @@
 static const char *LUA_IBJSONLIB_NAME = "ibjson";
 static const char *LUA_IBJSONLIB_VERSION = "1.0";
 
+/* Forward declare the callback type. */
+typedef struct ibjson_cbdata_t ibjson_cbdata_t;
+
+/**
+ * Function callback to push a value.
+ *
+ * When parsing JSON different values may be assigned differently.
+ * For example, a number parsed after a map key should be assigned to a 
+ * table.
+ *
+ * @param[in] cbdata Callback data.
+ */
+typedef void(*ibjson_push_fn_t)(ibjson_cbdata_t *cbdata);
+
 /**
  * Callback data passed around a Yajl parse.
  */
 struct ibjson_cbdata_t {
-    ib_mpool_t *mp;
-    lua_State *L;
+    ib_mpool_t *mp;           /**< Memory pool. */
+    lua_State *L;             /**< Lua stack. */
+    ibjson_push_fn_t push_fn; /**< Current function for pushing value. */
 };
 
-typedef struct ibjson_cbdata_t ibjson_cbdata_t;
+/**
+ * Nop.
+ *
+ * @param[in] cbata Callback data.
+ */
+static void ibjson_pushval(ibjson_cbdata_t *cbdata) {
+    /* Nop. The value on the stack is final. */
+}
+
+/**
+ * Append a value to the list at index -2 in the Lua list.
+ *
+ * @param[in] cbata Callback data.
+ */
+static void ibjson_pushlist(ibjson_cbdata_t *cbdata) {
+    int i = lua_objlen(cbdata->L, -2) + 1;
+
+    /* Copy the value to the top of the stack. */
+    lua_pushvalue(cbdata->L, -1);
+
+    /* Push the index to assign it to. */
+    lua_pushinteger(cbdata->L, i);
+
+    lua_replace(cbdata->L, -3);
+
+    lua_settable(cbdata->L, -3);
+}
+
+/**
+ * Assign the value at index -1 to the key at -2 into the table at -3.
+ *
+ * @param[in] cbata Callback data.
+ */
+static void ibjson_pushmap(ibjson_cbdata_t *cbdata) {
+    lua_settable(cbdata->L, -3);
+}
+
 
 /*****************************************************************************
  * YAJL callback definitions.
@@ -53,46 +104,124 @@ typedef struct ibjson_cbdata_t ibjson_cbdata_t;
 
 static int yajl_boolean(void *ctx, int boolVal)
 {
+    ibjson_cbdata_t *cbdata = (ibjson_cbdata_t *)ctx;
+
+    lua_pushboolean(cbdata->L, boolVal);
+
+    (cbdata->push_fn)(cbdata);
+
     return 1;
 }
 static int yajl_double(void *ctx, double doubleVal)
 {
+    ibjson_cbdata_t *cbdata = (ibjson_cbdata_t *)ctx;
+
+    lua_pushnumber(cbdata->L, doubleVal);
+
+    (cbdata->push_fn)(cbdata);
+
     return 1;
 }
 static int yajl_end_array(void *ctx)
 {
+    ibjson_cbdata_t *cbdata = (ibjson_cbdata_t *)ctx;
+
+    /* Restore the previous C function. */
+    cbdata->push_fn = (ibjson_push_fn_t)lua_tocfunction(cbdata->L, -2);
+
+    /* Remove it. */
+    lua_remove(cbdata->L, -2);
+
+    /* Handle final assignment. */
+    (cbdata->push_fn)(cbdata);
+
     return 1;
 }
 static int yajl_end_map(void *ctx)
 {
+    ibjson_cbdata_t *cbdata = (ibjson_cbdata_t *)ctx;
+
+    /* Restore the previous C function. */
+    cbdata->push_fn = (ibjson_push_fn_t)lua_tocfunction(cbdata->L, -2);
+
+    /* Remove it. */
+    lua_remove(cbdata->L, -2);
+
+    /* Handle final assignment. */
+    (cbdata->push_fn)(cbdata);
+
     return 1;
 }
 static int yajl_integer(void *ctx, long long integerVal)
 {
+    ibjson_cbdata_t *cbdata = (ibjson_cbdata_t *)ctx;
+
+    lua_pushinteger(cbdata->L, integerVal);
+
+    (cbdata->push_fn)(cbdata);
+
     return 1;
 }
 static int yajl_map_key(void *ctx, const unsigned char *key, size_t stringLen)
 {
+    ibjson_cbdata_t *cbdata = (ibjson_cbdata_t *)ctx;
+
+    lua_pushlstring(cbdata->L, (const char *)key, stringLen);
+
     return 1;
 }
 static int yajl_null(void *ctx)
 {
+    ibjson_cbdata_t *cbdata = (ibjson_cbdata_t *)ctx;
+
+    lua_pushnil(cbdata->L);
+
+    (cbdata->push_fn)(cbdata);
+
     return 1;
 }
 static int yajl_number(void *ctx, const char *numberVal, size_t numberLen)
 {
+    ibjson_cbdata_t *cbdata = (ibjson_cbdata_t *)ctx;
+
+    lua_pushlstring(cbdata->L, (const char *)numberVal, numberLen);
+
+    (cbdata->push_fn)(cbdata);
+
     return 1;
 }
 static int yajl_start_array(void *ctx)
 {
+    ibjson_cbdata_t *cbdata = (ibjson_cbdata_t *)ctx;
+
+    lua_pushcfunction(cbdata->L, (lua_CFunction)cbdata->push_fn);
+
+    lua_newtable(cbdata->L);
+
+    cbdata->push_fn = ibjson_pushlist;
+
     return 1;
 }
 static int yajl_start_map(void *ctx)
 {
+    ibjson_cbdata_t *cbdata = (ibjson_cbdata_t *)ctx;
+
+    lua_pushcfunction(cbdata->L, (lua_CFunction)cbdata->push_fn);
+
+    lua_newtable(cbdata->L);
+
+    cbdata->push_fn = ibjson_pushmap;
+
     return 1;
 }
 static int yajl_string(void *ctx, const unsigned char *stringVal, size_t stringLen)
 {
+    ibjson_cbdata_t *cbdata = (ibjson_cbdata_t *)ctx;
+
+    lua_pushlstring(cbdata->L, (const char *)stringVal, stringLen);
+
+    (cbdata->push_fn)(cbdata);
+
     return 1;
 }
 
@@ -136,15 +265,20 @@ LUALIB_API int ibjson_parse_string(lua_State *L) {
         return luaL_error(L, "Cannot allocate memory pool.");
     }
 
+    cbdata.L = L;
+    cbdata.mp = mp;
+    cbdata.push_fn = &ibjson_pushval;
+            
     yajl_handle yajl = yajl_alloc(&g_yajl_callbacks, NULL, (void *)&cbdata);
-
     if (yajl == NULL) {
         ib_mpool_release(mp);
         return luaL_error(L, "Cannot allocate YAJL parser handle.");
     }
 
-    /* Start with nil on the stack. */
-    lua_pushnil(L);
+    yc = yajl_config(
+        yajl,
+        yajl_allow_comments,
+        yajl_allow_multiple_values);
 
     yc = yajl_parse(yajl, json_text, json_text_sz);
     switch(yc) {
