b7165be57b7fbc0befbfcbb62c82a564521b2d24
Sam Baskinger <basking2@yahoo.com>
2013-07-03 20:35:01 -0500

    persistence_framework: Implementing persistence framework module.

---
+++ modules/persistence_framework.h
@@ -0,0 +1,196 @@
+/*****************************************************************************
+ * Licensed to Qualys, Inc. (QUALYS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * QUALYS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ****************************************************************************/
+
+#ifndef _ENGINE__PERSISTENCE_FRAMEWORK_H_
+#define _ENGINE__PERSISTENCE_FRAMEWORK_H_
+
+/**
+ * @file
+ * @brief IronBee Engine --- Persistence Framework
+ *
+ * @author Sam Baskinger <sbaskinger@qualys.com>
+ */
+
+#include <ironbee/build.h>
+#include <ironbee/engine_types.h>
+#include <ironbee/field.h>
+#include <ironbee/hash.h>
+
+#include <stdarg.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @defgroup IronBeeEnginePersistenceFramework IronBee Persistence Framework
+ * @ingroup IronBeeEngine
+ *
+ * This is the API for the IronBee persistence framework.
+ *
+ * The persistence framework is designed to make it easy for a module
+ * writer to...
+ *
+ * - register persistence implementations at configuration time,
+ * - instantiate named instances of those implmentations,
+ * - link those named instances to collections or instantiate anonymous
+ *   instances of an implementation,
+ * @{
+ */
+
+typedef struct ib_pstnsfw_t ib_pstnsfw_t;
+
+typedef ib_status_t (* ib_pstnsfw_create_fn_t)(
+    ib_engine_t *ib,
+    ib_list_t   *params,
+    void       **impl,
+    void        *cbdata
+);
+typedef void (* ib_pstnsfw_destroy_fn_t)(
+    void *impl,
+    void *cbdata
+);
+typedef ib_status_t (* ib_pstnsfw_load_fn_t)(
+    void *impl,
+    ib_tx_t *tx,
+    const char *key,
+    ib_list_t *list,
+    void *cbdata
+);
+typedef ib_status_t (* ib_pstnsfw_store_fn_t)(
+    void *impl,
+    ib_tx_t *tx,
+    const char *key,
+    const ib_list_t *list,
+    void *cbdata
+);
+
+/**
+ * Create a new persistence framework.
+ * @param[in,out] ib The IronBee engine this persistence engine will be
+ *                created from and registered to.
+ *                The main memory pool of @a ib is used.
+ * @param[in] module The user's module.
+ * @param[out] pstnsfw The persistence framework object.
+ * @returns
+ * - IB_OK On success.
+ * - IB_EALLOC On Allocation error.
+ */
+ib_status_t DLL_PUBLIC ib_pstnsfw_create(
+    ib_engine_t   *ib,
+    ib_module_t   *module,
+    ib_pstnsfw_t **pstnsfw
+);
+
+/**
+ * Register a set of functions that handle a particular type.
+ *
+ * The callback functions and callback data values may all be NULL and that
+ * function is skipped. For example, read-only persistence stores
+ * may pass in a NULL @a store_fn.
+ *
+ * @param[in] pstnsfw The persistence instance.
+ * @param[in] ctx Configuration context the handler is defined in.
+ * @param[in] type The name of the type this handles.
+ *            Calls to ib_pstnsfw_create_store() should pass in this
+ *            type to create an store that uses this registration.
+ * @param[in] create_fn Create callback.
+ * @param[in] create_data Callback data.
+ * @param[in] destroy_fn Destroy callback.
+ * @param[in] destroy_data Callback data.
+ * @param[in] load_fn Load callback.
+ * @param[in] load_data Callback data.
+ * @param[in] store_fn Store callback.
+ * @param[in] store_data Callback data.
+ *
+ * @returns
+ * - IB_OK On success.
+ * - IB_EEXIST If @a name is already defined.
+ * - Other
+ */
+ib_status_t DLL_PUBLIC ib_pstnsfw_register_type(
+    ib_pstnsfw_t            *pstnsfw,
+    ib_context_t             *ctx,
+    const char              *type,
+    ib_pstnsfw_create_fn_t   create_fn,
+    void                    *create_data,
+    ib_pstnsfw_destroy_fn_t  destroy_fn,
+    void                    *destroy_data,
+    ib_pstnsfw_load_fn_t     load_fn,
+    void                    *load_data,
+    ib_pstnsfw_store_fn_t    store_fn,
+    void                    *store_data
+);
+
+/**
+ * Fetch a registered type handler and create and instance of that type.
+ *
+ * @param[in] pstnsfw The persistence instance.
+ * @param[in] ctx Configuration context the store is defined in.
+ * @param[in] type The type.
+ * @param[in] name The name to store the newly created type under.
+ * @param[in] params A list of strings (null-terminated @c char @c *)
+ *            passed to the @ref ib_pstnsfw_create_fn_t for the
+ *            given type.
+ *
+ * @sa ib_pstnsfw_register_type()
+ *
+ * @returns
+ * - IB_OK On success.
+ * - IB_EEXIST If @a name is already defined.
+ * - Other
+ */
+ib_status_t DLL_PUBLIC ib_pstnsfw_create_store(
+    ib_pstnsfw_t *pstnsfw,
+    ib_context_t *ctx,
+    const char   *type,
+    const char   *name,
+    ib_list_t    *params
+);
+
+/**
+ * Map a collection to a named store created by ib_pstnsfw_create_store().
+ *
+ * @param[in] pstnsfw The persistence instance.
+ * @param[in] ctx The configuration context this will be in.
+ * @param[in] name The name of the collectioin to map.
+ * @param[in] key The key to store the collection under in the store.
+ *            This will be expanded against the transaction fields.
+ * @param[in] store The store name.
+ * @returns
+ * - IB_OK On success.
+ * - IB_EINVAL If @a name is already defined.
+ * - Other on failure.
+ */
+ib_status_t DLL_PUBLIC ib_pstnsfw_map_collection(
+    ib_pstnsfw_t *pstnsfw,
+    ib_context_t *ctx,
+    const char   *name,
+    const char   *key,
+    const char   *store
+);
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _ENGINE__PERSISTENCE_FRAMEWORK_H_ */
---
+++ modules/persistence_framework_private.h
@@ -0,0 +1,160 @@
+/*****************************************************************************
+ * Licensed to Qualys, Inc. (QUALYS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * QUALYS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ****************************************************************************/
+
+#ifndef _ENGINE__PERSISTENCE_FRAMEWORK_PRIVATE_H_
+#define _ENGINE__PERSISTENCE_FRAMEWORK_PRIVATE_H_
+
+/**
+ * @file
+ * @brief IronBee Engine --- Persistence Framework
+ *
+ * @author Sam Baskinger <sbaskinger@qualys.com>
+ */
+
+#include <ironbee/build.h>
+#include <ironbee/engine_types.h>
+#include <ironbee/field.h>
+#include <ironbee/hash.h>
+#include <ironbee/module.h>
+#include "persistence_framework.h"
+
+#include <stdarg.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * This structure contains handlers for a paritcular type.
+ *
+ * A handler cannot store data, though. A handler must first be used
+ * to create an implmenetation instance. The implmemetnation instance
+ * plus its associated handler structure is a @ref ib_pstnsfw_store_t.
+ */
+struct ib_pstnsfw_handler_t {
+    const char              *type;         /**< The type this handles. */
+    ib_pstnsfw_create_fn_t   create_fn;    /**< Create an instance. */
+    void                    *create_data;  /**< Callback data. */
+    ib_pstnsfw_destroy_fn_t  destroy_fn;   /**< Destroy an instance. */
+    void                    *destroy_data; /**< Callback data. */
+    ib_pstnsfw_load_fn_t     load_fn;      /**< Load data from an instance. */
+    void                    *load_data;    /**< Callback data. */
+    ib_pstnsfw_store_fn_t    store_fn;     /**< Store data in an instance. */
+    void                    *store_data;   /**< Callback data. */
+};
+typedef struct ib_pstnsfw_handler_t ib_pstnsfw_handler_t;
+
+/**
+ * A store is an instances of a @ref ib_pstnsfw_handler_t.
+ *
+ * A @ref ib_pstnsfw_handler_t plus the implmementation data
+ * created by ib_pstnsfw_handler_t::create_fn is a @ref ib_pstnsfw_store_t.
+ */
+struct ib_pstnsfw_store_t {
+    const char           *name;    /**< The name this is hashed under. */
+
+    /**
+     * The handler.
+     *
+     * When a store is destroyed this is set to NULL.
+     */
+    ib_pstnsfw_handler_t *handler;
+    void                 *impl;    /**< User implementation data. */
+};
+typedef struct ib_pstnsfw_store_t ib_pstnsfw_store_t;
+
+/**
+ * A mapping is a key, a collection name, and a @ref ib_pstnsfw_store_t.
+ *
+ * This represents a mapping of a collection to persisted data via key.
+ *
+ * The key may be string constant or may be a variable to be expanded
+ * when storing or loading data.
+ */
+struct ib_pstnsfw_mapping_t {
+    const char *name;          /**< Collection Name. */
+    const char *key;           /**< The key the collection is stored under. */
+    ib_pstnsfw_store_t *store; /**< The store the data is in. */
+};
+typedef struct ib_pstnsfw_mapping_t ib_pstnsfw_mapping_t;
+
+/**
+ * Persistence framework structure given to a user module.
+ *
+ * The user's module keeps this handle and returns it to the 
+ * persistence module API to fetch the configuration information.
+ */
+struct ib_pstnsfw_t {
+    ib_engine_t *ib;             /**< The engine registered to. */
+    ib_module_t *pstnsfw_module; /**< The persistence framework's module. */
+    ib_module_t *user_module;    /**< User's module structure. */
+};
+
+/**
+ * The persistence framework module configuration type.
+ */
+struct ib_pstnsfw_cfg_t {
+    /**
+     * Map of type to ib_pstnsfw_handler_t.
+     *
+     * This is copied in new configuration contexts.
+     */
+    ib_hash_t   *handlers;
+
+    /**
+     * A map of named @ref ib_pstnsfw_store_t.
+     *
+     * Names stores are looked up here and linked to named collections
+     * in ib_pstnsfw_t::coll_list.
+     *
+     * This is copied in new configuration contexts.
+     */
+    ib_hash_t   *stores;
+
+    /**
+     * A list of ib_pstnsfw_mapping_t.
+     *
+     * This is copied in new configuration contexts.
+     */
+    ib_list_t   *coll_list;
+};
+typedef struct ib_pstnsfw_cfg_t ib_pstnsfw_cfg_t;
+
+/**
+ * An array of @ref ib_pstnsfw_cfg_t indexed by the user module number.
+ */
+struct ib_pstnsfw_modlist_t {
+    /**
+     * Array of NULL or @ref ib_pstnsfw_cfg_t.
+     *
+     * The index is the client module's index value. The stored 
+     * @ref ib_pstsnfw_cfg_t is the configuration for that
+     * context and that module managed by the persistence module
+     * on behalf of the user module.
+     *
+     * If an entry is NULL, then it means that no module has registered.
+     */
+    ib_array_t *configs;
+};
+typedef struct ib_pstnsfw_modlist_t ib_pstnsfw_modlist_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _ENGINE__PERSISTENCE_FRAMEWORK_PRIVATE_H_ */
