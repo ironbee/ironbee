abb3d58bb299f9474fbefe75393264541dd4cab7
Sam Baskinger <sbaskinger@qualys.com>
2013-07-16 08:54:34 -0500

    util/file.c: Adding ib_file_readall().

---
+++ include/ironbee/file.h
@@ -0,0 +1,75 @@
+/*****************************************************************************
+ * Licensed to Qualys, Inc. (QUALYS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * QUALYS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ****************************************************************************/
+
+#ifndef _UTIL__FILE_H_
+#define _UTIL__FILE_H_
+
+/**
+ * @file
+ * @brief IronBee --- Utility Functions
+ *
+ * @author Sam Baskinger <sbaskinger@qualys.com>
+ */
+
+#include <ironbee/build.h>
+#include <ironbee/types.h>
+#include <ironbee/mpool.h>
+
+#include <sys/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @defgroup IronBeeFile File Utilities
+ * @ingroup IronBeeUtil
+ *
+ * Functions to manipulate files.
+ *
+ * @{
+ */
+
+/**
+ * Read an entire file.
+ *
+ * @param[in] mp Memory pool.
+ * @param[in] file Path to the file.
+ * @param[out] out The resultant buffer of bytes.
+ * @param[out] sz The size of the buffer.
+ *
+ * @return
+ * - IB_OK On success.
+ * - IB_EINVAL Error stating the file. Errno is set.
+ * - IB_EOTHER Read error. Errno is set.
+ * - IB_EALLOC On allocation error.
+ */
+ib_status_t ib_file_readall(
+    ib_mpool_t  *mp,
+    const char  *file,
+    const void **out,
+    size_t      *sz
+);
+
+/**
+ * @}
+ */
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _UTIL__FILE_H_ */
---
+++ util/file.c
@@ -0,0 +1,93 @@
+/*****************************************************************************
+ * Licensed to Qualys, Inc. (QUALYS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * QUALYS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ****************************************************************************/
+
+/**
+ * @file
+ * @brief IronBee --- Utility Functions
+ */
+
+#include "ironbee_config_auto.h"
+
+#include <ironbee/file.h>
+
+#include <assert.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+ib_status_t ib_file_readall(
+    ib_mpool_t  *mp,
+    const char  *file,
+    const void **out,
+    size_t      *sz
+)
+{
+    assert(mp != NULL);
+    assert(file != NULL);
+    assert(out != NULL);
+    assert(sz != NULL);
+
+    int         fd;
+    void       *buf;
+    size_t      bufsz;
+    size_t      fill = 0;
+    struct stat stat_data;
+    int         sys_rc;
+
+    /* Stat the file to get its total size. */
+    sys_rc = stat(file, &stat_data);
+    if (sys_rc != 0) {
+        return IB_EINVAL;
+    }
+
+    fd = open(file, O_RDONLY);
+    if (fd == -1) {
+        return IB_EINVAL;
+    }
+
+    bufsz = stat_data.st_size;
+    buf = ib_mpool_alloc(mp, bufsz);
+    if (buf == NULL) {
+        return IB_EALLOC;
+    }
+
+    for (;;) {
+        ssize_t r = read(fd, buf + fill, bufsz - fill);
+
+        /* EOF. */
+        if (r == 0) {
+            assert(fill == bufsz);
+            *sz = fill;
+            *out = buf;
+            return IB_OK;
+        }
+
+        /* Read error. */
+        if (r < 0) {
+            return IB_EOTHER;
+        }
+
+        /* We read something. */
+        fill += r;
+        assert(fill <= bufsz);
+    }
+
+    /* Technically unreachable code. */
+    assert(0 && "Unreachable code.");
+}
