29c85a536b83a01587faa8cffaeb3b122248b575
Christopher Alfeld <calfeld@qualys.com>
2013-07-22 14:26:43 -0500

    predicate: Add simple tree_copy() function to dup a tree.

---
+++ predicate/tree_copy.cpp
@@ -0,0 +1,49 @@
+/*****************************************************************************
+ * Licensed to Qualys, Inc. (QUALYS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * QUALYS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ****************************************************************************/
+
+/**
+ * @file
+ * @brief Predicate --- Tree Copy Implementation
+ *
+ * @author Christopher Alfeld <calfeld@qualys.com>
+ */
+
+#include <predicate/tree_copy.hpp>
+#include <predicate/parse.hpp>
+
+using namespace std;
+
+namespace IronBee {
+namespace Predicate {
+
+node_p tree_copy(const node_cp& source, CallFactory factory)
+{
+    // Simply parse the sexpr.  This isn't the most efficient but is
+    // extremely simple.
+    size_t i = 0;
+    string sexpr = source->to_s();
+    if (sexpr.empty()) {
+        return node_p();
+    }
+    if (sexpr[0] == '(') {
+        return parse_call(sexpr, i, factory);
+    }
+    return parse_literal(sexpr, i);
+}
+
+} // Predicate
+} // IronBee
---
+++ predicate/tree_copy.hpp
@@ -0,0 +1,55 @@
+/*****************************************************************************
+ * Licensed to Qualys, Inc. (QUALYS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * QUALYS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ****************************************************************************/
+
+/**
+ * @file
+ * @brief Predicate --- Tree Copy
+ *
+ * Defines tree_copy() which duplicates a tree of nodes.
+ *
+ * @author Christopher Alfeld <calfeld@qualys.com>
+ */
+
+#ifndef __PREDICATE__TREE_COPY__
+#define __PREDICATE__TREE_COPY__
+
+#include <predicate/dag.hpp>
+
+namespace IronBee {
+namespace Predicate {
+
+class CallFactory;
+
+/**
+ * Construct a copy of the tree rooted at @a source.
+ *
+ * This function will create a copy of a tree, duplicating all nodes.  The
+ * returned result will be a tree with each node having at most one parent.
+ * That is, the result is independent of any DAG that @a source is part of.
+ *
+ * @param[in] source  Root of tree to copy.
+ * @param[in] factory Call factory that knows about all calls in @a source.
+ * @return Root of copied tree.
+ * @throw einval if @a source contains a call that @a factory does not know
+ *               about.
+ **/
+node_p tree_copy(const node_cp& source, CallFactory factory);
+
+} // Predicate
+} // IronBee
+
+#endif
