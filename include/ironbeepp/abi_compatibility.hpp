55523ad540dbf21a440ff4fa844b48b6a9554c59
Sam Baskinger <sbaskinger@qualys.com>
2013-07-29 23:16:48 -0500

    Bumping IronBeePP ABI Number.

--- include/ironbeepp/abi_compatibility.hpp
+++ include/ironbeepp/abi_compatibility.hpp
@@ -35,7 +35,7 @@
 namespace IronBee {
 
 // Update following line when IronBee ABI changes.
-#if IB_ABINUM != 201306110
+#if IB_ABINUM != 201307300
 #error "ABI mismatch between IronBee++ and IronBee"
 #endif
 
