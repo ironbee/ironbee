a0d0ddb8679f0068410b876fc2dec6c1ec910001
Christopher Alfeld <calfeld@qualys.com>
2013-08-21 09:03:58 -0500

    fast/generate: Add rudimentary error reporting (CID 1062893, CID 1062894)

--- fast/generate.cpp
+++ fast/generate.cpp
@@ -47,76 +47,82 @@ int main(int argc, char **)
         return 1;
     }
 
-    ia::Intermediate::Automata a;
-    vector<string> id_vector;
-    {
-        ia::buffer_t index_data;
-        ia::BufferAssembler index_assembler(index_data);
-        typedef map<string, size_t> id_index_map_t;
-        id_index_map_t id_index_map;
-        string line;
-        string pattern;
-        string id;
-        ia::Generator::aho_corasick_begin(a);
-        while (cin) {
-            getline(cin, line);
-            if (line.empty()) {
-                continue;
-            }
-            size_t first_space = line.find_first_of(' ');
-            if (first_space == string::npos) {
-                cerr << "Invalid manifest line: " << line << endl;
-                return 1;
-            }
-            pattern = line.substr(0, first_space);
-            id      = line.substr(first_space + 1, string::npos);
+    try {
+        ia::Intermediate::Automata a;
+        vector<string> id_vector;
+        {
+            ia::buffer_t index_data;
+            ia::BufferAssembler index_assembler(index_data);
+            typedef map<string, size_t> id_index_map_t;
+            id_index_map_t id_index_map;
+            string line;
+            string pattern;
+            string id;
+            ia::Generator::aho_corasick_begin(a);
+            while (cin) {
+                getline(cin, line);
+                if (line.empty()) {
+                    continue;
+                }
+                size_t first_space = line.find_first_of(' ');
+                if (first_space == string::npos) {
+                    cerr << "Invalid manifest line: " << line << endl;
+                    return 1;
+                }
+                pattern = line.substr(0, first_space);
+                id      = line.substr(first_space + 1, string::npos);
 
-            id_index_map_t::iterator iter = id_index_map.lower_bound(id);
-            if (iter == id_index_map.end() || iter->first != id) {
-                iter = id_index_map.insert(iter, make_pair(id, id_vector.size()));
-                id_vector.push_back(id);
+                id_index_map_t::iterator iter = id_index_map.lower_bound(id);
+                if (iter == id_index_map.end() || iter->first != id) {
+                    iter = id_index_map.insert(iter, make_pair(id, id_vector.size()));
+                    id_vector.push_back(id);
+                }
+
+                size_t index = iter->second;
+                if (index > numeric_limits<uint32_t>::max()) {
+                    cerr << "More than 2^32 indices!  Can't handle that many." << endl;
+                    return 1;
+                }
+                index_data.clear();
+                index_assembler.append_object(uint32_t(index));
+
+                ia::Generator::aho_corasick_add_pattern(a, pattern, index_data);
             }
 
-            size_t index = iter->second;
-            if (index > numeric_limits<uint32_t>::max()) {
-                cerr << "More than 2^32 indices!  Can't handle that many." << endl;
-                return 1;
-            }
-            index_data.clear();
-            index_assembler.append_object(uint32_t(index));
-
-            ia::Generator::aho_corasick_add_pattern(a, pattern, index_data);
+            ia::Generator::aho_corasick_finish(a);
         }
 
-        ia::Generator::aho_corasick_finish(a);
-    }
+        ia::Intermediate::breadth_first(a, ia::Intermediate::optimize_edges);
+        ia::Intermediate::deduplicate_outputs(a);
 
-    ia::Intermediate::breadth_first(a, ia::Intermediate::optimize_edges);
-    ia::Intermediate::deduplicate_outputs(a);
+        a.metadata()["Output-Type"] = "integer";
 
-    a.metadata()["Output-Type"] = "integer";
+        {
+            ia::buffer_t index;
+            ia::BufferAssembler assembler(index);
 
-    {
-        ia::buffer_t index;
-        ia::BufferAssembler assembler(index);
-
-        BOOST_FOREACH(const string& id, id_vector) {
-            // Note appending trailing NUL.
-            assembler.append_bytes(
-                reinterpret_cast<const uint8_t *>(id.c_str()),
-                id.length() + 1
-            );
+            BOOST_FOREACH(const string& id, id_vector) {
+                // Note appending trailing NUL.
+                assembler.append_bytes(
+                    reinterpret_cast<const uint8_t *>(id.c_str()),
+                    id.length() + 1
+                );
+            }
+            a.metadata()["Index"] = string(index.begin(), index.end());
         }
-        a.metadata()["Index"] = string(index.begin(), index.end());
-    }
-    {
-        ia::buffer_t index_size;
-        ia::BufferAssembler assembler(index_size);
-        assembler.append_object(uint32_t(id_vector.size()));
-        a.metadata()["IndexSize"] = string(index_size.begin(), index_size.end());
-    }
+        {
+            ia::buffer_t index_size;
+            ia::BufferAssembler assembler(index_size);
+            assembler.append_object(uint32_t(id_vector.size()));
+            a.metadata()["IndexSize"] = string(index_size.begin(), index_size.end());
+        }
 
-    ia::Intermediate::write_automata(a, cout, 0);
+        ia::Intermediate::write_automata(a, cout, 0);
+    }
+    catch (const exception& e) {
+        cerr << "Error: " << e.what() << endl;
+        return 1;
+    }
 
     return 0;
 }
