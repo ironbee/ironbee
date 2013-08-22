b8d8e4cffda307262db2cf3f82194dd5ad9e41cf
Christopher Alfeld <calfeld@qualys.com>
2013-08-22 13:58:01 -0500

    predicate/tests: Add (currently failing) args test.

--- predicate/tests/tc_lua_predicate.rb
+++ predicate/tests/tc_lua_predicate.rb
@@ -47,4 +47,21 @@ class TestLuaPredicate < Test::Unit::TestCase
     assert_no_issues
     assert_log_match /CLIPP ANNOUNCE: basic1/
   end
+
+  def test_args
+    lua = <<-EOS
+      Action("args1", "1"):
+        phase([[REQUEST]]):
+        action([[clipp_announce:args1]]):
+        predicate(P.Rx('foo|bar', P.Field('ARGS')))
+    EOS
+    lua_file = File.expand_path(File.join(BUILDDIR, rand(10000).to_s + '.lua'))
+    File.open(lua_file, 'w') {|fp| fp.print lua}
+
+    clipp(make_config(lua_file,
+      :input => "echo:\"GET /foo?a=foo&b=bar\""
+    ))
+    assert_no_issues
+    assert_log_match /CLIPP ANNOUNCE: args1/
+  end
 end
