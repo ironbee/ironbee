9e0f2c521dba3e111684f365b6dfcdbdef7a1fbc
Christopher Alfeld <calfeld@qualys.com>
2013-09-12 10:28:26 -0500

    Bump ABI version.

--- include/ironbeepp/abi_compatibility.hpp
+++ include/ironbeepp/abi_compatibility.hpp
@@ -35,7 +35,7 @@
 namespace IronBee {
 
 // Update following line when IronBee ABI changes.
-#if IB_ABINUM != 201309040
+#if IB_ABINUM != 201309120
 #error "ABI mismatch between IronBee++ and IronBee"
 #endif
 
