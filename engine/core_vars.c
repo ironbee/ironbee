d492ce34d8a7bc0479da860a8edd05d46b0856c7
Brian Rectanus <brectanus@qualys.com>
2013-10-17 17:40:28 -0700

    core: Move generating field values to earlier hooks.

--- engine/core_vars.c
+++ engine/core_vars.c
@@ -747,7 +747,6 @@ static ib_status_t core_gen_request_header_fields(ib_engine_t *ib,
 
     assert(ib != NULL);
     assert(tx != NULL);
-    assert(event == handle_request_header_event);
 
     core_gen_tx_bytestr_alias2(tx, "server_addr",
                                conn->local_ipstr,
@@ -839,7 +838,6 @@ static ib_status_t core_gen_request_body_fields(ib_engine_t *ib,
 
     assert(ib != NULL);
     assert(tx != NULL);
-    assert(event == handle_request_event);
 
     /* Populate the ARGS collection. */
     rc = core_slow_get(&f, tx, "ARGS");
@@ -892,7 +890,6 @@ static ib_status_t core_gen_response_header_fields(
 
     assert(ib != NULL);
     assert(tx != NULL);
-    assert(event == handle_response_header_event);
 
     if (tx->response_line != NULL) {
         core_gen_tx_bytestr_alias(tx, "response_line",
@@ -932,7 +929,6 @@ static ib_status_t core_gen_response_body_fields(ib_engine_t *ib,
 {
     assert(ib != NULL);
     assert(tx != NULL);
-    assert(event == handle_response_event);
 
     return IB_OK;
 }
@@ -982,16 +978,16 @@ ib_status_t ib_core_vars_init(ib_engine_t *ib,
     ib_hook_tx_register(ib, tx_started_event,
                         core_gen_flags_collection, NULL);
 
-    ib_hook_tx_register(ib, handle_request_header_event,
+    ib_hook_tx_register(ib, request_header_finished_event,
                         core_gen_request_header_fields, NULL);
 
-    ib_hook_tx_register(ib, handle_request_event,
+    ib_hook_tx_register(ib, request_finished_event,
                         core_gen_request_body_fields, NULL);
 
-    ib_hook_tx_register(ib, handle_response_header_event,
+    ib_hook_tx_register(ib, response_header_finished_event,
                         core_gen_response_header_fields, NULL);
 
-    ib_hook_tx_register(ib, handle_response_event,
+    ib_hook_tx_register(ib, response_finished_event,
                         core_gen_response_body_fields, NULL);
 
     config = ib_engine_var_config_get(ib);
