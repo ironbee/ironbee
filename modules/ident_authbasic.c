e0ec0db10b184281ad8b58be17bc869cb135b3c7
Nick Kew <nkew@qualys.com>
2013-11-22 22:37:49 +0000

    Remove redundant initialisation

--- modules/ident_authbasic.c
+++ modules/ident_authbasic.c
@@ -61,7 +61,7 @@ static const char *header_get(ib_mpool_t *pool,
                               ib_parsed_header_wrapper_t *wrapper,
                               const char *name)
 {
-    ib_parsed_name_value_pair_list_t *p = wrapper->head;
+    ib_parsed_name_value_pair_list_t *p;
     /* To check "last" condition in for() would be to omit the last element.
      * So check each element before checking for end-of-list
      */
