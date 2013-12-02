120ef211e14e40ff79bf865cd29c8b26cebd69be
Sam Baskinger <sbaskinger@qualys.com>
2013-12-02 14:30:08 -0600

    BlockingMethod: Fixing status code range check.

--- engine/core.c
+++ engine/core.c
@@ -3392,7 +3392,7 @@ static ib_status_t core_dir_param1(ib_cfgparser_t *cp,
             status_str = p1 + sizeof("status=")-1;
             status  = atoi(status_str);
 
-            if (!(status <= 200 && status < 600))
+            if (status < 200 || status >= 600)
             {
                 ib_log_debug2(
                     ib,
