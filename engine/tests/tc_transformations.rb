9ff01d11f2faa2d45f7708d011520f5760e9530d
Sam Baskinger <sbaskinger@qualys.com>
2013-12-05 01:26:01 -0600

    core first() / last(): Adding Clipp tests for first() and last() transformations.

---
+++ engine/tests/tc_transformations.rb
@@ -0,0 +1,78 @@
+class TestTransformations < Test::Unit::TestCase
+  include CLIPPTest
+
+  def test_tfn_first
+      clipp(
+      :input_hashes => [simple_hash("GET /foobar/a\n")],
+      :default_site_config => <<-EOS
+        Action id:1 rev:1 phase:REQUEST_HEADER setvar:list:element1=1
+        Action id:2 rev:1 phase:REQUEST_HEADER setvar:list:element2=2
+
+        Rule list.first() @eq 1 id:3 rev:1 phase:REQUEST_HEADER clipp_announce:result
+      EOS
+    )
+    assert_no_issues
+    assert_log_match /CLIPP ANNOUNCE: result/
+  end
+
+  def test_tfn_last
+      clipp(
+      :input_hashes => [simple_hash("GET /foobar/a\n")],
+      :default_site_config => <<-EOS
+        Action id:1 rev:1 phase:REQUEST_HEADER setvar:list:element1=1
+        Action id:2 rev:1 phase:REQUEST_HEADER setvar:list:element2=2
+
+        Rule list.last() @eq 2 id:3 rev:1 phase:REQUEST_HEADER clipp_announce:result
+      EOS
+    )
+    assert_no_issues
+    assert_log_match /CLIPP ANNOUNCE: result/
+  end
+
+  def test_tfn_first_noexist
+      clipp(
+      :input_hashes => [simple_hash("GET /foobar/a\n")],
+      :default_site_config => <<-EOS
+        Rule list.first() @eq 1 id:1 rev:1 phase:REQUEST_HEADER clipp_announce:result
+      EOS
+    )
+    assert_no_issues
+    assert_log_no_match /CLIPP ANNOUNCE: result/
+  end
+
+  def test_tfn_last_noexist
+      clipp(
+      :input_hashes => [simple_hash("GET /foobar/a\n")],
+      :default_site_config => <<-EOS
+        Rule list.last() @eq 1 id:1 rev:1 phase:REQUEST_HEADER clipp_announce:result
+      EOS
+    )
+    assert_no_issues
+    assert_log_no_match /CLIPP ANNOUNCE: result/
+  end
+
+  def test_tfn_first_notlist
+      clipp(
+      :input_hashes => [simple_hash("GET /foobar/a\n")],
+      :default_site_config => <<-EOS
+        Action id:1 rev:1 phase:REQUEST_HEADER setvar:list=1
+        Rule list.first() @eq 1 id:2 rev:1 phase:REQUEST_HEADER clipp_announce:result
+      EOS
+    )
+    assert_no_issues
+    assert_log_match /CLIPP ANNOUNCE: result/
+  end
+
+  def test_tfn_last_notlist
+      clipp(
+      :input_hashes => [simple_hash("GET /foobar/a\n")],
+      :default_site_config => <<-EOS
+        Action id:1 rev:1 phase:REQUEST_HEADER setvar:list=1
+        Rule list.last() @eq 1 id:2 rev:1 phase:REQUEST_HEADER clipp_announce:result
+      EOS
+    )
+    assert_no_issues
+    assert_log_match /CLIPP ANNOUNCE: result/
+  end
+end
+
--- engine/tests/ts_all.rb
+++ engine/tests/ts_all.rb
@@ -4,6 +4,7 @@ require '../../clipp/clipp_test'
 
 require 'tc_core'
 require 'tc_operators'
+require 'tc_transformations'
 require 'tc_auditlogs'
 require 'tc_regression'
 require 'tc_holistic'
