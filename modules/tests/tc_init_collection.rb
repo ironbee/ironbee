29d119ac99e15e9ae11b8428dcf292062b420fe1
Brian Rectanus <brectanus@qualys.com>
2014-01-18 11:42:29 -0600

    init_collection: Add more unit tests covering recent crashes.

--- modules/tests/tc_init_collection.rb
+++ modules/tests/tc_init_collection.rb
@@ -102,5 +102,73 @@ class TestInitCollection < Test::Unit::TestCase
 
     assert_no_issues
   end
+
+  def test_init_collection_matching_site
+    clipp(
+      :input_hashes => [simple_hash("GET /foobar\n", "HTTP/1.1 200 OK\n\n")],
+      :config => '''
+        LoadModule ibmod_persistence_framework.so
+        LoadModule ibmod_init_collection.so
+        LogLevel INFO
+
+        <Site will-not-match>
+          SiteId 57f2b6d0-7783-012f-86c6-001f5b320164
+          Hostname will-not-match
+
+          InitCollection COL1 vars: \
+              "A=1" \
+              "B=2"
+        </Site>
+
+        <Site will-match>
+          SiteId 57f2b6d0-7783-012f-86c6-001f5b320165
+          Hostname *
+
+          InitCollection COL1 vars: \
+              "A=1" \
+              "B=2"
+        </Site>
+      ''',
+      :default_site_config => <<-EOS
+        Rule COL1 @clipp_print 'COL1' id:1 rev:1 phase:REQUEST
+      EOS
+    )
+
+    assert_no_issues
+  end
+
+  def test_init_collection_no_matching_site
+    clipp(
+      :input_hashes => [simple_hash("GET /foobar\n", "HTTP/1.1 200 OK\n\n")],
+      :config => '''
+        LoadModule ibmod_persistence_framework.so
+        LoadModule ibmod_init_collection.so
+        LogLevel INFO
+
+        <Site will-not-match>
+          SiteId 57f2b6d0-7783-012f-86c6-001f5b320164
+          Hostname will-not-match
+
+          InitCollection COL1 vars: \
+              "A=1" \
+              "B=2"
+        </Site>
+
+        <Site will-not-match2>
+          SiteId 57f2b6d0-7783-012f-86c6-001f5b320164
+          Hostname will-not-match2
+
+          InitCollection COL1 vars: \
+              "A=1" \
+              "B=2"
+        </Site>
+      ''',
+      :default_site_config => <<-EOS
+        Rule COL1 @clipp_print 'COL1' id:1 rev:1 phase:REQUEST
+      EOS
+    )
+
+    assert_no_issues
+  end
 end
 
