ed269a18c832b1dff6457a30051c571f0bd029cd
Brian Rectanus <brectanus@qualys.com>
2014-02-12 23:05:47 -0600

    abi: Bump the ABI value after removing a rule engine flag and changing other flag values.

--- include/ironbeepp/abi_compatibility.hpp
+++ include/ironbeepp/abi_compatibility.hpp
@@ -35,7 +35,7 @@
 namespace IronBee {
 
 // Update following line when IronBee ABI changes.
-#if IB_ABINUM != 201312180
+#if IB_ABINUM != 201402120
 #error "ABI mismatch between IronBee++ and IronBee"
 #endif
 
