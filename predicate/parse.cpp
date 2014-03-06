3dfe87492445f0066b425b3dd60ebc4005ba38b2
Christopher Alfeld <calfeld@qualys.com>
2014-03-06 14:05:41 -0600

    predicate/parse.cpp: Refactor in preparation for named literals. RNS-661

--- predicate/parse.cpp
+++ predicate/parse.cpp
@@ -63,13 +63,20 @@ void advance(size_t& i, size_t length, const string& msg)
     }
 }
 
-bool name_char(char c)
+bool first_name_char(char c)
 {
     return
         ( c >= 'a' && c <= 'z' ) ||
         ( c >= 'A' && c <= 'Z' ) ||
+        c == '_'
+        ;
+}
+
+bool name_char(char c)
+{
+    return first_name_char(c) ||
         ( c >= '0' && c <= '9' ) ||
-        c == '_' || c == '-'
+        c == '.' || c == '-'
         ;
 }
 
@@ -78,6 +85,89 @@ bool num_char(char c)
     return
         ( c >= '0' && c <= '9' )
         ;
+}
+
+Value parse_string(
+    const std::string& text,
+    size_t&            i,
+    MemoryManager      mm
+)
+{
+    size_t length = text.length();
+    bool escape = false;
+    string value;
+
+    if (text[i] != '\'') {
+        error(i, "Expected '");
+    }
+    advance(i, length, "Unterminated literal");
+    while (text[i] != '\'' || escape) {
+        if (text[i] == '\\' && ! escape) {
+            escape = true;
+        }
+        else {
+            value += text[i];
+            escape = false;
+        }
+        advance(i, length, "Unterminated literal");
+    }
+    return Field::create_byte_string(mm, "", 0,
+        ByteString::create(mm, value)
+    );
+}
+
+Value parse_number(
+    const std::string& text,
+    size_t&            i,
+    MemoryManager      mm
+)
+{
+    bool have_dot = false;
+    size_t initial_i = i;
+    size_t length = text.length();
+
+    if (text[i] == '-') {
+        advance(i, length, "Unterminated literal");
+    }
+
+    while (
+        i < length &&
+        (num_char(text[i]) || text[i] == '.')
+    ) {
+        if (text[i] == '.') {
+            if (have_dot) {
+                error(i, "Multiple dots in numeric.");
+            }
+            have_dot = true;
+        }
+        ++i;
+    }
+
+    string value = text.substr(initial_i, i - initial_i);
+    // Reduce i to match caller expectation: that i points to final
+    // character of literal.
+    --i;
+
+    if (have_dot) {
+        long double fvalue;
+        try {
+            fvalue = boost::lexical_cast<long double>(value);
+        }
+        catch (boost::bad_lexical_cast) {
+            error(i, "Could not convert to float.");
+        }
+        return Field::create_float(mm, "", 0, fvalue);
+    }
+    else {
+        int64_t ivalue;
+        try {
+            ivalue = boost::lexical_cast<int64_t>(value);
+        }
+        catch (boost::bad_lexical_cast) {
+            error(i, "Could not convert to integer.");
+        }
+        return Field::create_number(mm, "", 0, ivalue);
+    }
 }
 
 /**
@@ -97,87 +187,38 @@ ValueList parse_literal_values(
     List<Value> values = List<Value>::create(mm);
 
     size_t length = text.length();
-    bool escape = false;
-    string value;
 
-    // Null Literal
-    if (text.substr(i, 2) == "[]") {
-        i += 1;
-        return values;
-    }
-
-    // Number Literal
-    if (num_char(text[i]) || text[i] == '-') {
-        bool have_dot = false;
-        size_t initial_i = i;
-
-        if (text[i] == '-') {
+    switch (text[i]) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        case '-':
+            // Number
+            values.push_back(parse_number(text, i, mm));
+            break;
+        case '\'':
+            // String
+            values.push_back(parse_string(text, i, mm));
+            break;
+        case '[':
+            // Null
             advance(i, length, "Unterminated literal");
-        }
-
-        while (
-            i < length &&
-            (num_char(text[i]) || text[i] == '.')
-        ) {
-            if (text[i] == '.') {
-                if (have_dot) {
-                    error(i, "Multiple dots in numeric.");
-                }
-                have_dot = true;
+            if (text[i] != ']') {
+                error(i, string("Unexpected character after [") + text[i]);
             }
-            ++i;
-        }
-
-        value = text.substr(initial_i, i - initial_i);
-        // Reduce i to match caller expectation: that i points to final
-        // character of literal.
-        --i;
-
-        if (have_dot) {
-            long double fvalue;
-            try {
-                fvalue = boost::lexical_cast<long double>(value);
-            }
-            catch (boost::bad_lexical_cast) {
-                error(i, "Could not convert to float.");
-            }
-            values.push_back(Field::create_float(mm, "", 0, fvalue));
-            return values;
-        }
-        else {
-            int64_t ivalue;
-            try {
-                ivalue = boost::lexical_cast<int64_t>(value);
-            }
-            catch (boost::bad_lexical_cast) {
-                error(i, "Could not convert to integer.");
-            }
-            values.push_back(Field::create_number(mm, "", 0, ivalue));
-            return values;
-        }
+            break;
+        default:
+            error(i, string("Unexpected character ") + text[i]);
     }
 
-    // String Literal
-    if (text[i] != '\'') {
-        error(i, "Expected '");
-    }
-    advance(i, length, "Unterminated literal");
-    while (text[i] != '\'' || escape) {
-        if (text[i] == '\\' && ! escape) {
-            escape = true;
-        }
-        else {
-            value += text[i];
-            escape = false;
-        }
-        advance(i, length, "Unterminated literal");
-    }
-    values.push_back(
-        Field::create_byte_string(mm, "", 0,
-            ByteString::create(mm, value)
-        )
-    );
-    return values;
+   return values;
 }
 
 }
@@ -212,66 +253,51 @@ node_p parse_call(
 
     while (i < length && ! done) {
         switch (text[i]) {
-        case ' ':
-            advance(i, length, "Unterminated call");
-            break;
-        case '(': {
-            string op;
-            advance(i, length, "Unterminated call");
-            while (name_char(text[i])) {
-                op += text[i];
+            case ' ':
                 advance(i, length, "Unterminated call");
+                break;
+            case '(': {
+                string op;
+                advance(i, length, "Unterminated call");
+                while (name_char(text[i])) {
+                    op += text[i];
+                    advance(i, length, "Unterminated call");
+                }
+                if (op.empty()) {
+                    error(i, "Missing operation");
+                }
+                node_p n = factory(op);
+                if (! top) {
+                    // Very important to keep all our nodes in memory.
+                    top = n;
+                }
+                if (current) {
+                    current->add_child(n);
+                }
+                current = n;
+                break;
             }
-            if (op.empty()) {
-                error(i, "Missing operation");
+            case ')':
+                if (! current) {
+                    error(i, "Too many )");
+                }
+                else if (current->parents().empty()) {
+                    done = true;
+                }
+                else {
+                    current = current->parents().front().lock();
+                    advance(i, length, "Expected )");
+                }
+                break;
+            default: {
+                if (! current) {
+                    error(i, "Naked literal");
+                }
+                current->add_child(parse_literal(text, i));
+                assert(text[i] == '\'' || text[i] == ']' || num_char(text[i]));
+                advance(i, length, "Unterminated call");
+                break;
             }
-            node_p n = factory(op);
-            if (! top) {
-                // Very important to keep all our nodes in memory.
-                top = n;
-            }
-            if (current) {
-                current->add_child(n);
-            }
-            current = n;
-            break;
-        }
-        case ')':
-            if (! current) {
-                error(i, "Too many )");
-            }
-            else if (current->parents().empty()) {
-                done = true;
-            }
-            else {
-                current = current->parents().front().lock();
-                advance(i, length, "Expected )");
-            }
-            break;
-        case '\'':
-        case '[':
-        case '0':
-        case '1':
-        case '2':
-        case '3':
-        case '4':
-        case '5':
-        case '6':
-        case '7':
-        case '8':
-        case '9':
-        case '-':
-        {
-            if (! current) {
-                error(i, "Naked literal");
-            }
-            current->add_child(parse_literal(text, i));
-            assert(text[i] == '\'' || text[i] == ']' || num_char(text[i]));
-            advance(i, length, "Unterminated call");
-            break;
-        }
-        default:
-            error(i, string("Unexpected character ") + text[i]);
         }
     }
     if (! top) {
