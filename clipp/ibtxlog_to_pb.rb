615fd5e11f4fc209772ff4beb6ed3a3e9405f519
Christopher Alfeld <calfeld@qualys.com>
2014-03-26 13:05:40 -0500

    clipp/ibtxlog_to_pb.rb: Refactor into ClippScript plugin.  RNS-980

--- clipp/ibtxlog_to_pb.rb
+++ clipp/ibtxlog_to_pb.rb
@@ -10,73 +10,73 @@ $:.unshift(File.dirname(__FILE__))
 require 'clippscript'
 require 'hash_to_pb'
 
-if ! ARGV.empty?
-  STDERR.puts "Usage: #{$0}"
-  STDERR.puts "Example: cat txlog-* | #{$0} | clipp pb:- @parse ironbee:ironbee.conf"
-  exit 1
-end
-
-class ClippEmitter
-  def initialize(to)
-    @to = to
-    @conn_id = nil
-    @proxy = nil
-  end
-
-  def convert_headers(json)
-    result = {}
-    json.each do |header|
-      result[header['name']] = header['value']
-    end
-    result
-  end
-
-  def transaction(json)
-    incoming_conn_id = json['connection']['id']
-    if incoming_conn_id != @conn_id
-      flush
-      @conn_id = incoming_conn_id
-      @proxy = ClippScript::proxy(id: @conn_id)
-
-      @proxy.connection_opened(
-        local_ip:    json['connection']['clientIp'],
-        local_port:  json['connection']['clientPort'],
-        remote_ip:   json['connection']['serverIp'],
-        remote_port: json['connection']['serverPort'],
-      )
-    end
-    @proxy.transaction do |t|
-      req = json['request']
-      t.request(
-        method:   req['method'],
-        uri:      req['uri'],
-        protocol: req['protocol'],
-        headers:  convert_headers(req['headers'])
-      )
-      resp = json['response']
-      t.response(
-        protocol: resp['protocol'],
-        status:   resp['status'],
-        message:  resp['message'],
-        headers:  convert_headers(resp['headers'])
-      )
-    end
-  end
-
-  def flush
-    if @proxy
-      @proxy.connection_closed
-      IronBee::CLIPP::HashToPB::write_hash_to_pb(@to, @proxy.result)
-      @proxy = nil
+module ClippScript
+  class Environment
+    def from_ibtxlog(io)
+      conn_id = nil
+      input = nil
+      flush = lambda do
+        if input
+          input.connection_closed
+          emit(input.result)
+          input = nil
+        end
+      end
+      convert_headers = lambda do |json|
+        result = {}
+        json.each do |header|
+          result[header['name']] = header['value']
+        end
+        result
+      end
+      
+      io.each do |line|
+        json = JSON::parse(line)        
+        incoming_conn_id = json['connection']['id']
+        if incoming_conn_id != conn_id
+          flush[]
+          conn_id = incoming_conn_id
+          input = proxy(id: conn_id)
+        
+          input.connection_opened(
+            local_ip:    json['connection']['clientIp'],
+            local_port:  json['connection']['clientPort'],
+            remote_ip:   json['connection']['serverIp'],
+            remote_port: json['connection']['serverPort'],
+          )
+        end
+        input.transaction do |t|
+          req = json['request']
+          t.request(
+            method:   req['method'],
+            uri:      req['uri'],
+            protocol: req['protocol'],
+            headers:  convert_headers[req['headers']]
+          )
+          resp = json['response']
+          t.response(
+            protocol: resp['protocol'],
+            status:   resp['status'],
+            message:  resp['message'],
+            headers:  convert_headers[resp['headers']]
+          )
+        end
+      end
+      
+      flush[]
     end
   end
 end
 
-input = STDIN
-
-emitter = ClippEmitter.new(STDOUT)
-input.each_line do |tx_json|
-  tx = JSON.parse(tx_json)
-  emitter.transaction(tx)
+if __FILE__ == $0
+  if ! ARGV.empty?
+    STDERR.puts "Usage: #{$0}"
+    STDERR.puts "Example: cat txlog-* | #{$0} | clipp pb:- @parse ironbee:ironbee.conf"
+    exit 1
+  end
+  
+  input = STDIN
+  ClippScript.eval_io(STDOUT) do
+    from_ibtxlog(STDIN)
+  end
 end
-emitter.flush
