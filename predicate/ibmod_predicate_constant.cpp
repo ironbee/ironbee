4201ab3d7907fe8e57fa8529d04b87901af7f10f
Christopher Alfeld <calfeld@qualys.com>
2014-04-10 17:28:46 -0500

    predicate: Add ibmod_predicate_constant which provides the constant function.

---
+++ predicate/ibmod_predicate_constant.cpp
@@ -0,0 +1,89 @@
+/*****************************************************************************
+ * Licensed to Qualys, Inc. (QUALYS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * QUALYS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ****************************************************************************/
+
+/**
+ * @file
+ * @brief IronBee --- Predicate-Constant Module
+ *
+ * This module adds the `constant` function to Predicate.  It must be loaded
+ * after both `ibmod_predicate` and `ibmod_constant`.
+ *
+ * @author Christopher Alfeld <calfeld@qualys.com>
+ */
+
+#include <ironbeepp/module_bootstrap.hpp>
+
+#include <predicate/call_factory.hpp>
+#include <predicate/functional.hpp>
+#include <predicate/reporter.hpp>
+#include <predicate/ibmod_predicate.hpp>
+
+#include <modules/constant.h>
+
+namespace {
+    
+namespace P = IronBee::Predicate;
+
+//! Predicate `constant` function.
+class Constant :
+    public P::Functional::Simple
+{
+public:
+    //! Constructor.
+    Constant() : P::Functional::Simple(1, 0) {}
+    
+    //! Prepare -- do everything.
+    bool prepare(
+        IronBee::MemoryManager,
+        const P::Functional::value_vec_t& static_args,
+        P::Environment environment,
+        P::NodeReporter
+    )
+    {
+        P::Value key = static_args.front();
+        m_value = P::Value(
+            IronBee::Constant::get(environment, key.as_string())
+        );
+        return true;
+    }
+ 
+protected:   
+    //! Return the value we found in prepare().
+    P::Value eval_simple(
+        IronBee::MemoryManager,
+        const P::Functional::value_vec_t&
+    ) const
+    {
+        return m_value;
+    }
+    
+private:
+    //! Constant value found by prepare().
+    P::Value m_value;
+};
+
+//! Mdule load function.
+void load(IronBee::Module module)
+{
+    IBModPredicateCallFactory(module.engine()).add(
+        "constant", P::Functional::generate<Constant>
+    );
+}
+
+}
+
+IBPP_BOOTSTRAP_MODULE("predicate_constant", load);
---
+++ predicate/tests/tc_predicate_constant.rb
@@ -0,0 +1,23 @@
+require File.join(File.dirname(__FILE__), '..', '..', 'clipp', 'clipp_test')
+
+class TestPredicate < Test::Unit::TestCase
+  include CLIPPTest
+
+  def test_predicate_constant
+    clipp(
+      predicate: true,
+      modules: ['constant'],
+      config: "LoadModule \"#{TOP_BUILDDIR}/predicate/.libs/ibmod_predicate_constant.so\"",
+      default_site_config: <<-EOS
+        ConstantSet Foo Bar
+        Action id:1 phase:REQUEST_HEADER clipp_announce:YES "predicate:(eq 'Bar' (constant 'Foo'))"
+        Action id:2 phase:REQUEST_HEADER clipp_announce:NO "predicate:(eq 'Bar' (constant 'Baz'))"
+      EOS
+    ) do
+      transaction {|t| t.request(raw: 'GET /')}
+    end
+    assert_no_issues
+    assert_log_match /CLIPP ANNOUNCE: YES/
+    assert_log_no_match /CLIPP ANNOUNCE: NO/
+  end
+end
