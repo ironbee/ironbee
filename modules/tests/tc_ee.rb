e25ae0aa449d661a3a650668757ebdbd1b7abee7
Christopher Alfeld <calfeld@qualys.com>
2014-06-12 13:19:28 -0500

    modules/tests/tc_ee.rb: Generate automata at test time.

--- modules/tests/tc_ee.rb
+++ modules/tests/tc_ee.rb
@@ -1,16 +1,63 @@
+require '../../clipp/clipp_test'
+
 # Also see test_module_ee_oper.cpp
 # TODO: move test_module_ee_oper tests into this file.
 
 class TestEE < Test::Unit::TestCase
   include CLIPPTest
 
-  # echo foo | trie_generator > ee_non_streaming.a
-  # ec ee_non_streaming.a
+  ABIN = File.join(TOP_BUILDDIR, 'automata', 'bin')
+  TRIE = File.join(ABIN, 'trie_generator')
+  AC = File.join(ABIN, 'ac_generator')
+  EC = File.join(ABIN, 'ec')
+
+  def trie(input, output)
+    File.open(output, 'w') do |out|
+      IO.popen(TRIE, 'r+') do |io|
+        io.puts input.join("\n")
+        io.close_write
+        out.write io.read
+      end
+    end
+    ec(output)
+  end
+
+  def ac(input, output)
+    File.open(output, 'w') do |out|
+      IO.popen(AC, 'r+') do |io|
+        io.puts input.join("\n")
+        io.close_write
+        out.write io.read
+      end
+    end
+    ec(output)
+  end
+
+  def acp(input, output)
+    File.open(output, 'w') do |out|
+      IO.popen([AC, '-p'], 'r+') do |io|
+        io.puts input.join("\n")
+        io.close_write
+        out.write io.read
+      end
+    end
+    ec(output)
+  end
+
+  def ec(input)
+    system(EC, input)
+    input.gsub(/\.a$/, '.e')
+  end
+
   def test_non_streaming
+    id = generate_id
+    prefix = "#{BUILDDIR}/clipp_test_non_streaming_#{id}"
+    e = trie(['foo'], "#{prefix}.a")
     clipp(
+      id: id,
       modules: ['htp', 'ee'],
       default_site_config: <<-EOS
-        LoadEudoxus "test" "ee_non_streaming.e"
+        LoadEudoxus "test" "#{e}"
         Rule ARGS @ee test id:1 phase:REQUEST_HEADER "clipp_announce:MATCH=%{FIELD_NAME}"
       EOS
     ) do
@@ -25,10 +72,14 @@ class TestEE < Test::Unit::TestCase
   end
 
   def test_ee_match_trie
+    id = generate_id
+    prefix = "#{BUILDDIR}/clipp_test_ee_match_trie#{id}"
+    e = trie(['foo'], "#{prefix}.a")
     clipp(
+      id: id,
       modules: ['htp', 'ee'],
       default_site_config: <<-EOS
-        LoadEudoxus "test" "ee_non_streaming.e"
+        LoadEudoxus "test" "#{e}"
         Rule ARGS @ee_match test id:1 phase:REQUEST_HEADER "clipp_announce:MATCH=%{FIELD_NAME}"
       EOS
     ) do
@@ -42,13 +93,15 @@ class TestEE < Test::Unit::TestCase
     assert_log_match /CLIPP ANNOUNCE: MATCH=c/
   end
 
-  # echo 'foo' | ac_generator > ac_foo.a
-  # ec ac_foo.a
   def test_ee_match_ac
+    id = generate_id
+    prefix = "#{BUILDDIR}/clipp_test_ee_match_ac#{id}"
+    e = ac(['foo'], "#{prefix}.a")
     clipp(
+      id: id,
       modules: ['htp', 'ee'],
       default_site_config: <<-EOS
-        LoadEudoxus "test" "ac_foo.e"
+        LoadEudoxus "test" "#{e}"
         Rule ARGS @ee_match test id:1 phase:REQUEST_HEADER "clipp_announce:MATCH=%{FIELD_NAME}"
       EOS
     ) do
@@ -64,13 +117,15 @@ class TestEE < Test::Unit::TestCase
     assert_log_no_match /CLIPP ANNOUNCE: MATCH=e/
   end
 
-  # echo 'foo\d\d' | ac_generator -p > ac_foo_pattern.a
-  # ec ac_foo_pattern.a
   def test_ee_match_ac_pattern
+    id = generate_id
+    prefix = "#{BUILDDIR}/clipp_test_ee_match_ac_pattern#{id}"
+    e = acp(['foo\d\d'], "#{prefix}.a")
     clipp(
+      id: id,
       modules: ['htp', 'ee'],
       default_site_config: <<-EOS
-        LoadEudoxus "test" "ac_foo_pattern.e"
+        LoadEudoxus "test" "#{e}"
         Rule ARGS @ee test id:1 phase:REQUEST_HEADER "clipp_announce:MATCH=%{FIELD_NAME}"
       EOS
     ) do
@@ -101,25 +156,54 @@ class TestEE < Test::Unit::TestCase
     assert_log_no_match /ENOENT/
   end
 
-  # def test_streaming
-  #   clipp(
-  #     input: 'pb:INPUT_PATH',
-  #     modules: ['htp', 'ee'],
-  #     default_site_config: <<-EOS
-  #       LoadEudoxus "test" "ee_non_streaming.e"
-  #       StreamInspect request_body_stream @ee test capture id:1 \
-  #         "clipp_announce:MATCH=%{CAPTURE:0}"
-  #     EOS
-  #   ) do
-  #     transaction do |t|
-  #       t.request_started(raw: "GET / HTTP/1.1")
-  #       t.request_header(
-  #         headers: {"Content-Length" => "6"}
-  #       )
-  #       "foobar".each_char {|c| t.request_body(data: c)}
-  #     end
-  #   end
-  #   assert_no_issues
-  #   assert_log_match 'CLIPP ANNOUNCE: MATCH=foo'
-  # end
+  def test_streaming
+    id = generate_id
+    prefix = "#{BUILDDIR}/clipp_test_streaming#{id}"
+    e = trie(['foo'], "#{prefix}.a")
+    clipp(
+      id: id,
+      input: 'pb:INPUT_PATH',
+      modules: ['htp', 'ee'],
+      default_site_config: <<-EOS
+        LoadEudoxus "test" "#{e}"
+        StreamInspect request_body_stream @ee test capture id:1 \
+          "clipp_announce:MATCH=%{CAPTURE:0}"
+      EOS
+    ) do
+      transaction do |t|
+        t.request_started(raw: "GET / HTTP/1.1")
+        t.request_header(
+          headers: {"Content-Length" => "6"}
+        )
+        "foobar".each_char {|c| t.request_body(data: c)}
+      end
+    end
+    assert_no_issues
+    assert_log_match 'CLIPP ANNOUNCE: MATCH=foo'
+  end
+
+  def test_streaming2
+    id = generate_id
+    prefix = "#{BUILDDIR}/clipp_test_streaming2#{id}"
+    e = trie(['foo'], "#{prefix}.a")
+    clipp(
+      input: 'pb:INPUT_PATH',
+      modules: ['htp', 'ee'],
+      default_site_config: <<-EOS
+        LoadEudoxus "test" "#{e}"
+        StreamInspect request_body_stream @ee test capture id:1 \
+          "clipp_announce:MATCH=%{CAPTURE:0}"
+      EOS
+    ) do
+      transaction do |t|
+        t.request_started(raw: "GET / HTTP/1.1")
+        t.request_header(
+          headers: {"Content-Length" => "6"}
+        )
+        t.request_body(data: "foobar")
+      end
+    end
+    assert_no_issues
+    assert_log_match 'CLIPP ANNOUNCE: MATCH=foo'
+  end
 end
