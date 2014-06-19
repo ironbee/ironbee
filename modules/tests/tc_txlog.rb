d28ab6530d7667e7ceba6b53afd7fa7dd4fb3c94
Sam Baskinger <sbaskinger@qualys.com>
2014-06-19 13:24:29 -0500

    txlog.cpp: If the var source for REQUEST_HEADER_ORDER or RESPONSE_HEADER_ORDER is present, include it in the TxLog.

--- modules/tests/tc_txlog.rb
+++ modules/tests/tc_txlog.rb
@@ -92,4 +92,23 @@ class TestTxLog < Test::Unit::TestCase
     assert_no_issues
     assert_log_no_match /{"timestamp":.*}/
   end
+
+  def test_txlog_header_order
+    clipp(
+      modules: %w{ header_order txlog },
+      config: <<-EOS
+        TxLogEnabled on
+        TxLogIronBeeLog on
+      EOS
+    ) do
+      transaction do |t|
+        t.request(raw: "GET / HTTP/1.1\r\nHost: foo\r\n\r\n")
+        t.response(raw: "HTTP/1.1 200 OK")
+      end
+    end
+
+    assert_no_issues
+    assert_log_match /"requestHeaderOrder":/
+    assert_log_match /"responseHeaderOrder":/
+  end
 end
