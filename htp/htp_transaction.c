5d8a1fbe67dae8e5ac98b7ea60b1c0120391c9ac
Brian Rectanus <brectanus@qualys.com>
2014-08-01 10:36:29 -0500

    Squashed 'libs/libhtp/' changes from a3e907f..440da09
    440da09 Update for 0.5.15.
    e51944a While verifying the value of the "Transfer-Encoding" header, libhtp does a case-sensitive comparison for the value "chunked", thus pushing out case-sensitive values such as "Chunked".
    ceedcef Update for 0.5.14.
    
    git-subtree-dir: libs/libhtp
    git-subtree-split: 440da09a8a83f1136599139b5e150d85eda5bee4

--- htp/htp_transaction.c
+++ htp/htp_transaction.c
@@ -371,7 +371,7 @@ static htp_status_t htp_tx_process_request_headers(htp_tx_t *tx) {
         //      (2.2.22 on Ubuntu 12.04 LTS) instead errors out with "Unknown Transfer-Encoding: identity".
         //      And it behaves strangely, too, sending a 501 and proceeding to process the request
         //      (e.g., PHP is run), but without the body. It then closes the connection.
-        if (bstr_cmp_c(te->value, "chunked") != 0) {
+        if (bstr_cmp_c_nocase(te->value, "chunked") != 0) {
             // Invalid T-E header value.
             tx->request_transfer_coding = HTP_CODING_INVALID;
             tx->flags |= HTP_REQUEST_INVALID_T_E;
