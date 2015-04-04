e090cf7814ee81f56f1f5270c8b3aaace2372d38
Sam Baskinger <sbaskinger@qualys.com>
2015-04-03 21:09:12 -0500

    gtest_executor.sh: Temporarily remove redirection of stderr to log file.

--- tests/gtest_executor.sh
+++ tests/gtest_executor.sh
@@ -6,8 +6,7 @@ test_prg="$1"
 test_name=`basename "$test_prg"`
 
 "$test_prg" \
-    --gtest_output=xml:"${test_name}_details.xml" \
-    2> "${test_name}_stderr.log"
+    --gtest_output=xml:"${test_name}_details.xml" 
 
 rc=$?
 
