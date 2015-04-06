3c175dd9dae27e27ac93a38ab0ef0776882ba6b2
Brian Rectanus <brectanus@qualys.com>
2015-04-06 08:56:24 -0500

    Revert "gtest_executor.sh: Temporarily remove redirection of stderr to log file."
    This reverts commit e090cf7814ee81f56f1f5270c8b3aaace2372d38.

--- tests/gtest_executor.sh
+++ tests/gtest_executor.sh
@@ -6,7 +6,8 @@ test_prg="$1"
 test_name=`basename "$test_prg"`
 
 "$test_prg" \
-    --gtest_output=xml:"${test_name}_details.xml" 
+    --gtest_output=xml:"${test_name}_details.xml" \
+    2> "${test_name}_stderr.log"
 
 rc=$?
 
