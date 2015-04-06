0423687dda3ef049d3f4312defd3b2f28f69ebd9
Brian Rectanus <brectanus@qualys.com>
2015-04-06 09:24:46 -0500

    build: Allow setting VERBOSE_TESTS=1 to bypass the test stderr files and just output to stderr.

--- tests/gtest_executor.sh
+++ tests/gtest_executor.sh
@@ -1,17 +1,30 @@
 #!/bin/sh
 
 # This script executes GTest tests in a controlled manner.
+# Setting VERBOSE_TESTS lets output go to stderr instead of a log.
 
 test_prg="$1"
 test_name=`basename "$test_prg"`
+rc=0
 
-"$test_prg" \
-    --gtest_output=xml:"${test_name}_details.xml" \
-    2> "${test_name}_stderr.log"
+if [ "0${VERBOSE_TESTS}" -eq 0 ]; then
+    "$test_prg" \
+        --gtest_output=xml:"${test_name}_details.xml" \
+        2> "${test_name}_stderr.log"
+    rc=$?
 
-rc=$?
+    if [ $rc != 0 ]; then
+        echo "Test $test_prg exited non-zero."
+    fi
+else
+    echo "Running $test_prg..."
+    "$test_prg" \
+        --gtest_output=xml:"${test_name}_details.xml"
+    rc=$?
 
-if [ $rc != 0 ]; then
-    echo "Test $test_prg exited non-zero. See ${test_name}_stderr.log for details."
-    exit $rc
+    if [ $rc != 0 ]; then
+        echo "Test $test_prg exited non-zero. See ${test_name}_stderr.log for details."
+    fi
 fi
+
+exit $rc
