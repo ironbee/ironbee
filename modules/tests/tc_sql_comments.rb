f3a79e49cfbdba8a3413c58b9ba168d7f6f3fde7
Sam Baskinger <sbaskinger@qualys.com>
2015-07-30 22:27:37 -0500

    sql_comments.cpp: SQL embedded comments should support removing /*![integer] */. RNS-1586.

--- modules/tests/tc_sql_comments.rb
+++ modules/tests/tc_sql_comments.rb
@@ -91,4 +91,34 @@ class TestSql < CLIPPTest::TestCase
       assert_log_match "clipp_print [expected]: #{expected}"
     end
   end
+
+  [
+    [ "normalize_sql_comments()" , "a-- hi\nb"    , "a-- hi\nb"],
+    [ "normalize_sql_comments()" , "a# hi\nb"     , "a# hi\nb"],
+    [ "normalize_sql_comments()" , "a/* /* */b" , "ab"],
+    [ "normalize_sql_comments()" , "a/*! hi */b" , "a hi b"],
+    [ "normalize_sql_comments()" , "a/*!213413 hi */b" , "a hi b"],
+  ].each_with_index do |test_case, i|
+    transform, input, expected = test_case
+
+    define_method "test_sql_normalize_comments_#{i}".to_sym do
+      clipp(
+        modules: %w[ sql_comments ],
+        config: """
+          InitVar SQL \"#{input}\"
+        """,
+        default_site_config: """
+          Rule SQL.#{transform} @clipp_print expected id:act1 rev:1 phase:REQUEST
+        """
+      ) do
+        transaction do |t|
+          t.request(raw: 'GET / HTTP/1.0')
+          t.response(raw:"HTTP/1.0 200 OK")
+        end
+      end
+
+      assert_no_issues
+      assert_log_match "clipp_print [expected]: #{expected}"
+    end
+  end
 end
